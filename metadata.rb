76ff2fe378e4e0afd3aa8138c126df9d99ea3d24
David Schlenk <david.schlenk@spanlink.com>
2014-03-20 11:36:34 -0400

    Version bump - LWRP for adding packages and reports to statsd-configuration.xml

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'david.schlenk@spanlink.com'
 license          'Apache 2.0'
 description      'Installs and Configures opennms'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 depends          'yum'
 depends          'hostsfile'
