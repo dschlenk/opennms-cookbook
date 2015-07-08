1ddc645c3a5c73efdf81c33e63edc3b33b2b7069
David Schlenk <david.schlenk@spanlink.com>
2015-07-08 12:35:44 -0500

    change to new versioning scheme. Still semantic, but use OpenNMS version for MSB

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs and Configures opennms 15 and provides many useful LW
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports         'centos', ">= 6.0"
 supports         'rhel', ">= 6.0"
-version          '3.0.0'
+version          '16.0.0'
 depends          'yum'
 depends          'hostsfile'
 depends          'java'
-depends          'build-essential'
+#depends          'build-essential'
