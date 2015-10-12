49eb44800a565662be170dba024a06c70ff8468b
David Schlenk <david.schlenk@spanlink.com>
2015-10-12 13:31:58 -0500

    Fix mask support in event LWRP.

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name             'opennms'
 maintainer       'David Schlenk'
 maintainer_email 'david.schlenk@spanlink.com'
 license          'Apache 2.0'
-description      'Installs and Configures opennms 15 and provides many useful LWRPs.'
+description      'Installs and Configures opennms and provides many useful LWRPs.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports         'centos', ">= 6.0"
 supports         'rhel', ">= 6.0"
-version          '16.1.0'
+version          '16.1.1'
 depends          'yum'
 depends          'hostsfile'
 depends          'java'
