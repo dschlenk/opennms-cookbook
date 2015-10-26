5fa574bd2eacea665a06206c9cfbceac8332efec
David Schlenk <david.schlenk@spanlink.com>
2015-10-26 16:21:15 -0500

    Work around a strange yum GPG import bug. Is now imported via RPM directly instead of letting Yum attempt.  Refactor the packages stuff a bit to reduce code duplication.

--- recipes/notemplates.rb
+++ recipes/notemplates.rb
@@ -20,50 +20,6 @@ include_recipe 'build-essential::default'
 chef_gem 'java_properties'
 chef_gem 'rest-client'
 
-if node['opennms']['stable']
-  yum_repository 'opennms-stable-common' do
-    description 'RPMs Common to All OpenNMS Architectures RPMs (stable)'
-    baseurl node['yum']['opennms-stable-common']['baseurl']
-    mirrorlist node['yum']['opennms-stable-common']['url']
-    gpgkey node['yum']['opennms']['key_url']
-    failovermethod node['yum']['opennms-stable-common']['failovermethod']
-    includepkgs node['yum']['opennms-stable-common']['includepkgs']
-    exclude node['yum']['opennms-stable-common']['exclude']
-    action :create
-  end
-  yum_repository 'opennms-stable-rhel6' do
-    description 'RedHat Enterprise Linux 6.x and CentOS 6.x RPMs (stable)'
-    baseurl node['yum']['opennms-stable-rhel6']['baseurl']
-    mirrorlist node['yum']['opennms-stable-rhel6']['url']
-    gpgkey node['yum']['opennms']['key_url']
-    includepkgs node['yum']['opennms-stable-rhel6']['includepkgs']
-    exclude node['yum']['opennms-stable-rhel6']['exclude']
-    action :create
-  end
-else
-  yum_repository 'opennms-snapshot-common' do
-      description 'RPMs Common to All OpenNMS Architectures RPMs (stable)'
-      baseurl node['yum']['opennms-snapshot-common']['baseurl']
-      mirrorlist node['yum']['opennms-snapshot-common']['url']
-      gpgkey node['yum']['opennms']['key_url']
-      failovermethod node['yum']['opennms-snapshot-common']['failovermethod']
-      includepkgs node['yum']['opennms-snapshot-common']['includepkgs']
-      exclude node['yum']['opennms-snapshot-common']['exclude']
-      gpgcheck false
-      action :create
-  end
-  yum_repository 'opennms-snapshot-rhel6' do
-    description 'RedHat Enterprise Linux 6.x and CentOS 6.x RPMs (stable)'
-    baseurl node['yum']['opennms-snapshot-rhel6']['baseurl']
-    mirrorlist node['yum']['opennms-snapshot-rhel6']['url']
-    gpgkey node['yum']['opennms']['key_url']
-    includepkgs node['yum']['opennms-snapshot-rhel6']['includepkgs']
-    exclude node['yum']['opennms-snapshot-rhel6']['exclude']
-      gpgcheck false
-    action :create
-  end
-end
-
 fqdn = node[:fqdn]
 fqdn ||= node[:hostname]
 
@@ -75,36 +31,7 @@ hostsfile_entry node['ipaddress'] do
   action [:create_if_missing, :append]
 end
 
-onms_packages = ['opennms-core', 'opennms-webapp-jetty', 'opennms-docs']
-onms_versions =  [node['opennms']['version'], node['opennms']['version'],
-  node['opennms']['version']]
-
-if node['opennms']['plugin']['xml']
-  onms_packages.push 'opennms-plugin-protocol-xml'
-  onms_versions.push node['opennms']['version']
-end
-
-if node['opennms']['plugin']['nsclient']
-  onms_packages.push 'opennms-plugin-protocol-nsclient'
-  onms_versions.push node['opennms']['version']
-end
-
-package onms_packages do
-  version onms_versions
-  allow_downgrade node['opennms']['allow_downgrade']
-  action :install
-end
-
-package "iplike" do
-  action :install
-end
-
-package "perl-libwww-perl" do
-  action :install
-end
-package "perl-XML-Twig" do
-  action :install
-end
+include_recipe 'opennms::packages'
 
 if node['opennms']['upgrade']
   include_recipe 'opennms::upgrade'
