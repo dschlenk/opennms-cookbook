57cb47c38e226fcfb07643ad0489b8e142c57636
David Schlenk <david.schlenk@spanlink.com>
2016-07-28 16:55:56 -0500

    add grafana dashboard to opennms front page

--- recipes/grafana.rb
+++ recipes/grafana.rb
@@ -7,6 +7,8 @@
 # License: Apache 2.0
 
 require 'digest'
+require 'rest-client'
+
 node.default['grafana']['install_type'] = 'package'
 node.default['grafana']['version'] = '3.1.0-1468321182'
 node.default['grafana']['package']['version'] = '3.1.0-1468321182'
@@ -20,15 +22,8 @@ execute 'install opennms plugin' do
   notifies :restart, 'service[grafana-server]'
 end
 
-log 'start OpenNMS' do
-  notifies :start, 'service[opennms]', :immediately
-end
-
 node.default_unless['grafana']['opennms_datasource']['password'] = random_password
 pwhash = Digest::MD5.hexdigest(node['grafana']['opennms_datasource']['password']).upcase
-opennms_user 'grafana' do
-  password pwhash
-end
 
 grafana_datasource 'OpenNMS (local)' do
   datasource(
@@ -41,4 +36,38 @@ grafana_datasource 'OpenNMS (local)' do
     isDefault: true,
     withCredentials: true
   )
+end
+
+ruby_block 'create API key for OpenNMS' do
+  block do
+    require 'rest-client'
+    begin
+    response = RestClient::Request.new(
+      method: :post,
+      url: 'http://localhost:3000/api/auth/keys',
+      user: 'admin',
+      password: 'admin',
+      headers: { :accept => :json,
+      content_type: :json },
+      payload: '{"role":"Viewer","name":"OpenNMS"}'
+    ).execute
+    results = JSON.parse(response.to_str)
+    Chef::Log.debug " results: #{results} key: #{results['key']}"
+    node.override['opennms']['properties']['grafana']['api_key'] = results['key']
+    node.override['opennms']['properties']['grafana']['show'] = true
+    rescue => e
+      Chef::Log.warn e.response
+    end
+  end
+  notifies :create, "template[#{node['opennms']['conf']['home']}/etc/opennms.properties]", :immediately
+  only_if { node['opennms']['properties']['grafana']['api_key'] == '' }
+end
+
+# required to add user via ReST
+log 'start OpenNMS' do
+  notifies :start, 'service[opennms]', :immediately
+end
+
+opennms_user 'grafana' do
+  password pwhash
 end
