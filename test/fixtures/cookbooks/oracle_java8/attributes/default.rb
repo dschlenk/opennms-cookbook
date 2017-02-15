9972e7544dbae2e2c37c52ef41d05cacf7613982
David Schlenk <david.schlenk@spanlink.com>
2017-02-15 10:34:14 -0600

    add oracle java8 test fixure cookbook

---
+++ test/fixtures/cookbooks/oracle_java8/attributes/default.rb
@@ -0,0 +1,6 @@
+default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['install_flavor']                         = 'oracle'
+default['java']['jdk']['8']['x86_64']['checksum']         = '97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc'
+default['java']['jdk']['8']['x86_64']['url']              = 'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
+default['java']['jdk_version']                            = 8
+default['java']['alternatives_priority'] = 18001210
