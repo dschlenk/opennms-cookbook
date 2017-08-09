4b032a91aca14bab83648d9beeca97e3c069db1c
David Schlenk <david.schlenk@spanlink.com>
2017-08-09 12:33:55 -0500

    make a custom resource for avail_category

---
+++ test/integration/default/libraries/avail_category.rb
@@ -0,0 +1,59 @@
+require 'rexml/document'
+class AvailCategory < Inspec.resource(1)
+  name 'avail_category'
+
+  desc '
+    OpenNMS Availability Category
+  '
+
+  example '
+    describe avail_category(\'Web Servers\') do
+      its(\'comment\') { should eq \'the comment\' }
+      its(\'normal\') { should eq \'99.0\' }
+      its(\'warning\') { should eq \'95.0\' }
+      its(\'service\') { should eq [\'HTTP\'] }
+    end
+  '
+
+  def initialize(cat_name)
+    @cat_name = cat_name
+    category_file = '/opt/opennms/etc/categories.xml'
+    cf = inspec.file(category_file)
+    doc = REXML::Document.new(cf.content)
+    cat_el = doc.elements[category_path(@cat_name)]
+    @comment = cat_el.elements['comment'].text.to_s
+    @normal = cat_el.elements['normal'].text.to_s
+    @warning = cat_el.elements['warning'].text.to_s
+    @service = []
+    cat_el.each_element('service') do |s|
+      @service.push s.text.to_s
+    end
+    @rule = cat_el.elements['rule'].text.to_s
+  end
+
+  def comment
+    @comment
+  end
+
+  def normal
+    @normal
+  end
+
+  def warning
+    @warning
+  end
+
+  def service
+    @service
+  end
+
+  def rule
+    @rule
+  end
+
+  private
+
+  def category_path(name) 
+    "/catinfo/categorygroup/categories/category[contains(.,'#{name}')]"
+  end
+end
