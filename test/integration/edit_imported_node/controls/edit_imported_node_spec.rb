5164839e9bf63cc4953b27303fb83f2f9c46eb29
David Schlenk <david.schlenk@spanlink.com>
2017-09-08 10:58:07 -0500

    edit imported node integration test

--- test/integration/edit_imported_node/controls/edit_imported_node_spec.rb
+++ test/integration/edit_imported_node/controls/edit_imported_node_spec.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 control 'edit_imported_node' do
+  describe import_node('nodeC', 'dry-source') do
+    it { should exist }
+    its('city') { should eq 'Brooklyn' }
+    its('building') { should eq 'Big' }
+    its('categories') { should eq ['Servers', 'Dev'] }
+    its('assets') { should eq({ 'vendorPhone' => '311' }) }
+  end
 end
