4da311128995fbe8bd53bb0219aaa8e78152164b
David Schlenk <david.schlenk@spanlink.com>
2017-09-08 10:40:37 -0500

    integration tests for disco_url

--- test/integration/disco_url/controls/disco_url_spec.rb
+++ test/integration/disco_url/controls/disco_url_spec.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 control 'disco_url' do
+  describe disco_url('file:/opt/opennms/etc/include') do
+    it { should exist }
+    its('file_name') { should eq '/opt/opennms/etc/include' }
+    its('retry_count') { should eq 13 }
+    its('discovery_timeout') { should eq 4000 }
+  end
+
+  describe disco_url('http://example.com/include') do
+    it { should exist }
+  end
+
+  describe disco_url('http://other.net/things') do
+    it { should exist }
+    its('location') { should eq 'Detroit' }
+    its('foreign_source') { should eq 'disco-url-source' }
+  end
 end
