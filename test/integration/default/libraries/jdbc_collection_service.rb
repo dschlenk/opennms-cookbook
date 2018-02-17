2b013cc1b0bef6f49dbc4e41e9bffcf8c971d9ec
David Schlenk <david.schlenk@spanlink.com>
2018-02-16 20:48:16 -0600

    inspec for jdbc_collection_service

---
+++ test/integration/default/libraries/jdbc_collection_service.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require 'rexml/document'
+class JdbcCollectionService < Inspec.resource(1)
+  name 'jdbc_collection_service'
+
+  desc '
+    OpenNMS jdbc_collection_service
+  '
+
+  example '
+    describe jdbc_collection_service(\'JDBC\', \'collection\', \'package\') do
+      it { should exist }
+      its(\'interval\') { should eq 300000 }
+      its(\'user_defined\') { should eq true }
+      its(\'status\') { should eq \'off\' }
+      its(\'collection_timeout\') { should eq 5000 }
+      its(\'retry_count\') { should eq 10 }
+      its(\'port\') { should eq 5432 }
+      its(\'thresholding_enabled\') { should eq true }
+      its(\'driver\') { should eq \'org.postgresql.Driver\' }
+      its(\'user\') { should eq \'wibble\' }
+      its(\'password\') { should eq \'wobble\' }
+      its(\'url\') { should eq \'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble\' }
+    end
+  '
+
+  def initialize(service, collection, package)
+    doc = REXML::Document.new(inspec.file('/opt/opennms/etc/collectd-configuration.xml').content)
+    s_el = doc.elements["/collectd-configuration/package[@name='#{package}']/service[@name='#{service}' and parameter[@key='collection' and @value='#{collection}']]"]
+    @params = {}
+    @params[:interval] = s_el.attributes['interval'].to_i
+    @params[:user_defined] = false
+    @params[:user_defined] = true if s_el.attributes['user-defined'].to_s == 'true'
+    @params[:status] = s_el.attributes['status'].to_s
+    @params[:collection_timeout] = s_el.elements["parameter[@key = 'timeout']/@value"].to_s.to_i
+    @params[:retry_count] = s_el.elements["parameter[@key = 'retry']/@value"].to_s.to_i
+    @params[:port] = s_el.elements["parameter[@key = 'port']/@value"].to_s.to_i
+    @params[:thresholding_enabled] = false
+    @params[:thresholding_enabled] = true if s_el.elements["parameter[@key = 'thresholding-enabled']/@value"].to_s == 'true'
+    @params[:driver] = s_el.elements["parameter[@key = 'driver']/@value"].to_s
+    @params[:user] = s_el.elements["parameter[@key = 'user']/@value"].to_s
+    @params[:password] = s_el.elements["parameter[@key = 'password']/@value"].to_s
+    @params[:url] = s_el.elements["parameter[@key = 'url']/@value"].to_s
+  end
+
+  def method_missing(param)
+    @params[param]
+  end
+end
--- test/integration/jdbc_collection_service/controls/jdbc_collection_service_spec.rb
+++ test/integration/jdbc_collection_service/controls/jdbc_collection_service_spec.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 control 'jdbc_collection_service' do
+  describe jdbc_collection_service('JDBCFoo', 'foo', 'foo') do
+    its('interval') { should eq 400000 }
+    its('user_defined') { should eq true }
+    its('status') { should eq 'off' }
+    its('collection_timeout') { should eq 5000 }
+    its('retry_count') { should eq 10 }
+    its('port') { should eq 15432 }
+    its('thresholding_enabled') { should eq true }
+    its('driver') { should eq 'org.postgresql.Driver' }
+    its('user') { should eq 'wibble' }
+    its('password') { should eq 'wobble' }
+    its('url') { should eq 'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble' }
+  end
+
+  describe jdbc_collection_service('JDBC', 'default', 'example1') do
+    its('driver') { should eq 'org.postgresql.Driver' }
+    its('user') { should eq 'wibble' }
+    its('password') { should eq 'wobble' }
+    its('url') { should eq 'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble' }
+    # values derived from defaults
+    its('interval') { should eq 300000 }
+    its('user_defined') { should eq false }
+    its('status') { should eq 'on' }
+    its('retry_count') { should eq 1 }
+    its('thresholding_enabled') { should eq false }
+  end
 end
