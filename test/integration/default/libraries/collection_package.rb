2277414c123aac2cd52ad6842c6f7119d8204f92
David Schlenk <david.schlenk@spanlink.com>
2018-02-19 12:21:34 -0600

    inspec for jmx things

--- test/integration/default/libraries/collection_package.rb
+++ test/integration/default/libraries/collection_package.rb
@@ -18,35 +18,42 @@ class CollectionPackage < Inspec.resource(1)
     @name = name
     doc = REXML::Document.new(inspec.file('/opt/opennms/etc/collectd-configuration.xml').content)
     p_el = doc.elements["/collectd-configuration/package[@name = '#{@name}']"]
-    @params = {}
-    @params[:filter] = p_el.elements['filter'].text.to_s
-    @params[:include_ranges] = []
-    p_el.each_element('include-range') do |ir|
-      @params[:include_ranges].push 'begin' => ir.attributes['begin'], 'end' => ir.attributes['end']
+    @exists = !p_el.nil?
+    if @exists
+      @params = {}
+      @params[:filter] = p_el.elements['filter'].text.to_s
+      @params[:include_ranges] = []
+      p_el.each_element('include-range') do |ir|
+        @params[:include_ranges].push 'begin' => ir.attributes['begin'], 'end' => ir.attributes['end']
+      end
+      @params[:exclude_ranges] = []
+      p_el.each_element('exclude-range') do |er|
+        @params[:exclude_ranges].push 'begin' => er.attributes['begin'], 'end' => er.attributes['end']
+      end
+      @params[:specifics] = []
+      p_el.each_element('specific') do |s|
+        @params[:specifics].push s.text.to_s
+      end
+      @params[:include_urls] = []
+      p_el.each_element('include-url') do |iu|
+        @params[:include_urls].push iu.text.to_s
+      end
+      @params[:store_by_if_alias] = false
+      @params[:store_by_if_alias] = true unless p_el.elements["storeByIfAlias[contains(., 'true')]"].nil?
+      @params[:store_by_node_id] = false
+      @params[:store_by_node_id] = true unless p_el.elements["storeByNodeID[contains(., 'true')]"].nil?
+      @params[:if_alias_domain] = name
+      unless p_el.elements['ifAliasDomain'].nil?
+        @params[:if_alias_domain] = p_el.elements['ifAliasDomain'].text.to_s
+      end
+      @params[:stor_flag_override] = false
+      @params[:stor_flag_override] = true unless p_el.elements["storFlagOverride[contains(., 'true')]"].nil?
+      @params[:if_alias_comment] = p_el.elements['ifAliasComment'].text.to_s unless p_el.elements['ifAliasComment'].nil?
     end
-    @params[:exclude_ranges] = []
-    p_el.each_element('exclude-range') do |er|
-      @params[:exclude_ranges].push 'begin' => er.attributes['begin'], 'end' => er.attributes['end']
-    end
-    @params[:specifics] = []
-    p_el.each_element('specific') do |s|
-      @params[:specifics].push s.text.to_s
-    end
-    @params[:include_urls] = []
-    p_el.each_element('include-url') do |iu|
-      @params[:include_urls].push iu.text.to_s
-    end
-    @params[:store_by_if_alias] = false
-    @params[:store_by_if_alias] = true unless p_el.elements["storeByIfAlias[contains(., 'true')]"].nil?
-    @params[:store_by_node_id] = false
-    @params[:store_by_node_id] = true unless p_el.elements["storeByNodeID[contains(., 'true')]"].nil?
-    @params[:if_alias_domain] = name
-    unless p_el.elements['ifAliasDomain'].nil?
-      @params[:if_alias_domain] = p_el.elements['ifAliasDomain'].text.to_s
-    end
-    @params[:stor_flag_override] = false
-    @params[:stor_flag_override] = true unless p_el.elements["storFlagOverride[contains(., 'true')]"].nil?
-    @params[:if_alias_comment] = p_el.elements['ifAliasComment'].text.to_s unless p_el.elements['ifAliasComment'].nil?
+  end
+
+  def exist?
+    @exists
   end
 
   def method_missing(param)
--- test/integration/jmx/controls/jmx_spec.rb
+++ test/integration/jmx/controls/jmx_spec.rb
@@ -1,3 +1,35 @@
 # frozen_string_literal: true
 control 'jmx' do
+  describe collection_package 'jmx1' do
+    it { should exist }
+    its('filter') { should eq "IPADDR != '0.0.0.0'" }
+  end
+
+  describe jmx_collection('jmxcollection') do
+    it { should exist }
+    its('rrd_step') { should eq 300 }
+    its('rras') { should eq ['RRA:AVERAGE:0.5:1:2016', 'RRA:AVERAGE:0.5:12:1488', 'RRA:AVERAGE:0.5:288:366', 'RRA:MAX:0.5:288:366', 'RRA:MIN:0.5:288:366'] }
+  end
+
+  describe jmx_collection_service('jmx', 'jmxcollection', 'jmx1') do
+    it { should exist }
+    its('ds_name') { should eq 'jmx-ds-name' }
+    its('friendly_name') { should eq 'jmx-friendly-name' }
+    its('interval') { should eq 300000 }
+    its('user_defined') { should eq false }
+    its('status') { should eq 'on' }
+    its('collection_timeout') { should eq 3000 }
+    its('retry_count') { should eq 1 }
+    its('thresholding_enabled') { should eq false }
+    its('port') { should eq 1099 }
+    its('protocol') { should eq 'rmi' }
+    its('url_path') { should eq '/jmxrmi' }
+    its('rrd_base_name') { should eq 'java' }
+  end
+
+  describe jmx_mbean('org.apache.activemq.Queue', 'jmxcollection') do
+    it { should exist }
+    its('objectname') { should eq 'org.apache.activemq:BrokerName=msgbroker-a.pe.spanlink.com,Type=Queue,Destination=splk.sw' }
+    its('attribs') { should eq 'ConsumerCount' => { 'alias' => '5ConsumerCnt', 'type' => 'gauge' }, 'InFlightCount' => { 'alias' => '5InFlightCnt', 'type' => 'gauge' } }
+  end
 end
