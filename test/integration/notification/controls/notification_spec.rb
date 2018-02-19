57da28cff872d5c8948ee4ce3a754c092c4817b2
David Schlenk <david.schlenk@spanlink.com>
2018-02-19 17:55:21 -0600

    inspec for notifications

--- test/integration/notification/controls/notification_spec.rb
+++ test/integration/notification/controls/notification_spec.rb
@@ -1,3 +1,28 @@
 # frozen_string_literal: true
 control 'notification' do
+  describe notification('exampleDown') do
+    it { should exist }
+    its('status') { should eq 'on' }
+    its('writeable') { should eq 'yes' }
+    its('uei') { should eq 'uei.opennms.org/example/exampleDown' }
+    its('description') { should eq 'An example of a down type event notification.' }
+    its('rule') { should eq "IPADDR != '0.0.0.0'" }
+    its('destination_path') { should eq 'Email-Admin' }
+    its('text_message') { should eq 'Your service is bad and you should feel bad.' }
+    its('subject') { should eq 'Bad service.' }
+    its('numeric_message') { should eq '31337' }
+    its('event_severity') { should eq 'Major' }
+    its('vbname') { should eq 'snmpifname' }
+    its('vbvalue') { should eq 'eth0' }
+  end
+
+  describe notification('example2Broken') do
+    it { should exist }
+    its('status') { should eq 'on' }
+    its('uei') { should eq 'example2/exampleBroken' }
+    its('destination_path') { should eq 'Email-Admin' }
+    its('text_message') { should eq 'broken' }
+    its('writeable') { should eq 'yes' }
+    its('rule') { should eq "IPADDR != '0.0.0.0'" }
+  end
 end
