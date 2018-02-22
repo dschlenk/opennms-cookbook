cdb0189db5016b52df46db47a44cbc8e2f16ec86
David Schlenk <david.schlenk@spanlink.com>
2018-02-22 16:52:01 -0600

    make the webopts test recipe actually work and then write an inspec for it

--- test/fixtures/cookbooks/onms_lwrp_test/recipes/webopts.rb
+++ test/fixtures/cookbooks/onms_lwrp_test/recipes/webopts.rb
@@ -8,11 +8,7 @@ cookbook_file 'jetty.keystore' do
   action :create_if_missing
 end
 
-node.default['opennms']['properties']['jetty']['https_port'] = 443
-node.default['opennms']['properties']['jetty']['https_host'] = '0.0.0.0'
-node.default['opennms']['properties']['jetty']['keystore'] = "#{node['opennms']['conf']['home']}/etc/jetty.keystore"
-node.default['opennms']['properties']['jetty']['ks_password'] = 'changeit'
-node.default['opennms']['properties']['jetty']['key_password'] = 'changeit'
-node.default['opennms']['properties']['jetty']['cert_alias'] = 'opennms-jetty-certificate'
-node.default['opennms']['properties']['jetty']['ajp'] = 8009
-node.default['opennms']['properties']['jetty']['ajp'] = 8009
+cookbook_file 'jetty.xml' do
+  path '/opt/opennms/etc/jetty.xml'
+end
+# see default attributes file for other configuration changes made to the web server
