f1d17a07351ddb0f8797de850484c55f073f4cb2
David Schlenk <david.schlenk@spanlink.com>
2018-02-21 20:36:38 -0600

    inspec for system def resources

--- test/integration/system_def/controls/system_def_spec.rb
+++ test/integration/system_def/controls/system_def_spec.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 control 'system_def' do
+  describe system_def('Cisco Routers') do
+    it { should exist }
+    its('groups') { should include 'mib2-tcp' }
+    its('groups') { should include 'mib2-powerethernet' }
+  end
+  describe system_def('Cisco ASA5510sy') do
+    it { should exist }
+    its('groups') { should_not include 'cisco-pix' }
+    its('groups') { should_not include 'cisco-memory' }
+  end
 end
