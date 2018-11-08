ba5af4b84646824a695ef5f33c106b0c2ccaf7a4
David Schlenk <david.schlenk@spanlink.com>
2018-11-08 14:38:01 -0600

    change order of repos to fix #42; update java version in test fixture

--- test/fixtures/cookbooks/oracle_java8/attributes/default.rb
+++ test/fixtures/cookbooks/oracle_java8/attributes/default.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['install_flavor']                         = 'oracle'
-default['java']['jdk']['8']['x86_64']['checksum']         = '97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc'
+default['java']['jdk']['8']['x86_64']['checksum']         = '53c29507e2405a7ffdbba627e6d64856089b094867479edc5ede4105c1da0d65'
 default['java']['jdk']['8']['x86_64']['url']              = 'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['url']              = 'http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz'
 default['java']['jdk_version']                            = 8
 default['java']['alternatives_priority'] = 18001210
