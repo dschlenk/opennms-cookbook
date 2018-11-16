c34642da8db23cd769fc21d8e9846ceab1e0ea9d
mcomfort <mcomfort@convergeone.com>
2018-11-16 09:42:48 -0500

    Need david help to take a look at this file

--- providers/collection_package.rb
+++ providers/collection_package.rb
@@ -44,17 +44,20 @@ end
 
 def load_current_resource
 	@current_resource = Chef::Resource.resource_for_node(:opennms_collection_package, node).new(@new_resource.name)
-	@current_resource.filter(@new_resource.filter)
-	@current_resource.specifics(@new_resource.specifics)
-	@current_resource.include_ranges(@new_resource.include_ranges)
-	@current_resource.exclude_ranges(@new_resource.exclude_ranges)
-	@current_resource.include_urls(@new_resource.include_urls)
-	@current_resource.store_by_if_alias(@new_resource.store_by_if_alias)
-	@current_resource.store_by_node_id(@new_resource.store_by_node_id)
-	@current_resource.if_alias_domain(@new_resource.if_alias_domain)
-	@current_resource.stor_flag_override(@new_resource.stor_flag_override)
-	@current_resource.if_alias_comment(@new_resource.if_alias_comment)
-	@current_resource.outage_calendars(@new_resource.outage_calendars)
+	@current_resource.name(@new_resource.name) unless @new_resource.name.nil?
+	@current_resource.remote(@new_resource.remote) unless @new_resource.remote.nil?
+	
+	@current_resource.filter(@new_resource.filter) unless @new_resource.filter.nil?
+	@current_resource.specifics(@new_resource.specifics) unless @new_resource.specifics.nil?
+	@current_resource.include_ranges(@new_resource.include_ranges) unless @new_resource.include_ranges.nil?
+	@current_resource.exclude_ranges(@new_resource.exclude_ranges) unless @new_resource.exclude_ranges.nil?
+	@current_resource.include_urls(@new_resource.include_urls) unless @new_resource.include_urls.nil?
+	@current_resource.store_by_if_alias(@new_resource.store_by_if_alias) unless @new_resource.store_by_if_alias.nil?
+	@current_resource.store_by_node_id(@new_resource.store_by_node_id) unless @new_resource.store_by_node_id.nil?
+	@current_resource.if_alias_domain(@new_resource.if_alias_domain) unless @new_resource.if_alias_domain.nil?
+	@current_resource.stor_flag_override(@new_resource.stor_flag_override) unless @new_resource.stor_flag_override.nil?
+	@current_resource.if_alias_comment(@new_resource.if_alias_comment) unless @new_resource.if_alias_comment.nil?
+	@current_resource.outage_calendars(@new_resource.outage_calendars) unless @new_resource.outage_calendars.nil?
 	
 	file = ::File.new("#{node['opennms']['conf']['home']}/etc/collectd-configuration.xml", 'r')
 	contents = file.read
@@ -62,59 +65,147 @@ def load_current_resource
 	
 	doc = REXML::Document.new(contents, respect_whitespace: :all)
 	doc.context[:attribute_quote] = :quote
-	package_el = matching_package_name(doc, @current_resource)
+	package_el = matching_package(doc, @new_resource)
 	
 	if !package_el.nil?
-		@current_resource.exists = true
-	  if filter_change?(package_el, @current_resource.filter) || specifics_change?(package_el, @current_resource.specifics) || include_ranges_change?(package_el, @current_resource.include_ranges)
-			Chef::Log.debug("uei #{@current_resource.name} has changed.")
-			@current_resource.different = true
-	  end
+		@current_resource.exists = true if package_exists?(@current_resource.name)
+		@current_resource.different = if filter_equal?(package_el, @current_resource.filter) \
+	                 && specifics_equal?(package_el, @current_resource.specifics) \
+	                 && include_ranges_equal?(package_el, @current_resource.include_ranges) \
+	                 && exclude_ranges_equal?(package_el, @current_resource.exclude_ranges) \
+	                 && include_urls_equal?(package_el, @current_resource.include_urls) \
+	                 && store_by_if_alias_equal?(package_el, @current_resource.store_by_if_alias) \
+	                 && store_by_node_id_equal?(package_el, @current_resource.store_by_node_id) \
+	                 && if_alias_domain_equal?(package_el, @current_resource.if_alias_domain) \
+	                 && stor_flag_override_equal?(package_el, @current_resource.stor_flag_override) \
+	                 && if_alias_comment_equal?(package_el, @current_resource.if_alias_comment) \
+	                 && outage_calendars_equal?(package_el, @current_resource.outage_calendars)
+			                              false
+			                            else
+				                            true
+		                              end
+	else
+		@current_resource.different = true
 	end
 end
-
-
 
 private
 
-def matching_package_name(doc, current_resource)
-	definition = nil
-	doc.elements.each('/collectd-configuration/package') do |package_name|
-		next unless package_name.attributes['name'].to_s == current_resource.to_s
-		definition = package_name
+def matching_package(doc, current_resource)
+	package = nil
+	doc.elements.each('/collectd-configuration/package') do |package_el|
+		next unless package_el.attributes['name'].to_s == current_resource.name.to_s\
+                    && package_el.attributes['remote'].to_s == current_resource.remote.to_s
+		package = package_el
 		break
 	end
-	definition
+	package
 end
 
-def filter_change?(package_el, current_filter)
-	filter_el = package_el.elements['filter'].text.to_s
-	Chef::Log.debug "Filter ? New: #{current_filter.filter}; Current: #{filter_el}"
-	return true unless filter_el == current_filter.filter
+def filter_equal?(doc, current_filter)
+	Chef::Log.debug("Filter ? New: #{doc.elements['filter'].nil?} && #{current_filter}")
+	return true if doc.elements['filter'].nil? && (current_filter.nil? || current_filter.empty?)
+	
+	filter_el = doc.elements['filter'].to_s
+	filter_el == current_filter
 end
 
-def specifics_change?(package_el, specifics)
-	Chef::Log.debug("Check for no specifics: #{package_el.elements['specific'].nil?} && #{specifics}")
-	return false if package_el.elements['specific'].nil? && (specifics.nil? || specifics.empty?)
+def specifics_equal?(doc, specifics)
+	Chef::Log.debug("Check for no specifics: #{doc.elements['specific'].nil?} && #{specifics}")
+	return true if doc.elements['specific'].nil? && (specifics.nil? || specifics.empty?)
+	
 	curr_specifics = []
-	package_el.elements.each('specific') do |specific|
+	doc.elements.each('specific') do |specific|
 		curr_specifics.push specific.text
 	end
 	curr_specifics.sort!
 	Chef::Log.debug("specifics equal? #{curr_specifics} == #{specifics}")
 	sorted_specifics = nil
 	sorted_specifics = specifics.sort unless specifics.nil?
-	return true unless curr_specifics == sorted_specifics
+	curr_specifics == sorted_specifics
 end
 
-def include_ranges_change?(package_el, include_ranges)
-	Chef::Log.debug("Check for no ranges: #{package_el.elements['include-range'].nil?} && #{include_ranges}")
-	return false if package_el.elements['include-range'].nil? && (include_ranges.nil? || include_ranges.empty?)
-	curr_include_ranges = {}
-	package_el.elements.each('include-range') do |ncr_el|
-		curr_include_ranges[ncr_el.attributes['begin']] = ncr_el.attributes['end']
+def include_ranges_equal?(doc, include_ranges)
+	Chef::Log.debug("Check for include ranges: #{doc.elements['include-range'].nil?} && #{include_ranges}")
+	return true if doc.elements['include-range'].nil? && (include_ranges.nil? || include_ranges.empty?)
+	
+	doc.elements.each('include-range') do |ncr_el|
+		ncr_el.attributes['begin'].to_s == include_ranges['begin'].to_s && ncr_el.attributes['end'].to_s == include_ranges['end'].to_s
 	end
-	return true unless curr_include_ranges == include_ranges
+
+end
+
+def exclude_ranges_equal?(doc, exclude_ranges)
+	Chef::Log.debug("Check for exclude-range: #{doc.elements['exclude-range'].nil?} && #{exclude_ranges}")
+	return true if doc.elements['exclude-range'].nil? && (exclude_ranges.nil? || exclude_ranges.empty?)
+	
+	doc.elements.each('exclude-range') do |exl_el|
+		exl_el.attributes['begin'].to_s == exclude_ranges['begin'].to_s && exl_el.attributes['end'].to_s == exclude_ranges['end'].to_s
+	end
+
+end
+
+def include_urls_equal?(doc, include_urls)
+	Chef::Log.debug("Check for no include_urls: #{doc.elements['include-url'].nil?} && #{include_urls}")
+	return true if doc.elements['include-url'].nil? && (include_urls.nil? || include_urls.empty?)
+	
+	curr_include_urls = []
+	doc.elements.each('include-url') do |urls|
+		curr_include_urls.push urls.text
+	end
+	curr_include_urls.sort!
+	Chef::Log.debug("include-url equal? #{curr_specifics} == #{include_urls}")
+	sorted_include_urls = nil
+	sorted_include_urls = include_urls.sort unless include_urls.nil?
+	curr_include_urls == sorted_include_urls
+end
+
+def store_by_if_alias_equal?(doc, store_by_if_alias)
+	return true if doc.elements['storeByIfAlias'].nil? && (store_by_if_alias.nil? || store_by_if_alias.empty?)
+	
+	store_by_if_alias_el = doc.elements['storeByIfAlias'].to_s
+	Chef::Log.debug "storeByIfAlias ? New: #{store_by_if_alias}; Current: #{store_by_if_alias_el}"
+	store_by_if_alias_el == store_by_if_alias
+end
+
+def store_by_node_id_equal?(doc, store_by_node_id)
+	return true if doc.elements['storeByNodeID'].nil? && (store_by_node_id.nil? || store_by_node_id.empty?)
+	
+	store_by_node_id_el = doc.elements['storeByNodeID'].to_s
+	Chef::Log.debug "storeByNodeID ? New: #{store_by_node_id}; Current: #{store_by_node_id_el}"
+	store_by_node_id_el == store_by_node_id
+end
+
+def if_alias_domain_equal?(doc, if_alias_domain)
+	return true if doc.elements['ifAliasDomain'].nil? && (if_alias_domain.nil? || if_alias_domain.empty?)
+	
+	if_alias_domain_el = doc.elements['ifAliasDomain'].to_s
+	Chef::Log.debug "ifAliasDomain ? New: #{if_alias_domain}; Current: #{if_alias_domain_el}"
+	if_alias_domain_el == if_alias_domain
+end
+
+def stor_flag_override_equal?(doc, stor_flag_override)
+	return true if doc.elements['storFlagOverride'].nil? && (stor_flag_override.nil? || stor_flag_override.empty?)
+	
+	stor_flag_override_el = doc.elements['storFlagOverride'].to_s
+	Chef::Log.debug "storFlagOverride ? New: #{stor_flag_override}; Current: #{stor_flag_override_el}"
+	stor_flag_override_el == stor_flag_override
+end
+
+def if_alias_comment_equal?(doc, if_alias_comment)
+	return true if doc.elements['ifAliasComment'].nil? && (if_alias_comment.nil? || if_alias_comment.empty?)
+	
+	if_alias_comment_el = doc.elements['ifAliasComment'].to_s
+	Chef::Log.debug "ifAliasComment ? New: #{if_alias_comment}; Current: #{if_alias_comment_el}"
+	if_alias_comment_el == if_alias_comment
+end
+
+def outage_calendars_equal?(doc, outage_calendars)
+	return true if doc.elements['outage-calendar'].nil? && (outage_calendars.nil? || outage_calendars.empty?)
+	
+	outage_calendars_el = doc.elements['outage-calendar'].to_s
+	Chef::Log.debug "outage-calendar ? New: #{outage_calendars}; Current: #{outage_calendars_el}"
+	outage_calendars_el == outage_calendars
 end
 
 def package_exists?(name)
@@ -189,72 +280,92 @@ end
 
 def update_collection_package
 	Chef::Log.debug "Updating collectd-configuration.xml definition : '#{new_resource.name}'"
-	file = ::File.new("#{node['opennms']['conf']['home']}/etc/collectd-configuration.xml")
+	file = ::File.new("#{node['opennms']['conf']['home']}/etc/collectd-configuration.xml", 'r')
 	contents = file.read
-	doc = REXML::Document.new(contents, respect_whitespace: :all)
-	doc.context[:attribute_quote] = :quote
 	file.close
 	
-	package_el = matching_package_name(doc, new_resource.name)
+	doc = REXML::Document.new(contents, respect_whitespace: :all)
+	doc.context[:attribute_quote] = :quote
 	
-	filter_el = package_el.add_element('filter')
-	filter_el.add_text(REXML::CData.new(new_resource.filter))
-
-	unless new_resource.specifics.nil?
-		new_resource.specifics.each do |specific|
-			if package_el.elements["specific[text() = '#{specific}']"].nil?
-				specific_el = package_el.add_element 'specific'
-				specific_el.add_text(specific)
+	package_el = matching_package(doc, new_resource)
+	
+	if !package_el.nil?
+		unless new_resource.filter.nil?
+			if package_el.elements["filter[text()  = '#{new_resource.filter}']"].nil?
+				filter_el = package_el.add_element 'filter'
+				filter_el.add_text(REXML::CData.new(new_resource.filter))
 			end
 		end
-	end
-	unless new_resource.include_ranges.nil?
-		new_resource.include_ranges.each do |r_begin, r_end|
-			if !package_el.nil? && package_el.elements["include-range[@begin = '#{r_begin}' and @end = '#{r_end}']"].nil?
-				package_el.add_element 'include-range', 'begin' => r_begin, 'end' => r_end
+		unless new_resource.specifics.nil?
+			new_resource.specifics.each do |specific|
+				if package_el.elements["specific[text() = '#{specific}']"].nil?
+					sel = package_el.add_element 'specific'
+					sel.add_text(specific)
+				end
 			end
 		end
-	end
-	unless new_resource.exclude_ranges.nil?
-		new_resource.exclude_ranges.each do |r_begin, r_end|
-			if !package_el.nil? && package_el.elements["exclude-range[@begin = '#{r_begin}' and @end = '#{r_end}']"].nil?
-				package_el.add_element 'exclude_range', 'begin' => r_begin, 'end' => r_end
+		unless new_resource.include_ranges.nil?
+			new_resource.include_ranges.each do |r_begin, r_end|
+				if package_el.elements["include-range[@begin = '#{r_begin}' @end = '#{r_end}']"].nil?
+					package_el.add_element 'include-range', 'begin' => r_begin, 'end' => r_end
+				end
 			end
 		end
-	end
-	unless new_resource.include_urls.nil?
-		new_resource.include_urls.each do |include_url|
-			if package_el.elements["include-url[text() = '#{include_url}']"].nil?
-				include_url_el = package_el.add_element 'include-url'
-				include_url_el.add_text(include_url)
+		unless new_resource.exclude_ranges.nil?
+			new_resource.exclude_ranges.each do |r_begin, r_end|
+				if package_el.elements["exclude-range[@begin = '#{r_begin}'  @end = '#{r_end}']"].nil?
+					package_el.add_element 'exclude-range', 'begin' => r_begin, 'end' => r_end
+				end
 			end
 		end
-	end
-	if new_resource.store_by_if_alias == true
-		sb_ifalias_el = package_el.add_element('storeByIfAlias')
-		sb_ifalias_el.add_text('true')
-	end
-	if new_resource.store_by_node_id != 'normal'
-		sb_nodeid_el = package_el.add_element('storeByNodeID')
-		sb_nodeid_el.add_text(new_resource.store_by_node_id)
-	end
-	unless new_resource.if_alias_domain.nil?
-		ifalias_domain_el = package_el.add_element('ifAliasDomain')
-		ifalias_domain_el.add_text(new_resource.if_alias_domain)
-	end
-	if new_resource.stor_flag_override == true
-		stor_flag_override_el = package_el.add_element('storFlagOverride')
-		stor_flag_override_el.add_text('true')
-	end
-	unless new_resource.if_alias_comment.nil?
-		ifalias_comment_el = package_el.add_element('ifAliasComment')
-		ifalias_comment_el.add_text(new_resource.if_alias_comment)
-	end
-	unless new_resource.outage_calendars.nil?
-		new_resource.outage_calendars.each do |oc|
-			if package_el.elements["outage-calendar[text() = '#{oc}']"].nil?
-				oc_el = package_el.add_element ('outage-calendar')
-				oc_el.add_text(oc)
+		unless new_resource.include_urls.nil?
+			new_resource.include_urls.each do |include_url|
+				if package_el.elements["include-url[text() = '#{new_resource.include_urls}']"].nil?
+					include_url_el = package_el.add_element 'include-url'
+					include_url_el.add_text(include_url)
+				end
+			end
+		end
+		unless new_resource.store_by_if_alias.nil?
+			if package_el.elements["storeByIfAlias[text() = '#{new_resource.store_by_if_alias}']"].nil?
+				if new_resource.store_by_if_alias == true
+					sb_ifalias_el = package_el.add_element 'storeByIfAlias'
+					sb_ifalias_el.add_text('true')
+				end
+			end
+		end
+		unless new_resource.store_by_node_id.nil?
+			if package_el.elements["storeByNodeID[text() = '#{new_resource.store_by_node_id}']"].nil?
+				if new_resource.store_by_node_id != 'normal'
+					sb_nodeid_el = package_el.add_element 'storeByNodeID'
+					sb_nodeid_el.add_text(new_resource.store_by_node_id)
+				end
+			end
+		end
+		unless new_resource.if_alias_domain.nil?
+			if package_el.elements["ifAliasDomain[text() = '#{new_resource.if_alias_domain}']"].nil?
+				dm_ifalias_el = package_el.add_element 'ifAliasDomain'
+				dm_ifalias_el.add_text(new_resource.if_alias_domain)
+			end
+		end
+		unless new_resource.stor_flag_override.nil?
+			if package_el.elements["storFlagOverride[text() = '#{new_resource.stor_flag_override}']"].nil?
+				stor_flag_override_el = package_el.add_element 'storFlagOverride'
+				stor_flag_override_el.add_text(new_resource.stor_flag_override)
+			end
+		end
+		unless new_resource.if_alias_comment.nil?
+			if package_el.elements["storFlagOverride[text() = '#{new_resource.stor_flag_override}']"].nil?
+				ifalias_comment_el = package_el.add_element 'ifAliasComment'
+				ifalias_comment_el.add_text(new_resource.if_alias_comment)
+			end
+		end
+		unless new_resource.outage_calendars.nil?
+			new_resource.outage_calendars.each do |oc|
+				if package_el.elements["outage-calendar[text() = '#{oc}']"].nil?
+					oc_el = package_el.add_element 'outage-calendar'
+					oc_el.add_text(oc)
+				end
 			end
 		end
 	end
@@ -269,8 +380,7 @@ def delete_collection_package
 	doc = REXML::Document.new(contents, respect_whitespace: :all)
 	doc.context[:attribute_quote] = :quote
 	
-	package_el = matching_package_name(doc, new_resource)
+	package_el = matching_package(doc, new_resource)
 	doc.root.delete(package_el)
-	
 	Opennms::Helpers.write_xml_file(doc, "#{node['opennms']['conf']['home']}/etc/collectd-configuration.xml")
 end
