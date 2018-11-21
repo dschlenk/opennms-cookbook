a7b14fc769ab067c5cb49503e04708a72649b083
mcomfort <mcomfort@convergeone.com>
2018-11-20 22:03:20 -0500

    Fixed error converge

--- test/fixtures/cookbooks/onms_lwrp_test/recipes/collection_package_update_filter.rb
+++ test/fixtures/cookbooks/onms_lwrp_test/recipes/collection_package_update_filter.rb
@@ -2,13 +2,7 @@
 include_recipe 'onms_lwrp_test::collection_package'
 opennms_collection_package 'foo' do
 	filter "(IPADDR != '1.1.1.1') & (categoryName == 'update_foo')"
-  remote true
-  action :create
-  notifies :restart, 'service[opennms]', :delayed
-end
-
-opennms_collection_package 'bar' do
-	filter "IPADDR != '10.0.0.10'"
+	remote true
 	action :create
 	notifies :restart, 'service[opennms]', :delayed
 end
--- test/integration/collection_package_update_filter/controls/collection_package_update_filter_spec.rb
+++ test/integration/collection_package_update_filter/controls/collection_package_update_filter_spec.rb
@@ -4,9 +4,4 @@ control 'collection_package_update_filter' do
 		its('remote'){should eq true}
 		its('filter') {should eq "(IPADDR != '1.1.1.1') & (categoryName == 'update_foo')"}
 	end
-	
-	describe collection_package 'bar' do
-		it {should exist}
-		its('filter') {should eq "(IPADDR != '10.0.0.10')"}
-	end
 end
