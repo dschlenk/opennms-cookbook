551247f4c631ada289c7236b791effa3a33a47a0
David Schlenk <dschlenk@convergeone.com>
2019-05-13 15:41:35 -0500

    need a non-name attribute way of referring to a group name to do deletes properly. Fix logic errors.

--- providers/snmp_collection_group.rb
+++ providers/snmp_collection_group.rb
@@ -31,21 +31,22 @@ action :create do
 end
 
 action :delete do
-	if @current_resource.exists
-		converge_by("Deleting #{@new_resource}") do
-			delete_snmp_collection_group
-		end
-	else
-		Chef::Log.info("#{@new_resource} does not exist - nothing to do.")
-	end
+  if @current_resource.exists
+    converge_by("Deleting #{@new_resource}") do
+      delete_snmp_collection_group
+    end
+  else
+    Chef::Log.info("#{@new_resource} does not exist - nothing to do.")
+  end
 end
 
 
 def load_current_resource
   @current_resource = Chef::Resource.resource_for_node(:opennms_snmp_collection_group, node).new(@new_resource.name)
+  @current_resource.group_name(@new_resource.group_name || @new_resource.name)
   @current_resource.collection_name(@new_resource.collection_name)
 
-  # Good enough for create but that's about it
+  # Good enough for create / delete, but needs work if we implement support for changing existing
   if group_exists?(@current_resource.collection_name, @current_resource.name)
     @current_resource.exists = true
   end
@@ -61,7 +62,8 @@ def group_exists?(collection_name, name)
 end
 
 def create_snmp_collection_group
-  Chef::Log.debug "Adding snmp collection group: '#{new_resource.name}'"
+  group_name = new_resource.group_name || new_resource.name
+  Chef::Log.debug "Adding snmp collection group: '#{group_name}'"
   file = ::File.new("#{node['opennms']['conf']['home']}/etc/datacollection-config.xml")
   contents = file.read
   doc = REXML::Document.new(contents, respect_whitespace: :all)
@@ -69,7 +71,7 @@ def create_snmp_collection_group
   file.close
 
   collection_el = doc.elements["/datacollection-config/snmp-collection[@name='#{new_resource.collection_name}']"]
-  include_collection_el = collection_el.add_element 'include-collection', 'dataCollectionGroup' => new_resource.name
+  include_collection_el = collection_el.add_element 'include-collection', 'dataCollectionGroup' => group_name
   unless new_resource.system_def.nil?
     include_collection_el.add_attribute('systemDef', new_resource.system_def)
   end
@@ -78,7 +80,7 @@ def create_snmp_collection_group
     exclude_filter_el.add_text(REXML::CData.new(exclude_filter))
   end
   dcfile = new_resource.file || 'foo'
-  cookbook_file "#{dcfile}_#{new_resource.name}" do
+  cookbook_file "#{dcfile}_#{group_name}" do
     source dcfile
     path "#{node['opennms']['conf']['home']}/etc/datacollection/#{new_resource.file}"
     owner 'root'
@@ -97,26 +99,19 @@ def restart_collectd
 end
 
 def delete_snmp_collection_group
-	group_name = new_resource.collection_name || new_resource.name
-	Chef::Log.debug "Deleting snmp collection group : '#{group_name}'"
-	
-	file = ::File.new("#{node['opennms']['conf']['home']}/etc/datacollection-config.xml", 'r')
-	contents = file.read
-	doc = REXML::Document.new(contents, respect_whitespace: :all)
-	doc.context[:attribute_quote] = :quote
-	snmp_collection_group_el = snmp_collection_group_el(doc, new_resource, true)
-	Chef::Log.debug("element deleted is #{snmp_collection_group_el}")
-	Opennms::Helpers.write_xml_file(doc, "#{node['opennms']['conf']['home']}/etc/xml-datacollection-config.xml")
-end
+  group_name = new_resource.group_name || new_resource.name
+  Chef::Log.debug "Deleting snmp collection group : '#{group_name}'"
+  
+  file = ::File.new("#{node['opennms']['conf']['home']}/etc/datacollection-config.xml", 'r')
+  contents = file.read
+  doc = REXML::Document.new(contents, respect_whitespace: :all)
+  doc.context[:attribute_quote] = :quote
+  file.close
 
-def snmp_collection_group_el(doc, resource, delete = false)
-	group_name = resource.collection_name || resource.name
-	collection_el = doc.elements["/datacollection-config/snmp-collection[@name='#{resource.collection_name}']"]
-	include_collection_el = collection_el.add_element 'include-collection', 'dataCollectionGroup' => resource.name
-	
-	if group_exists?(resource.collection_name, resource.name)
-		Chef::Log.debug("found? #{include_collection_el}")
-		doc.root.elements.delete(include_collection_el) if !include_collection_el.nil? && delete
-	end
-	include_collection_el
+  # only remove the reference to the group from the collection - the file stays put in case it is used by another collection
+  # or a group in the file is used by another systemDef, etc
+  inc_collection_el = doc.elements["/datacollection-config/snmp-collection[@name='#{resource.collection_name}']/include-collection[@dataCollectionGroup = '#{group_name}']"]
+  doc.root.elements.delete(inc_collection_el)
+
+  Opennms::Helpers.write_xml_file(doc, "#{node['opennms']['conf']['home']}/etc/xml-datacollection-config.xml")
 end
--- test/integration/snmp_collection_group_delete/controls/snmp_collection_group_delete_spec.rb
+++ test/integration/snmp_collection_group_delete/controls/snmp_collection_group_delete_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 control 'snmp_collection_group_delete' do
-	describe snmp_collection_group('group-name-to-delete', 'file-name-to-delete.xml', 'collection-name-to-delete') do
-		it { should_not exist }
-	end
+  describe snmp_collection_group('group-name-to-delete', 'file-name-to-delete.xml', 'collection-name-to-delete') do
+    it { should_not exist }
+  end
 end
 
 control 'snmp_collection_group_delete' do
-	describe snmp_collection_group('group-name-tobe-delete', 'file-name-to-delete.xml', 'default') do
-		it { should_not exist }
-	end
+  describe snmp_collection_group('group-name-tobe-delete', 'file-name-to-delete.xml', 'default') do
+    it { should_not exist }
+  end
 end
