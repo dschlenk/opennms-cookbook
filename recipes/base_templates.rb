479f9d32997373c7a36dfaa1c6683896fcc63a42
isiamionau <isiiamionau@convergeone.com>
2020-02-11 06:46:39 -0600

    WAT-2254

--- recipes/base_templates.rb
+++ recipes/base_templates.rb
@@ -40,6 +40,8 @@ when '23'
   template_dir = 'horizon-23/'
 when '24'
   template_dir = 'horizon-24/'
+when '25'
+  template_dir = 'horizon-25/'
 end
 if Opennms::Helpers.major(node['opennms']['version']).to_i >= 22
   node.default['opennms']['datacollection']['default']['ref_cpq_im'] = true
