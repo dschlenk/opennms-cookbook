1952b6c78238205735415ba47f161c6eb0855941
David Schlenk <dschlenk@convergeone.com>
2020-02-26 11:10:08 -0600

    make postgres work when opennms ver < 25

--- test/fixtures/cookbooks/onms_lwrp_test/recipes/poller_delete.rb
+++ test/fixtures/cookbooks/onms_lwrp_test/recipes/poller_delete.rb
@@ -6,3 +6,10 @@ opennms_poller_service 'delete SNMPBar2' do
   class_name 'org.opennms.netmgt.poller.monitors.SnmpMonitor'
   action :delete
 end
+
+opennms_poller_service "delete 'SNMPBar 2'" do
+  service_name 'SNMPBar 2'
+  package_name 'bar'
+  class_name 'org.opennms.netmgt.poller.monitors.SnmpMonitor'
+  action :delete
+end
