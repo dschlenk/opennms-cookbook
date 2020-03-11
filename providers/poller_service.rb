29b6851c2b644865ec5d0cd6a97a3df93f800f53
David Schlenk <dschlenk@convergeone.com>
2020-03-11 09:13:09 -0500

    find changes when switching from some params to none

--- providers/poller_service.rb
+++ providers/poller_service.rb
@@ -95,6 +95,10 @@ def service_changed?(current_resource)
   end
   curr_params = {}
   params_str = {}
+  service_el.elements.each("parameter[@key != 'port' and @key != 'timeout']") do |p|
+    val = p.attributes['value'].to_s # make a string outta whatever this was
+    curr_params[p.attributes['key']] = val
+  end
   if current_resource.parameters.is_a?(Hash)
     # make a version of params that has strings for both keys and values
     # just so we can compare without forcing certain param values to
@@ -102,12 +106,11 @@ def service_changed?(current_resource)
     current_resource.parameters.each do |k, v|
       params_str[k.to_s] = v.to_s
     end
-    service_el.elements.each("parameter[@key != 'port' and @key != 'timeout']") do |p|
-      val = p.attributes['value'].to_s # make a string outta whatever this was
-      curr_params[p.attributes['key']] = val
-    end
     Chef::Log.debug "curr_params: '#{curr_params}'; params: #{params_str}"
     return true if !current_resource.parameters.nil? && curr_params != params_str
+  elsif !curr_params.empty?
+    # means we've got some parameters that aren't special, and we don't want any
+    return true
   end
   false
 end
