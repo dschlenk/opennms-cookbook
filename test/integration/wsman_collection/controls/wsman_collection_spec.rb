704d434cc2d5522fb81e5380644aab492e4d935f
Melissa Comfort <mcomfort@convergeone.com>
2020-08-10 12:32:05 -0400

    fixed test

--- test/integration/wsman_collection/controls/wsman_collection_spec.rb
+++ test/integration/wsman_collection/controls/wsman_collection_spec.rb
@@ -3,7 +3,7 @@ control 'wsman_collection' do
   describe wsman_collection('foo') do
     its('rrd_step') { should eq 600 }
     its('rras') { should eq ['RRA:AVERAGE:0.5:2:4032', 'RRA:AVERAGE:0.5:24:2976', 'RRA:AVERAGE:0.5:576:732', 'RRA:MAX:0.5:576:732', 'RRA:MIN:0.5:576:732'] }
-    its('include_system_definitions') { should eq nil }
+    its('include_system_definitions') { should eq nil } # we add empty <include_system_definitions/>
   end
 
   # minimal
