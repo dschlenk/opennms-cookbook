0082bd6c295bc1c72f9bf07358d7bec7f16a15bd
Melissa Comfort <mcomfort@convergeone.com>
2021-08-03 20:58:05 -0400

    Install jdk11

--- test/fixtures/cookbooks/openjdk_java11/metadata.rb
+++ test/fixtures/cookbooks/openjdk_java11/metadata.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 name 'openjdk_java11'
 license 'Apache 2.0'
-version '0.1.0'
-depends 'java'
 source_url        'https://github.com/sous-chefs/java'
 issues_url        'https://github.com/sous-chefs/java/issues'
+version '0.1.0'
+depends 'java'
+
