4ebe6071898f154ca570b4172bad57c66261033c
Melissa Comfort <mcomfort@convergeone.com>
2021-08-03 22:28:39 -0400

    Install jdk11

--- test/fixtures/cookbooks/openjdk_java11/recipes/default.rb
+++ test/fixtures/cookbooks/openjdk_java11/recipes/default.rb
@@ -1,6 +1,6 @@
-include_recipe 'java'
+# frozen_string_literal: true
 
 apt_update
-
+include_recipe 'java::default'
 # make sure we have java installed
 openjdk_pkg_install node['java']['jdk_version']
