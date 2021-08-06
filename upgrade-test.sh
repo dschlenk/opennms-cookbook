1f913f0b2ee0d1ba388f9f22bf5108d80f5a2175
Melissa Comfort <mcomfort@convergeone.com>
2021-08-06 17:16:03 -0400

    regenreate the suites for testing

--- upgrade-test.sh
+++ upgrade-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 BASE_VERSION=16
-TEST_VERSIONS=(17 18 19 20 21 22 23 24 25 26)
+TEST_VERSIONS=(25 26 27 28)
 CENTOS_REL=69
 PREV=16
 chef exec kitchen destroy default-${BASE_VERSION}-centos-${CENTOS_REL}
