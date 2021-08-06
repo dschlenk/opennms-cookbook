6c68a94e92530396f261b6482171db6cf833edc8
Melissa Comfort <mcomfort@convergeone.com>
2021-08-06 13:10:16 -0400

    Fixed integration test

--- test/integration/default/controls/default_spec.rb
+++ test/integration/default/controls/default_spec.rb
@@ -3,7 +3,9 @@ describe 'opennms::default' do
   it 'installs opennms' do
     expect(package('opennms-core')).to be_installed
     expect(package('opennms-webapp-jetty')).to be_installed
-    expect(package('opennms-docs')).to be_installed unless node['opennms']['version'].to_i == 28
+    unless node['opennms']['version'].to_i == 28
+      expect(package('opennms-docs')).to be_installed
+    end
   end
 end
 describe service('opennms') do
