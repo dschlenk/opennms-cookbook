21ac00453443f798f63a47d6b356d6725547c3f2
Melissa Comfort <mcomfort@convergeone.com>
2021-08-17 19:27:48 -0400

    version 27 and up did not use the attributes port

--- test/integration/webopts/controls/webopts_spec.rb
+++ test/integration/webopts/controls/webopts_spec.rb
@@ -4,8 +4,9 @@ control 'webopts' do
     it { should exist }
   end
 
-  describe port('8443') do
-    it { should be_listening }
-    its('processes') { should include 'java' }
-  end
+  # Version 27 did not use test/fixtures/cookbooks/onms_lwrp_test/attributes/default.rb anymore. I comment it out and that why we don't need test either
+  #describe port('8443') do
+  #  it { should be_listening }
+  #  its('processes') { should include 'java' }
+  #end
 end
