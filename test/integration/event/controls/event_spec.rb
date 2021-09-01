d0015fbddd041ce27189e9e6228b3fc420532bf7
Melissa Comfort <mcomfort@convergeone.com>
2021-09-01 15:49:08 -0400

    Add test for position top & evenconf_position top in new created file

--- test/integration/event/controls/event_spec.rb
+++ test/integration/event/controls/event_spec.rb
@@ -91,4 +91,17 @@ control 'event' do
   describe event('uei.opennms.org/anUeiForANewThingInANewFile', 'events/chef2.events.xml') do
     it { should_not exist }
   end
+
+  describe event('uei.opennms.org/anUeiForANewThingInANewFileWithPositionTop', 'events/chef3.events.xml') do
+    it { should exist }
+    its('position') { should eq 0 }
+    its('event_label') { should eq 'Chef defined event: thresholdExceeded' }
+    its('descr') { should eq '<p>A threshold defined by a chef recipe has been exceeded.</p>' }
+    its('logmsg') { should eq 'A threshold has been exceeded.' }
+    its('logmsg_dest') { should eq 'logndisplay' }
+    its('logmsg_notify') { should be true }
+    its('severity') { should eq 'Minor' }
+    its('alarm_data') { should eq('reduction_key' => '%uei%:%dpname%:%nodeid%:%interface%:%parm[ds]%:%parm[threshold]%:%parm[trigger]%:%parm[rearm]%:%parm[label]%',  'alarm_type' => 1, 'auto_clean' => false) }
+
+  end
 end
