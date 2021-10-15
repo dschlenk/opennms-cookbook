0b8320e0997ac4d347f42426c5d634a373e2f5ea
David Schlenk <dschlenk@convergeone.com>
2021-10-15 17:20:48 -0500

    restore upgrade test to full suite

--- upgrade-test.sh
+++ upgrade-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-BASE_VERSION=26
-TEST_VERSIONS=(26 27 28)
+BASE_VERSION=16
+TEST_VERSIONS=(17 18 19 20 21 22 23 24 25 26 27 28)
 CENTOS_REL=7
-PREV=26
+PREV=16
 chef exec kitchen destroy default-${BASE_VERSION}-centos-${CENTOS_REL}
 chef exec kitchen verify default-${BASE_VERSION}-centos-${CENTOS_REL}
 for v in ${TEST_VERSIONS[@]}; do
