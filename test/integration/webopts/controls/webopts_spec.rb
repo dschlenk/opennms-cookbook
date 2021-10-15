4e84fdeebbb9d904bbc17ffe58e456ebb1d421e6
David Schlenk <dschlenk@convergeone.com>
2021-10-15 17:19:21 -0500

    restore webopts test

--- test/integration/webopts/controls/webopts_spec.rb
+++ test/integration/webopts/controls/webopts_spec.rb
@@ -4,9 +4,8 @@ control 'webopts' do
     it { should exist }
   end
 
-  # Version 27 did not use test/fixtures/cookbooks/onms_lwrp_test/attributes/default.rb anymore. I comment it out and that why we don't need test either
-  # describe port('8443') do
-  #  it { should be_listening }
-  #  its('processes') { should include 'java' }
-  # end
+  describe port('8443') do
+    it { should be_listening }
+    its('processes') { should include 'java' }
+  end
 end
