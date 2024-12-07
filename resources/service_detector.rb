f5b23ffbb6ddc4acd21e4d5adfd1e328ba860f6d
Melissa Comfort <mcomfort@onec1.com>
2024-12-07 07:05:07 -0500

    Add service detector suite to dokken

--- resources/service_detector.rb
+++ resources/service_detector.rb
@@ -56,7 +56,7 @@ action :create do
     detectors = foreign_source.elements["/detectors"]
     # create a REXML::Element with a name attribute and a class attribute, then add parameter children for each of new_resource.parameters + timeout, retry_count, port
     # then add the element to foreign_source.elements["/detectors"]
-    if detector.nil?add_element 'detector', 'name' => service_name, 'class' => new_resource.class_name
+    if detector.nil? add_element 'detector', 'name' => service_name, 'class' => new_resource.class_name
       detector_el << REXML::XMLDecl.new
       detector_el.add_element('detector', { 'name' => service_name, 'class' => new_resource.class_name })
       unless new_resource.timeout.nil?
