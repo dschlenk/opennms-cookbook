a1e2f461c5a5bf29dff48a1549bbec944f36af87
David Schlenk <dschlenk@onec1.com>
2024-12-09 16:16:23 +0000

    WIP

--- resources/collection_graph.rb
+++ resources/collection_graph.rb
@@ -1,16 +1,52 @@
-# frozen_string_literal: true
 # Adds/updates/deletes a graph in either the specified currently existing or
 # new graph file in $ONMS_HOME/etc/snmp-graph.properties.d/
 # or the default collection graph file, $ONMS_HOME/etc/snmp-graph.properties
 
-actions :create, :create_if_missing, :delete
-default_action :create
+include Opennms::XmlHelper
+include Opennms::Cookbook::Graph::CollectionGraphTemplate
 
-attribute :short_name, kind_of: String, name_attribute: true
-attribute :long_name, kind_of: String, required: true
-attribute :columns, kind_of: Array, required: true
-attribute :type, kind_of: String, required: true
-attribute :command, kind_of: String, required: true
-attribute :file, kind_of: String # refers to the name of the file to add the graph def to
+property :short_name, String, name_property: true
+property :file, String, identity: true # refers to the name of the file to add the graph def to
+# required for new
+property :long_name, String
+# required for new
+property :columns, Array, callbacks: {
+  'should be an Array of Strings' => lambda { |p|
+    !p.any? { |a| !a.is_a?(String) }
+  }
+}
+# required for new
+property :type, String
+# required for new
+property :command, String
 
-attr_accessor :exists, :type_exists, :file_exists
+action_class do
+  include Opennms::XmlHelper
+  include Opennms::Cookbook::Graph::CollectionGraphTemplate
+end
+
+load_current_value do |new_resource|
+  current_value_does_not_exist! unless ::File.exist?("#{onms_etc}/snmp-graph.properties.d/#{new_resource.file}")
+  gf = cgf_resource("#{onms_etc}/snmp-graph.properties.d/#{new_resource.file}").variables[:config] unless cgf_resource("#{onms_etc}/snmp-graph.properties.d/#{new_resource.file}").nil?
+  gf = Opennms::Cookbook::Graph::CollectionGraphPropertiesFile.read("#{onms_etc}/snmp-graph.properties.d/#{new_resource.file}") if gf.nil?
+  report = gf.reports[new_resource.short_name]
+  current_value_does_not_exist! if report.nil?
+  # this:
+  %i(long_name columns type command).each do |p|
+    send(p, report.send(p))
+  end
+  # is the same as this:
+  #long_name report.long_name
+  #columns report.columns
+  #type report.type
+  #command report.command
+end
+
+action :create do
+end
+action :create_if_missing do
+end
+action :update do
+end
+action :delete do
+end
