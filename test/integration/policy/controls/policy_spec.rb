2ebb394fafe09d7983ffb5026858861d68cc4b4e
Melissa Comfort <mcomfort@onec1.com>
2024-12-11 11:18:45 -0500

    adding policy

--- test/integration/policy/controls/policy_spec.rb
+++ test/integration/policy/controls/policy_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 control 'policy' do
-  describe foreign_source('policy-source') do
+  describe foreign_source('policy-source', 1239) do
     it { should exist }
   end
 
-  describe policy('Production Category', 'policy-source') do
+  describe policy('Production Category', 'policy-source', 1239) do
     it { should exist }
     its('class_name') { should eq 'org.opennms.netmgt.provision.persist.policies.NodeCategorySettingPolicy' }
     its('parameters') { should eq 'category' => 'Test', 'matchBehavior' => 'ALL_PARAMETERS' }
