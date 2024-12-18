ab0ab6b27d75d5517ca537ad8b5e18e253517214
Melissa Comfort <mcomfort@onec1.com>
2024-12-18 13:24:39 -0500

    Add import

--- resources/import.rb
+++ resources/import.rb
@@ -3,18 +3,15 @@ include Opennms::Cookbook::Provision::ForeignSourceHttpRequest
 include Opennms::XmlHelper
 include Opennms::Rbac
 
-
+property :name,  String, identity: true
 property :foreign_source_name, String, default: 'imported:'
 property :sync_import, [TrueClass, FalseClass], default: false
 property :sync_wait_periods, Integer, default: 30
 property :sync_wait_secs, Integer, default: 10
-property :class_name, String
-property :foreign_source_name, String, identity: true
 
 load_current_value do |new_resource|
-  fs_resource_init(new_resource.foreign_source_name)
+  fs_resource_init(new_resource.name)
   model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message) unless model_import(new_resource.foreign_source_name).nil?
-  model_import = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new(new_resource.foreign_source_name, "#{baseurl}/requisitions/#{new_resource.foreign_source_name}").message) if model_import.nil?
   current_value_does_not_exist! if model_import.nil?
   Chef::Log.debug "add_import response is: #{model_import}"
 end
@@ -26,7 +23,7 @@ end
 
 action :create do
   converge_if_changed do
-    fs_resource_init(new_resource.foreign_source_name)
+    fs_resource_init(new_resource.name)
     model_import_init(new_resource.foreign_source_name)
     model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root
     model_import(new_resource.foreign_source_name).message model_import.to_s
@@ -38,7 +35,7 @@ end
 
 action :sync do
   converge_if_changed do
-    fs_resource_init(new_resource.foreign_source_name)
+    fs_resource_init(new_resource.name)
     model_import_init(new_resource.foreign_source_name)
     model_import_sync(new_resource.foreign_source_name, true)
   end
