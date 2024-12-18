fc63ae3c463b81f0789685d5130645d1d091f8c4
Melissa Comfort <mcomfort@onec1.com>
2024-12-18 16:25:23 -0500

    Add import

--- resources/import.rb
+++ resources/import.rb
@@ -10,7 +10,6 @@ property :sync_wait_periods, Integer, default: 30
 property :sync_wait_secs, Integer, default: 10
 
 load_current_value do |new_resource|
-  fs_resource_init(new_resource.foreign_source_name)
   model_import = REXML::Document.new(model_import(new_resource.name).message) unless model_import(new_resource.name).nil?
   model_import = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new(new_resource.foreign_source_name, "#{baseurl}/requisitions/#{new_resource.name}").message) if model_import.nil?
   current_value_does_not_exist! if model_import.nil?
@@ -26,7 +25,6 @@ end
 
 action :create do
   converge_if_changed do
-    fs_resource_init(new_resource.foreign_source_name)
     model_import_init(new_resource.name, new_resource.foreign_source_name)
     model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root
     model_import(new_resource.foreign_source_name).message model_import.to_s
@@ -38,7 +36,6 @@ end
 
 action :sync do
   converge_if_changed do
-    fs_resource_init(new_resource.foreign_source_name)
     model_import_init(new_resource.name, new_resource.foreign_source_name)
     model_import_sync(new_resource.name, new_resource.foreign_source_name, true)
   end
