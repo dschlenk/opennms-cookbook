7975bfcbced2941d85247cecba48f90231bda77b
David Schlenk <dschlenk@convergeone.com>
2024-12-19 11:35:05 -0600

    Update response_graph.rb
    method name fix in load_current_value; TODO comments in :delete action

--- resources/response_graph.rb
+++ resources/response_graph.rb
@@ -18,7 +18,8 @@ end
 
 load_current_value do |new_resource|
   current_value_does_not_exist! unless ::File.exist?("#{onms_etc}/response-graph.properties")
-  current_value_does_not_exist! unless check_graph_in_file("#{onms_etc}/response-graph.properties", new_resource.short_name)
+  # fixed: method name not correct
+  current_value_does_not_exist! unless check_file_for_graph("#{onms_etc}/response-graph.properties", new_resource.short_name)
 end
 
 action :create do
@@ -33,12 +34,18 @@ action :create_if_missing do
   run_action(:create)
 end
 
+# TODO: implement the missing methods in libraries/graph.rb (noted below),
+# write test code in test/fixtures/cookbooks/opennms_resource_tests/recipes/response_graph.rb that uses it
+# and InSpec assertion code in test/integration/response_graph/controls/response_graph_spec.rb.
 action :delete do
+  # Typically you don't wrap :delete actions in converge_if_changed blocks. Instead, check to see if it exists and use a converge_by block if it does and do nothing if it doesn't. See resources/snmp_collection.rb for an example.
   converge_if_changed do
-    if check_graph_in_file(graph_file_path, new_resource.short_name)
-      remove_graph_from_file(new_resource.short_name)
+    # fixed: method name not correct
+    if check_file_for_graph(graph_file_path, new_resource.short_name) # graph_file_path is not (yet?) a method in libraries/graph.rb
+      remove_graph_from_file(new_resource.short_name) # remove_graph_from_file is not (yet) a method in libraries/graph.rb
       Chef::Log.info("Deleted graph #{new_resource.short_name} from #{onms_etc}/response-graph.properties")
     else
+      # this is unnecessary. Chef will 
       Chef::Log.warn("Graph #{new_resource.short_name} not found in #{onms_etc}/response-graph.properties. Skipping deletion.")
     end
   end
