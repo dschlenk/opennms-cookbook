e5e5e97c1a50c070c114ce45a172851182b7cf88
Melissa Comfort <mcomfort@onec1.com>
2024-12-31 20:45:47 -0500

    Add import node interface

--- resources/import_node.rb
+++ resources/import_node.rb
@@ -31,6 +31,7 @@ load_current_value do |new_resource|
   current_value_does_not_exist! if model_import_node.nil?
   node = model_import_node.elements["node[@foreign-id = '#{new_resource.foreign_id}']"]
   current_value_does_not_exist! if node.nil?
+  node_label node.attributes['node-label']  unless node.attributes['node-label'].nil?
   parent_foreign_source node.attributes['parent-foreign-source'] unless node.attributes['parent-foreign-source'].nil?
   parent_foreign_id node.attributes['parent-foreign-id'] unless node.attributes['parent-foreign-id'].nil?
   parent_node_label node.attributes['parent-node-label'] unless node.attributes['parent-node-label'].nil?
