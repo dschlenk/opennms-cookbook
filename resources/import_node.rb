d37fe3b59b501e73bfc258d39daa6d66203e5d82
Melissa Comfort <mcomfort@onec1.com>
2024-12-31 22:15:11 -0500

    Add import node interface

--- resources/import_node.rb
+++ resources/import_node.rb
@@ -64,9 +64,9 @@ action :create do
     model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root unless model_import(new_resource.foreign_source_name).nil?
     model_import_node = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}").message) unless model_import.nil?
     import_node = model_import_node.elements["node[@foreign-id = '#{new_resource.foreign_id}']"] unless model_import_node.nil?
-    node_name = new_resource.node_label || new_resource.name
+  
     if import_node.nil?
-      node_el = model_import_node.add_element 'node', 'node-label' => node_name, 'foreign-id' => new_resource.foreign_id
+      node_el = model_import.add_element 'node', 'node-label' => new_resource.node_label, 'foreign-id' => new_resource.foreign_id
       unless new_resource.parent_foreign_source.nil?
         node_el.attributes['parent-foreign-source'] = new_resource.parent_foreign_source
       end
@@ -94,7 +94,7 @@ action :create do
       end
       model_import_node_create(new_resource.foreign_source_name).message model_import.to_s
     else
-      import_node.attributes['node-label'] = node_name
+      import_node.attributes['node-label'] = new_resource.node_label
       import_node.attributes['foreign-id'] = new_resource.foreign_id
       unless new_resource.parent_foreign_source.nil?
         import_node.attributes['parent-foreign-source'] = new_resource.parent_foreign_source
