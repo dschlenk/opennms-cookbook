4cb97119355c7c50d431c9289cdcbb4810cecb68
Melissa Comfort <mcomfort@onec1.com>
2024-12-31 21:58:13 -0500

    Add import node interface

--- resources/import_node.rb
+++ resources/import_node.rb
@@ -4,7 +4,7 @@ include Opennms::Rbac
 
 property :node_label, String
 property :foreign_source_name, String
-property :foreign_id, String, required: true
+property :foreign_id, String
 property :parent_foreign_source, String
 property :parent_foreign_id, String
 property :parent_node_label, String
