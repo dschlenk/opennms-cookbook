2846cbf9aeb27dd25f040f393058981462791b8b
Melissa Comfort <mcomfort@onec1.com>
2025-01-02 13:20:23 -0500

    Refactoring

--- resources/import_node_interface.rb
+++ resources/import_node_interface.rb
@@ -42,7 +42,7 @@ end
 
 action :create do
   converge_if_changed do
-    import_url = "/requisitions/#{new_resource.foreign_source_name}"
+    import_url = "/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}/interfaces/#{new_resource.ip_addr}"
     model_import_init(new_resource.foreign_source_name, import_url)
     model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root unless model_import(new_resource.foreign_source_name).nil?
     model_import_node = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}").message) unless model_import.nil?
