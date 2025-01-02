237dc702d738362fa259b8a331d2bdaadf9a0781
Melissa Comfort <mcomfort@onec1.com>
2025-01-02 09:48:52 -0500

    Refactoring

--- resources/import.rb
+++ resources/import.rb
@@ -7,8 +7,9 @@ property :foreign_source_name, String, default: 'imported:'
 property :sync_import, [TrueClass, FalseClass], default: false
 property :sync_wait_periods, Integer, default: 30
 property :sync_wait_secs, Integer, default: 10
-
+attr_accessor :import_url, String
 load_current_value do |new_resource|
+  import_url "/requisitions/#{new_resource.name}"
   model_import = REXML::Document.new(model_import(new_resource.name).message) unless model_import(new_resource.name).nil?
   model_import = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new(new_resource.foreign_source_name, "#{baseurl}/requisitions/#{new_resource.name}").message) if model_import.nil?
   current_value_does_not_exist! if model_import.nil?
@@ -19,12 +20,12 @@ action_class do
   include Opennms::Cookbook::Provision::ModelImportHttpRequest
   include Opennms::XmlHelper
   include Opennms::Rbac
+
 end
 
 action :create do
   converge_if_changed do
-    url = "/requisitions/#{new_resource.name}"
-    model_import_init(new_resource.name, url)
+    model_import_init(new_resource.name, import_url)
     model_import = REXML::Document.new(model_import(new_resource.name).message).root
     model_import(new_resource.name).message model_import.to_s
     if !new_resource.sync_import.nil? && new_resource.sync_import
@@ -35,8 +36,7 @@ end
 
 action :sync do
   converge_if_changed do
-    url = "/requisitions/#{new_resource.name}"
-    model_import_init(new_resource.name, url)
+    model_import_init(new_resource.name, import_url)
     model_import_sync(new_resource.name, true)
   end
 end
