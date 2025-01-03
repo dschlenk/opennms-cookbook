942fb5c6957a7c20c7c41329e5cda1d372b4a5f0
Melissa Comfort <mcomfort@onec1.com>
2025-01-03 09:30:21 -0500

    Refactoring

--- resources/import_node_interface.rb
+++ resources/import_node_interface.rb
@@ -25,8 +25,8 @@ load_current_value do |new_resource|
   current_value_does_not_exist! if model_import.nil?
   model_import_node = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}").message) unless model_import.nil?
   current_value_does_not_exist! if model_import_node.nil?
-  interface = model_import_node.elements["interface[@ip-addr = '#{new_resource.ip_addr}']"] unless model_import_node.nil?
-  #interface = import_node.elements["interface[@ip-addr = '#{new_resource.ip_addr}']"]
+  model_import_node_interface = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}/interfaces/#{new_resource.ip_addr}").message) unless model_import.nil?
+  interface = model_import_node_interface.elements["interface[@ip-addr = '#{new_resource.ip_addr}']"] unless model_import_node_interface.nil?
   current_value_does_not_exist! if interface.nil?
   status interface.attributes['status'] unless interface.attributes['status'].nil?
   managed interface.attributes['managed'] unless interface.attributes['managed'].nil?
@@ -41,15 +41,12 @@ end
 
 action :create do
   converge_if_changed do
-    model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root unless model_import(new_resource.foreign_source_name).nil?
-    model_import_node_interface = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}/interfaces/#{new_resource.ip_addr}").message) unless model_import.nil?
-    if model_import_node_interface.nil?
-      model_import.add_element 'node', 'node-label' => new_resource.name, 'foreign-id' => new_resource.foreign_id
-    else
-      interface = model_import_node_interface.elements["interface[@ip-addr = '#{new_resource.ip_addr}']"]
-    end
-
-    current_value_does_not_exist! if interface.nil?
+    model_import_init(new_resource.foreign_source_name)
+    model_import = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root
+    model_import_node = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}").message) unless model_import.nil?
+    import_node = model_import_node.elements["node [@node-label = '#{new_resource.name}' and @foreign-id = '#{new_resource.foreign_id}']"] unless model_import_node.nil?
+    model_import_node_interface = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}/interfaces/#{new_resource.ip_addr}").message) unless import_node.nil?
+    interface = model_import_node_interface.elements["interface[@ip-addr = '#{new_resource.ip_addr}']"] unless model_import_node_interface.nil?
     if interface.nil?
       i_el = model_import.add_element 'interface', 'ip-addr' => new_resource.ip_addr
       unless new_resource.status.nil?
@@ -73,7 +70,7 @@ action :create do
         interface.attributes['snmp-primary'] = new_resource.snmp_primary
       end
       model_import(new_resource.foreign_source_name).message model_import.to_s
-    end
+      end
   end
 end
 
