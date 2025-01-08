0cd25743e229be98fa461a4e1579e2da894ca75d
Melissa Comfort <mcomfort@onec1.com>
2025-01-08 15:24:38 -0500

    refactoring

--- resources/import_node_interface.rb
+++ resources/import_node_interface.rb
@@ -76,13 +76,13 @@ end
 action :create do
   converge_if_changed do
     model_import_init(new_resource.foreign_source_name)
-    model_import_root = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root
-    Chef::Application.fatal!("Missing requisition #{new_resource.foreign_source_name}.") if model_import_root.nil?
-    current_value_does_not_exist! if model_import_root.nil?
+    model_import_root = REXML::Document.new(model_import(new_resource.foreign_source_name).message).root unless model_import(new_resource.foreign_source_name).nil?
+    # Chef::Application.fatal!("Missing requisition #{new_resource.foreign_source_name}.") if model_import_root.nil?
+    # current_value_does_not_exist! if model_import_root.nil?
     model_import_root = REXML::Document.new(Opennms::Cookbook::Provision::ModelImport.new("#{new_resource.foreign_source_name}", "#{baseurl}/requisitions/#{new_resource.foreign_source_name}/nodes/#{new_resource.foreign_id}").message) if model_import_root.nil?
     current_value_does_not_exist! if model_import_root.nil?
     node_el = model_import_root.elements["node[@foreign-id = '#{new_resource.foreign_id}']"] unless model_import_root.nil?
-    Chef::Application.fatal!( "Missing node with foreign ID #{new_resource.foreign_id}.") unless node_el.nil?
+    #Chef::Application.fatal!( "Missing node with foreign ID #{new_resource.foreign_id}.") unless node_el.nil?
     interface_el = node_el.elements["interface[@ip-addr = '#{new_resource.name}']"] unless node_el.nil?
     Chef::Log.debug "Missing requisition #{new_resource.foreign_source_name}." if interface_el.nil?
     if interface_el.nil?
