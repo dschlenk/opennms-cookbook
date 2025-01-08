1a3f2b1a1882b7c3bea3373a27b3bd9c7cdb1083
Melissa Comfort <mcomfort@onec1.com>
2025-01-08 15:56:08 -0500

    refactoring

--- test/integration/import_node_interface/controls/import_node_interface_spec.rb
+++ test/integration/import_node_interface/controls/import_node_interface_spec.rb
@@ -8,7 +8,7 @@ control 'import_node_interface' do
 
   describe import_node_interface('72.72.72.73', 'dry-source', 'interfaceTestNodeID', 1242) do
     it { should exist }
-    its('managed') { should be true }
+    its('managed') { should be false }
     its('snmp_primary') { should eq 'N' }
   end
 end
