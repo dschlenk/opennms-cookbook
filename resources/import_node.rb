cff5d8db2c61a9e077da07270dfaa19502b447fc
Melissa Comfort <mcomfort@onec1.com>
2025-01-09 17:12:43 -0500

    Add meta-data test

--- resources/import_node.rb
+++ resources/import_node.rb
@@ -97,7 +97,7 @@ action :create do
       end
       unless new_resource.meta_data.nil?
         new_resource.meta_data.each do |metadata|
-          node_el.add_element'meta-data', 'context' => metadata["context"] , 'key' => metadata["key"], 'value' => metadata["value"]
+          node_el.add_element'meta-data', 'context' => metadata['context'] , 'key' => metadata['key'], 'value' => metadata['value']
         end
       end
     else import_node.attributes['node-label'] = new_resource.name
@@ -131,7 +131,7 @@ action :create do
     end
     unless new_resource.meta_data.nil?
       new_resource.meta_data.each do |metadata|
-          import_node.add_element 'meta-data', 'context' => metadata["context"] , 'key' => metadata["key"], 'value' => metadata["value"]
+          import_node.add_element 'meta-data', 'context' => metadata['context'] , 'key' => metadata['key'], 'value' => metadata['value']
       end
     end
     end
