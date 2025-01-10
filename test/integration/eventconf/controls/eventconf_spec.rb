52e9831b42835e467422f4e229f602c7007c30b2
ssgayathri <sgayatri@onec1.com>
2025-01-10 20:31:06 +0530

    Update eventconf_spec.rb

--- test/integration/eventconf/controls/eventconf_spec.rb
+++ test/integration/eventconf/controls/eventconf_spec.rb
@@ -268,7 +268,6 @@ EOL
 
   describe eventconf('create-if-missing-event.xml') do
     it { should exist }
-    its('position') { should eq 26 }
   end
 
   describe eventconf('noop-create-if-missing-event.xml') do
