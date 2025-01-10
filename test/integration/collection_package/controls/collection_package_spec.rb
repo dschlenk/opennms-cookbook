c1fea742d78c367bf0e1f7c9338f898b079005d4
ssgayathri <sgayatri@onec1.com>
2025-01-10 17:03:28 +0530

    Update collection_package_spec.rb
    adding noop create_if_missing

--- test/integration/collection_package/controls/collection_package_spec.rb
+++ test/integration/collection_package/controls/collection_package_spec.rb
@@ -17,8 +17,21 @@ control 'collection_package' do
   end
 
   describe collection_package('create_if_missing') do
+    package_name 'foo'  
     its('remote') { should eq true }
     its('filter') { should eq "IPADDR != '0.0.0.0' & categoryName == 'foo'" }
+    its('specifics') { should eq ['10.0.0.1'] }
+    its('include_ranges') { should eq [{ 'begin' => '10.0.1.1', 'end' => '10.0.1.254' }] }
+    its('exclude_ranges') { should eq [{ 'begin' => '10.0.2.1', 'end' => '10.0.2.254' }] }
+    its('include_urls') { should eq ['file:/opt/opennms/etc/foo'] }
+    its('store_by_if_alias') { should eq true }
+    its('if_alias_domain') { should eq 'foo.com' }
+    its('outage_calendars') { should eq ['ignore localhost on mondays'] }
+  end
+
+  describe collection_package 'noop create_if_missing' do
+    package_name 'foo'
+    its('filter') { should eq "IPADDR != '0.0.0.0'" }
     its('specifics') { should eq ['10.0.0.1'] }
     its('include_ranges') { should eq [{ 'begin' => '10.0.1.1', 'end' => '10.0.1.254' }] }
     its('exclude_ranges') { should eq [{ 'begin' => '10.0.2.1', 'end' => '10.0.2.254' }] }
