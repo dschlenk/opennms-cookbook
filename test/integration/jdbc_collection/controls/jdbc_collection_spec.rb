0f837906f816f294400f2ef20881e871bcd43159
David Schlenk <dschlenk@convergeone.com>
2025-01-15 09:18:25 -0600

    Update jdbc_collection_spec.rb

--- test/integration/jdbc_collection/controls/jdbc_collection_spec.rb
+++ test/integration/jdbc_collection/controls/jdbc_collection_spec.rb
@@ -15,6 +15,6 @@ control 'jdbc_collection' do
   describe jdbc_collection('create_if_missing') do
     it { should exist }
     its('rrd_step') { should eq 600 }
-    its('rras') { should eq ['RRA:AVERAGE:0.5:1:2016', 'RRA:AVERAGE:0.5:12:1488', 'RRA:AVERAGE:0.5:288:366', 'RRA:MAX:0.5:288:366', 'RRA:MIN:0.5:288:366'] }
+    its('rras') { should eq ['RRA:AVERAGE:0.5:2:4032', 'RRA:AVERAGE:0.5:24:2976', 'RRA:AVERAGE:0.5:576:732', 'RRA:MAX:0.5:576:732', 'RRA:MIN:0.5:576:732'] }
   end
 end
