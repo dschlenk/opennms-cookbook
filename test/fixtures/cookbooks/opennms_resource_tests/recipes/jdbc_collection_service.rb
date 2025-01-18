0c10b884b7e0f29f37daccff11bfbb3807311174
ssgayathri <sgayatri@onec1.com>
2025-01-18 19:21:11 +0530

    Update jdbc_collection_service.rb
    adding create if missing

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/jdbc_collection_service.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/jdbc_collection_service.rb
@@ -27,3 +27,42 @@ opennms_jdbc_collection_service 'JDBC' do
   password 'wobble'
   url 'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble'
 end
+
+opennms_jdbc_collection_service 'create_if_missing' do
+  service_name 'create_if_missing'
+  collection 'create_if_missing'
+  package_name 'jdbc_package_create_if_missing'
+  interval 400_000
+  user_defined true
+  status 'off'
+  timeout 5000
+  retry_count 10
+  port 15_432
+  thresholding_enabled true
+  driver 'org.postgresql.Driver'
+  driver_file 'jdbc.jar'
+  user 'wibble'
+  password 'wobble'
+  url 'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble'
+  action :create_if_missing
+end
+
+opennms_jdbc_collection_service 'noop_create_if_missing' do
+  service_name 'noop_create_if_missing'
+  collection 'create_if_missing'
+  package_name 'jdbc_package_noop_create_if_missing'
+  interval 400_000
+  user_defined true
+  status 'off'
+  timeout 5000
+  retry_count 10
+  port 15_432
+  thresholding_enabled true
+  driver 'org.postgresql.Driver'
+  driver_file 'jdbc.jar'
+  user 'wibble'
+  password 'wobble'
+  url 'jdbc:postgresql://OPENNMS_JDBC_HOSTNAME:15432/wibble_wobble'
+  action :create_if_missing
+  only_if { false }
+end
