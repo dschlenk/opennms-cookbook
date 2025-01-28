d2e7e1c0003b7a1666d18860f6a5359da064fadd
ssgayathri <sgayatri@onec1.com>
2025-01-28 20:14:31 +0530

    Update syslog_file.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/syslog_file.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/syslog_file.rb
@@ -6,14 +6,14 @@ opennms_syslog_file 'syslog-file2.xml' do
   position 'bottom'
 end
 
+opennms_syslog_file 'NetgearProsafeSmartSwitch.syslog.xml' do
+  action :delete
+end
+
 opennms_syslog_file 'create-if-missing-syslog-file.xml' do
   action :create_if_missing
 end
 
 opennms_syslog_file 'noop-create-if-missing-syslog-file.xml' do
   action :create_if_missing
-end
-
-opennms_syslog_file 'NetgearProsafeSmartSwitch.syslog.xml' do
-  action :delete
 end
