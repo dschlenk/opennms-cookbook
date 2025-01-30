735cd998523e9e2138894c52d5842b67332a0ba7
ssgayathri <sgayatri@onec1.com>
2025-01-30 16:16:19 +0530

    Update xml_source.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/xml_source.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/xml_source.rb
@@ -86,6 +86,6 @@ opennms_xml_source 'http://{ipaddr}/rpc' do
         { 'name' => 'V21', 'type' => 'GAUGE', 'xpath' => 'cfm-iter-ethlm-entry/cfm-last-measured-near-end-eir-loss-stats' },
         { 'name' => 'V22', 'type' => 'GAUGE', 'xpath' => 'cfm-iter-ethlm-entry/cfm-last-measured-far-end-eir-loss-stats' },
       ]
-    }
+    },
   ]
 end
