2381ba6f7287bbdcef5c83ed7bf0257d3f130e58
ssgayathri <sgayatri@onec1.com>
2025-02-05 19:20:47 +0530

    Update threshd_service_spec.rb

--- test/integration/threshd_service/controls/threshd_service_spec.rb
+++ test/integration/threshd_service/controls/threshd_service_spec.rb
@@ -6,7 +6,7 @@ control 'threshd_service' do
     its('include_ranges') { should eq [{ 'begin' => '172.17.14.1', 'end' => '172.17.14.254' }, { 'begin' => '172.17.21.1', 'end' => '172.17.21.254' }] }
     its('exclude_ranges') { should eq [{ 'begin' => '10.1.0.1', 'end' => '10.254.254.254' }] }
     its('include_urls') { should eq ['file:/opt/opennms/etc/include2'] }
-    its('services') { should eq [{ 'name' => 'SNMP', 'interval' => 300_000, 'status' => 'on', 'params' => { 'thresholding-group' => 'cheftest2' } }, { 'name' => 'WS-Man', 'interval' => 300000, 'status' => 'on', 'user-defined' => true, 'params' => { 'thresholding-group' => 'cheftest2' } }, { 'name' => 'SNMP', 'status' => 'off', 'params' => { 'thresholding-group' => 'cheftest2' } }] }
+    its('services') { should eq [{ 'name' => 'SNMP', 'interval' => 300_000, 'status' => 'on', 'params' => { 'thresholding-group' => 'cheftest2' } }, { 'name' => 'WS-Man', 'interval' => 300000, 'status' => 'on', 'user-defined' => true, 'params' => { 'thresholding-group' => 'cheftest2' } }, { 'name' => 'WS-Man2', 'status' => 'off', 'params' => { 'thresholding-group' => 'cheftest2' } }] }
   end
 
   describe threshd_package('mib2') do
