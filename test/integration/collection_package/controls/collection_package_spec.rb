dd001629692c36ef8da4705fa717fadf2e17de95
ssgayathri <sgayatri@onec1.com>
2025-02-07 00:53:43 +0530

    Update collection_package_spec.rb

--- test/integration/collection_package/controls/collection_package_spec.rb
+++ test/integration/collection_package/controls/collection_package_spec.rb
@@ -27,15 +27,4 @@ control 'collection_package' do
     its('if_alias_domain') { should eq 'foo.com' }
     its('outage_calendars') { should eq ['ignore localhost on mondays'] }
   end
-
-  describe collection_package 'noop create_if_missing' do
-    its('filter') { should eq "IPADDR != '0.0.0.0'" }
-    its('specifics') { should eq ['10.0.0.1'] }
-    its('include_ranges') { should eq [{ 'begin' => '10.0.1.1', 'end' => '10.0.1.254' }] }
-    its('exclude_ranges') { should eq [{ 'begin' => '10.0.2.1', 'end' => '10.0.2.254' }] }
-    its('include_urls') { should eq ['file:/opt/opennms/etc/foo'] }
-    its('store_by_if_alias') { should eq true }
-    its('if_alias_domain') { should eq 'foo.com' }
-    its('outage_calendars') { should eq ['ignore localhost on mondays'] }
-  end
 end
