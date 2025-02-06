439e5d03fbda2999ab89e9349a3ad9276e2f78a9
ssgayathri <sgayatri@onec1.com>
2025-02-07 01:08:36 +0530

    Update collection_package.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/collection_package.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/collection_package.rb
@@ -30,17 +30,3 @@ opennms_collection_package 'create_if_missing' do
   outage_calendars ['ignore localhost on mondays']
   action :create_if_missing
 end
-
-# create_if_missing that doesn't do anything
-opennms_collection_package 'noop create_if_missing' do
-  filter "IPADDR != '0.0.0.0'"
-  specifics ['10.0.0.1']
-  include_ranges [{ 'begin' => '10.0.1.1', 'end' => '10.0.1.254' }]
-  exclude_ranges [{ 'begin' => '10.0.2.1', 'end' => '10.0.2.254' }]
-  include_urls ['file:/opt/opennms/etc/foo']
-  store_by_if_alias true
-  if_alias_domain 'foo.com'
-  remote true
-  outage_calendars ['ignore localhost on mondays']
-  action :create_if_missing
-end
