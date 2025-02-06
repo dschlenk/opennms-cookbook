cef1c42b5f4c0c73b241c8cd6556a1b10cff03ed
ssgayathri <sgayatri@onec1.com>
2025-02-06 15:20:54 +0530

    Update threshold_common.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/threshold_common.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/threshold_common.rb
@@ -54,6 +54,6 @@ opennms_threshold_group 'hrstorage' do
   action :delete
 end
 
-opennms_threshold_group 'create_if_missing' do
+opennms_threshold_group 'cheftest' do
   action :create_if_missing
 end
