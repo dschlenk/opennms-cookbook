f8eac2a3bf5e1063946b32becd9bf8386ae9ab9d
David Schlenk <dschlenk@convergeone.com>
2025-03-21 11:15:54 -0500

    Update translation_helper.rb

--- libraries/translation_helper.rb
+++ libraries/translation_helper.rb
@@ -116,11 +116,11 @@ module Opennms
         attr_reader :assignments, :preserve_snmp_data
 
         def initialize(assignments: nil, preserve_snmp_data: nil)
-          if assignments.nil?
-            assignments = []
-          else
-            assignments = assignments
-          end
+          @assignments = if assignments.nil?
+                          []
+                        else
+                          assignments
+                        end
           @preserve_snmp_data = preserve_snmp_data
         end
 
