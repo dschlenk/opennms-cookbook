59cc099af76d8cafc8bc49c7846f10c9415f55d7
ssgayathri <sgayatri@onec1.com>
2025-03-24 19:42:26 +0530

    Update translation_specs.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/translation_specs.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/translation_specs.rb
@@ -1,5 +1,5 @@
 log 'calltranslationspecs'
-assignment = Opennms::Cookbook::Translations::TranslationAssignment.new(name: 'uei', type: 'field', 'value' => Opennms::Cookbook::Translations::TranslationValue.new(type: 'constant', result: 'uei.opennms.org/translatedUei'))
+assignment = Opennms::Cookbook::Translations::TranslationAssignment.new(name: 'uei', type: 'field', value => Opennms::Cookbook::Translations::TranslationValue.new(type: 'constant', result: 'uei.opennms.org/translatedUei'))
 mapping = Opennms::Cookbook::Translations::TranslationMapping.new(assignments: [assignment])
 spec = Opennms::Cookbook::Translations::TranslationSpec.new(uei: 'uei.opennms.org/fakeUei')
 spec.mappings.push mapping
