8d7da0467351130dcf507b0209630cadbcf4a140
ssgayathri <sgayatri@onec1.com>
2025-04-02 00:49:57 +0530

    Update translation_specs_spec.rb

--- test/integration/translation_specs/controls/translation_specs_spec.rb
+++ test/integration/translation_specs/controls/translation_specs_spec.rb
@@ -182,4 +182,106 @@ control 'translation_specs' do
   ]) do
     it { should exist }
   end
+
+  describe translation_spec('uei.opennms.org/vendor/Cisco/traps/ciscoConfigManEvent', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/internal/translator/entityConfigChanged',
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'configSource',
+        type: 'parameter',
+        value: {
+          values: [
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.9\.9\.43\.1\.1\.6\.1\.3\..*', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'configUser',
+        type: 'parameter',
+        value: {
+          type: 'constant',
+          result: 'Unknown',
+        },
+      },
+    },
+  ]) do
+    it { should exist }
+  end
+
+  describe translation_spec('uei.opennms.org/vendor/Juniper/traps/jnxCmCfgChange', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/internal/translator/entityConfigChanged',
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'configSource',
+        type: 'parameter',
+        value: {
+          values: [
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.4\..*', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'configUser',
+        type: 'parameter',
+        value: {
+          values: [
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.5\..*', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+  ]) do
+    it { should exist }
+  end
+
+  describe translation_spec('uei.opennms.org/internal/telemetry/clockSkewDetected', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/translator/telemetry/clockSkewDetected',
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'nodeid',
+        type: 'field',
+        value: {
+          type: 'sql',
+          result: 'SELECT n.nodeid FROM node n, ipinterface i WHERE n.nodeid = i.nodeid AND i.ipaddr = ? AND n.location = ?',
+          values: [
+            { type: 'field', name: 'interface', matches: '.*', result: '${0}' },
+            { type: 'parameter', name: 'monitoringSystemLocation', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+  ]) do
+    it { should exist }
+  end
 end
