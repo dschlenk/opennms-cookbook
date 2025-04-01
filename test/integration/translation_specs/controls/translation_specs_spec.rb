ff882368eebc3c1843f742fbb05574ac540688be
ssgayathri <sgayatri@onec1.com>
2025-04-02 00:24:51 +0530

    Update translation_specs_spec.rb

--- test/integration/translation_specs/controls/translation_specs_spec.rb
+++ test/integration/translation_specs/controls/translation_specs_spec.rb
@@ -1,7 +1,19 @@
 control 'translation_specs' do
-  describe translation_spec('uei.opennms.org/internal/telemetry/clockSkewDetected', [{ assignment: { name: 'uei', type: 'field', value: { type: 'constant', result: 'uei.opennms.org/internal/telemetry/clockSkewDetected' } } }]) do
+  describe translation_spec('uei.opennms.org/internal/telemetry/clockSkewDetected', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/internal/telemetry/clockSkewDetected',
+        },
+      },
+    },
+  ]) do
     it { should exist }
   end
+
   describe translation_spec('uei.opennms.org/generic/traps/SNMP_Link_Down', [
     {
       assignment: {
@@ -9,9 +21,9 @@ control 'translation_specs' do
         type: 'field',
         value: {
           type: 'constant',
-          result: 'uei.opennms.org/translator/traps/SNMP_Link_Down'
-        }
-      }
+          result: 'uei.opennms.org/translator/traps/SNMP_Link_Down',
+        },
+      },
     },
     {
       assignment: {
@@ -23,10 +35,10 @@ control 'translation_specs' do
           result: 'SELECT snmp.snmpIfDescr FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
           values: [
             { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' }
-          ]
-        }
-      }
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
     },
     {
       assignment: {
@@ -38,10 +50,10 @@ control 'translation_specs' do
           result: 'SELECT snmp.snmpIfName FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
           values: [
             { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' }
-          ]
-        }
-      }
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
     },
     {
       assignment: {
@@ -53,10 +65,10 @@ control 'translation_specs' do
           result: 'SELECT snmp.snmpIfAlias FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
           values: [
             { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' }
-          ]
-        }
-      }
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
     },
     {
       assignment: {
@@ -64,10 +76,10 @@ control 'translation_specs' do
         type: 'field',
         value: {
           type: 'constant',
-          result: 'uei.opennms.org/internal/topology/linkDown'
-        }
-      }
-    }
+          result: 'uei.opennms.org/internal/topology/linkDown',
+        },
+      },
+    },
   ]) do
     it { should exist }
   end
