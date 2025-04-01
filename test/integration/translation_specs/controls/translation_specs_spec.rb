ad2db9a103ba8453a30198d1d15392306e1a1be4
ssgayathri <sgayatri@onec1.com>
2025-04-02 00:36:00 +0530

    Update translation_specs_spec.rb

--- test/integration/translation_specs/controls/translation_specs_spec.rb
+++ test/integration/translation_specs/controls/translation_specs_spec.rb
@@ -83,4 +83,103 @@ control 'translation_specs' do
   ]) do
     it { should exist }
   end
+
+    describe translation_spec('uei.opennms.org/generic/traps/SNMP_Link_Up', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/translator/traps/SNMP_Link_Up',
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'ifDescr',
+        type: 'parameter',
+        default: 'Unknown',
+        value: {
+          type: 'sql',
+          result: 'SELECT snmp.snmpIfDescr FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
+          values: [
+            { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'ifName',
+        type: 'parameter',
+        default: 'Unknown',
+        value: {
+          type: 'sql',
+          result: 'SELECT snmp.snmpIfName FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
+          values: [
+            { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'ifAlias',
+        type: 'parameter',
+        default: 'Unknown',
+        value: {
+          type: 'sql',
+          result: 'SELECT snmp.snmpIfAlias FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
+          values: [
+            { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
+            { type: 'parameter', name: '~^\.1\.3\.6\.1\.2\.1\.2\.2\.1\.1\.([0-9]*)$', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/internal/topology/linkUp',
+        },
+      },
+    },
+  ]) do
+    it { should exist }
+  end
+
+    describe translation_spec('uei.opennms.org/external/hyperic/alert', [
+    {
+      assignment: {
+        name: 'uei',
+        type: 'field',
+        value: {
+          type: 'constant',
+          result: 'uei.opennms.org/internal/translator/hypericAlert',
+        },
+      },
+    },
+    {
+      assignment: {
+        name: 'nodeid',
+        type: 'field',
+        value: {
+          type: 'sql',
+          result: 'SELECT n.nodeid FROM node n WHERE n.foreignid = ? AND n.foreignsource = ?',
+          values: [
+            { type: 'parameter', name: 'platform.id', matches: '.*', result: '${0}' },
+            { type: 'parameter', name: 'alert.source', matches: '.*', result: '${0}' },
+          ],
+        },
+      },
+    },
+  ]) do
+    it { should exist }
+  end
 end
