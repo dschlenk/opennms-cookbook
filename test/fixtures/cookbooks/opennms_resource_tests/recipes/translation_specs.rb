b369eec6c6ff397dfe4cb9f7d9938a81f5ca7984
ssgayathri <sgayatri@onec1.com>
2025-04-06 21:27:52 +0530

    Update translation_specs.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/translation_specs.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/translation_specs.rb
@@ -11,59 +11,56 @@ end
 log 'calltranslationspecs2'
 
 value1 = Opennms::Cookbook::Translations::TranslationValue.new(
-    type: 'constant',
-    result: 'uei.opennms.org/translatedUei'
-  )
-  value2 = Opennms::Cookbook::Translations::TranslationValue.new(
-    type: 'constant',
-    result: 'Major'
-  )
-  value3 = Opennms::Cookbook::Translations::TranslationValue.new(
-    type: 'expression',
-    result: 'Issue detected on ${ifName}: ${alarmText}'
-  )
-  value4 = Opennms::Cookbook::Translations::TranslationValue.new(
-    type: 'sql',
-    result: 'SELECT snmpIfName FROM snmpInterface WHERE nodeid = ?::integer AND snmpifindex = ?::integer',
-    values: [
-      Opennms::Cookbook::Translations::TranslationValue.new(type: 'field', name: 'nodeid', matches: '.*', result: '${0}'),
-      Opennms::Cookbook::Translations::TranslationValue.new(type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}')
-    ]
-  )
+  type: 'constant',
+  result: 'uei.opennms.org/translatedUei'
+)
+value2 = Opennms::Cookbook::Translations::TranslationValue.new(
+  type: 'constant',
+  result: 'Major'
+)
+value3 = Opennms::Cookbook::Translations::TranslationValue.new(
+  type: 'expression',
+  result: 'Issue detected on ${ifName}: ${alarmText}'
+)
+value4 = Opennms::Cookbook::Translations::TranslationValue.new(
+  type: 'sql',
+  result: 'SELECT snmpIfName FROM snmpInterface WHERE nodeid = ?::integer AND snmpifindex = ?::integer',
+  values: [
+    Opennms::Cookbook::Translations::TranslationValue.new(type: 'field', name: 'nodeid', matches: '.*', result: '${0}'),
+    Opennms::Cookbook::Translations::TranslationValue.new(type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}'),
+  ]
+)
 
-  # Creating translation assignments
-  assignment1 = Opennms::Cookbook::Translations::TranslationAssignment.new(
-    name: 'uei',
-    type: 'field',
-    value: value1
-  )
-  assignment2 = Opennms::Cookbook::Translations::TranslationAssignment.new(
-    name: 'severity',
-    type: 'field',
-    value: value2
-  )
-  assignment3 = Opennms::Cookbook::Translations::TranslationAssignment.new(
-    name: 'description',
-    type: 'field',
-    value: value3
-  )
-  assignment4 = Opennms::Cookbook::Translations::TranslationAssignment.new(
-    name: 'ifName',
-    type: 'parameter',
-    default: 'Unknown',
-    value: value4
-  )
+assignment1 = Opennms::Cookbook::Translations::TranslationAssignment.new(
+  name: 'uei',
+  type: 'field',
+  value: value1
+)
+assignment2 = Opennms::Cookbook::Translations::TranslationAssignment.new(
+  name: 'severity',
+  type: 'field',
+  value: value2
+)
+assignment3 = Opennms::Cookbook::Translations::TranslationAssignment.new(
+  name: 'description',
+  type: 'field',
+  value: value3
+)
+assignment4 = Opennms::Cookbook::Translations::TranslationAssignment.new(
+  name: 'ifName',
+  type: 'parameter',
+  default: 'Unknown',
+  value: value4
+)
 
-  # Creating the mapping
-  mapping = Opennms::Cookbook::Translations::TranslationMapping.new(
-    assignments: [assignment1, assignment2, assignment3, assignment4]
-  )
+mapping = Opennms::Cookbook::Translations::TranslationMapping.new(
+  assignments: [assignment1, assignment2, assignment3, assignment4]
+)
 
-  # Creating the spec
-  spec = Opennms::Cookbook::Translations::TranslationSpec.new(
-    uei: 'uei.opennms.org/fakeUei2'
-  )
-  spec.mappings.push(mapping)
+spec = Opennms::Cookbook::Translations::TranslationSpec.new(
+  uei: 'uei.opennms.org/fakeUei2'
+)
+spec.mappings.push(mapping)
 
 opennms_translation_specs 'adding second spec' do
   specs [spec]
