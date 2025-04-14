fb904b186a0bc4e204eb15ecc59c4f4704f3c8b8
ssgayathri <sgayatri@onec1.com>
2025-04-14 15:08:23 +0530

    Update translation_specs_spec.rb

--- test/integration/translation_specs/controls/translation_specs_spec.rb
+++ test/integration/translation_specs/controls/translation_specs_spec.rb
@@ -1,39 +1,29 @@
 control 'translation_specs' do
-  #    <event-translation-spec uei='uei.opennms.org/internal/telemetry/clockSkewDetected'>
-  #      <mappings>
-  #        <mapping>
-  #          <assignment name='uei' type='field'>
-  #            <value result='uei.opennms.org/translator/telemetry/clockSkewDetected' type='constant'/>
-  #          </assignment>
-  #          <assignment name='nodeid' type='field'>
-  #            <value result='SELECT n.nodeid FROM node n, ipinterface i WHERE n.nodeid = i.nodeid AND i.ipaddr = ? AND n.location = ?' type='sql'>
-  #              <value matches='.*' name='interface' result='${0}' type='field'/>              <value matches='.*' name='monitoringSystemLocation' result='${0}' type='parameter'/>            </value>
-  #          </assignment>
-  #        </mapping>
-  #      </mappings>
-  #    </event-translation-spec>
   describe translation_spec('uei.opennms.org/internal/telemetry/clockSkewDetected', [
     {
       assignments: [
-      {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/translator/telemetry/clockSkewDetected',
+        {
+          name: 'uei',
+          type: 'field',
+          value: {
+            type: 'constant',
+            result: 'uei.opennms.org/translator/telemetry/clockSkewDetected'
+          }
         },
-      },
-      {
-        name: 'nodeid',
-        type: 'field',
-        value: {
-          type: 'sql',
-          result: 'SELECT n.nodeid FROM node n, ipinterface i WHERE n.nodeid = i.nodeid AND i.ipaddr = ? AND n.location = ?',
-          values: [{ type: 'field', name: 'interface', matches: '.*', result: '${0}' },
-                   { type: 'parameter', result: '${0}', name: 'monitoringSystemLocation', matches: '.*' }],
-        },
-      }],
-    },
+        {
+          name: 'nodeid',
+          type: 'field',
+          value: {
+            type: 'sql',
+            result: 'SELECT n.nodeid FROM node n, ipinterface i WHERE n.nodeid = i.nodeid AND i.ipaddr = ? AND n.location = ?',
+            values: [
+              { type: 'field', name: 'interface', matches: '.*', result: '${0}' },
+              { type: 'parameter', name: 'monitoringSystemLocation', matches: '.*', result: '${0}' }
+            ]
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
@@ -46,8 +36,8 @@ control 'translation_specs' do
           type: 'field',
           value: {
             type: 'constant',
-            result: 'uei.opennms.org/translator/traps/SNMP_Link_Down',
-          },
+            result: 'uei.opennms.org/translator/traps/SNMP_Link_Down'
+          }
         },
         {
           name: 'ifDescr',
@@ -58,9 +48,9 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfDescr FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
         },
         {
           name: 'ifName',
@@ -71,9 +61,9 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfName FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
         },
         {
           name: 'ifAlias',
@@ -84,11 +74,11 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfAlias FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
-        },
-      ],
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
+        }
+      ]
     },
     {
       assignments: [
@@ -97,11 +87,11 @@ control 'translation_specs' do
           type: 'field',
           value: {
             type: 'constant',
-            result: 'uei.opennms.org/internal/topology/linkDown',
-          },
-        },
-      ],
-    },
+            result: 'uei.opennms.org/internal/topology/linkDown'
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
@@ -114,8 +104,8 @@ control 'translation_specs' do
           type: 'field',
           value: {
             type: 'constant',
-            result: 'uei.opennms.org/translator/traps/SNMP_Link_Up',
-          },
+            result: 'uei.opennms.org/translator/traps/SNMP_Link_Up'
+          }
         },
         {
           name: 'ifDescr',
@@ -126,9 +116,9 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfDescr FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
         },
         {
           name: 'ifName',
@@ -139,9 +129,9 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfName FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
         },
         {
           name: 'ifAlias',
@@ -152,11 +142,11 @@ control 'translation_specs' do
             result: 'SELECT snmp.snmpIfAlias FROM snmpInterface snmp WHERE snmp.nodeid = ?::integer AND snmp.snmpifindex = ?::integer',
             values: [
               { type: 'field', name: 'nodeid', matches: '.*', result: '${0}' },
-              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' },
-            ],
-          },
-        },
-      ],
+              { type: 'parameter', name: '~^\\.1\\.3\\.6\\.1\\.2\\.1\\.2\\.2\\.1\\.1\\.([0-9]*)$', matches: '.*', result: '${0}' }
+            ]
+          }
+        }
+      ]
     },
     {
       assignments: [
@@ -165,145 +155,126 @@ control 'translation_specs' do
           type: 'field',
           value: {
             type: 'constant',
-            result: 'uei.opennms.org/internal/topology/linkUp',
-          },
-        },
-      ],
-    },
+            result: 'uei.opennms.org/internal/topology/linkUp'
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
 
   describe translation_spec('uei.opennms.org/external/hyperic/alert', [
     {
-      assignments: {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/internal/translator/hypericAlert',
+      assignments: [
+        {
+          name: 'uei',
+          type: 'field',
+          value: {
+            type: 'constant',
+            result: 'uei.opennms.org/internal/translator/hypericAlert'
+          }
         },
-      },
-      {
-        name: 'nodeid',
-        type: 'field',
-        value: {
-          type: 'sql',
-          result: 'SELECT n.nodeid FROM node n WHERE n.foreignid = ? AND n.foreignsource = ?',
-          values: [
-            { type: 'parameter', name: 'platform.id', matches: '.*', result: '${0}' },
-            { type: 'parameter', name: 'alert.source', matches: '.*', result: '${0}' },
-          ],
-        },
-      },
-    },
+        {
+          name: 'nodeid',
+          type: 'field',
+          value: {
+            type: 'sql',
+            result: 'SELECT n.nodeid FROM node n WHERE n.foreignid = ? AND n.foreignsource = ?',
+            values: [
+              { type: 'parameter', name: 'platform.id', matches: '.*', result: '${0}' },
+              { type: 'parameter', name: 'alert.source', matches: '.*', result: '${0}' }
+            ]
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
 
   describe translation_spec('uei.opennms.org/vendor/Cisco/traps/ciscoConfigManEvent', [
     {
-      assignments: {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/internal/translator/entityConfigChanged',
+      assignments: [
+        {
+          name: 'uei',
+          type: 'field',
+          value: {
+            type: 'constant',
+            result: 'uei.opennms.org/internal/translator/entityConfigChanged'
+          }
         },
-      },
-      {
-        name: 'configSource',
-        type: 'parameter',
-        value: {
-          values: [
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.9\.9\.43\.1\.1\.6\.1\.3\..*', matches: '.*', result: '${0}' },
-          ],
+        {
+          name: 'configSource',
+          type: 'parameter',
+          value: {
+            values: [
+              { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.9\.9\.43\.1\.1\.6\.1\.3\..*', matches: '.*', result: '${0}' }
+            ]
+          }
         },
-      },
-      {
-        name: 'configUser',
-        type: 'parameter',
-        value: {
-          type: 'constant',
-          result: 'Unknown',
-        },
-      },
-    },
+        {
+          name: 'configUser',
+          type: 'parameter',
+          value: {
+            type: 'constant',
+            result: 'Unknown'
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
 
   describe translation_spec('uei.opennms.org/vendor/Juniper/traps/jnxCmCfgChange', [
     {
-      assignments: {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/internal/translator/entityConfigChanged',
+      assignments: [
+        {
+          name: 'uei',
+          type: 'field',
+          value: {
+            type: 'constant',
+            result: 'uei.opennms.org/internal/translator/entityConfigChanged'
+          }
         },
-      },
-      {
-        name: 'configSource',
-        type: 'parameter',
-        value: {
-          values: [
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.4\..*', matches: '.*', result: '${0}' },
-          ],
+        {
+          name: 'configSource',
+          type: 'parameter',
+          value: {
+            values: [
+              { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.4\..*', matches: '.*', result: '${0}' }
+            ]
+          }
         },
-      },
-      {
-        name: 'configUser',
-        type: 'parameter',
-        value: {
-          values: [
-            { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.5\..*', matches: '.*', result: '${0}' },
-          ],
-        },
-      },
-    },
-  ]) do
-    it { should exist }
-  end
-
-  describe translation_spec('uei.opennms.org/internal/telemetry/clockSkewDetected', [
-    {
-      assignments: {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/translator/telemetry/clockSkewDetected',
-        },
-      },
-      {
-        name: 'nodeid',
-        type: 'field',
-        value: {
-          type: 'sql',
-          result: 'SELECT n.nodeid FROM node n, ipinterface i WHERE n.nodeid = i.nodeid AND i.ipaddr = ? AND n.location = ?',
-          values: [
-            { type: 'field', name: 'interface', matches: '.*', result: '${0}' },
-            { type: 'parameter', name: 'monitoringSystemLocation', matches: '.*', result: '${0}' },
-          ],
-        },
-      },
-    },
+        {
+          name: 'configUser',
+          type: 'parameter',
+          value: {
+            values: [
+              { type: 'parameter', name: '~^\.1\.3\.6\.1\.4\.1\.2636\.3\.18\.1\.7\.1\.5\..*', matches: '.*', result: '${0}' }
+            ]
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
 
   describe translation_spec('uei.opennms.org/fakeUei', [
     {
-      assignments: {
-        name: 'uei',
-        type: 'field',
-        value: {
-          type: 'constant',
-          result: 'uei.opennms.org/translatedUei',
-        },
-      },
-    },
+      assignments: [
+        {
+          name: 'uei',
+          type: 'field',
+          value: {
+            type: 'constant',
+            result: 'uei.opennms.org/translatedUei'
+          }
+        }
+      ]
+    }
   ]) do
     it { should exist }
   end
