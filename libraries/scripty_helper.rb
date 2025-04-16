b03cea6ee18b38b7f1bea031879bf04c4050678a
ssgayathri <sgayatri@onec1.com>
2025-04-16 21:14:18 +0530

    Update scripty_helper.rb

--- libraries/scripty_helper.rb
+++ libraries/scripty_helper.rb
@@ -1,10 +1,66 @@
 module Opennms
   module Cookbook
     module Scripty
+      module ScriptTemplate
+        def scripty_resource_init
+          scripty_resource_create unless scripty_resource_exist?
+        end
+
+        def scripty_resource
+          return unless scripty_resource_exist?
+          find_resource!(:template, "#{onms_etc}/scriptd-configuration.xml")
+        end
+
+        private
+
+        def scripty_resource_exist?
+          !find_resource(:template, "#{onms_etc}/scriptd-configuration.xml").nil?
+        rescue Chef::Exceptions::ResourceNotFound
+          false
+        end
+
+        def scripty_resource_create
+          file = Opennms::Cookbook::Scripty::ScriptyConfigurationFile.read("#{onms_etc}/scriptd-configuration.xml")
+          with_run_context(:root) do
+            declare_resource(:template, "#{onms_etc}/scriptd-configuration.xml") do
+              cookbook 'opennms'
+              source 'scriptd-configuration.xml.erb'
+              owner node['opennms']['username']
+              group node['opennms']['groupname']
+              mode '0664'
+              variables(config: file)
+              action :nothing
+              delayed_action :create
+            end
+          end
+        end
+      end
+      
+      class ScriptyConfigurationFile
+        include Opennms::XmlHelper
+        attr_reader :scripts
+
+        def initialize(scripts = nil)
+          @scripts = scripts
+        end
+
+        def read!(file = 'scriptd-configuration.xml')
+          raise ArgumentError, "File #{file} does not exist" unless ::File.exist?(file)
+          doc = xmldoc_from_file(file)
+          doc.root.each_element('/scriptd-configuration') do |scriptd_configuration|
+        end
+
+        def self.read(file = 'scriptd-configuration.xml')
+          scriptedfile = ScriptyConfigurationFile.new
+          scriptedfile.read!(file)
+          scriptedfile
+        end
+
+
       class ScriptdConfig
         attr_reader :engine, :start_script, :stop_script, :reload_script, :event_script"
 
-        def initialize(engine: nil, start_script: nil, stop_script: nil, reload_script: nil, event_script: nil)
+        def initialize(engine: nil, start_script: nil, stop_script: nil, reload_script: nil, event_script: nil, transactional: nil)
           @engine = if engine.nil?
                            []
                          else
