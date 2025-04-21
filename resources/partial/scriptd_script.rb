e70a223de7c29b471efa89d9eecb4bb5f970b020
David Schlenk <dschlenk@convergeone.com>
2025-04-21 11:22:45 -0500

    Update and rename _scriptd_script.rb to scriptd_script.rb

--- resources/partial/scriptd_script.rb
+++ resources/partial/scriptd_script.rb
@@ -4,6 +4,7 @@ unified_mode true
 property :script_name, String, name_property: true
 property :language, String, required: true
 property :script, String
+property :type, String, equal_to: ['start', 'stop', 'reload', 'event'], default: 'event'
 
 action_class do
   include Opennms::XmlHelper
