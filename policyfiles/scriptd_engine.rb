485363384a51a82186bdf51ca5d48da25ca88efd
ssgayathri <sgayatri@onec1.com>
2025-04-23 19:20:00 +0530

    Create scriptd_engine.rb

---
+++ policyfiles/scriptd_engine.rb
@@ -0,0 +1,6 @@
+name 'opennms_scriptd_engine'
+default_source :supermarket
+run_list 'opennms::postgres', 'openjdk17', 'opennms', 'opennms_resource_tests::scriptd_engine'
+cookbook 'opennms', path: '../'
+cookbook 'openjdk17', path: '../test/fixtures/cookbooks/openjdk17'
+cookbook 'opennms_resource_tests', path: '../test/fixtures/cookbooks/opennms_resource_tests'
