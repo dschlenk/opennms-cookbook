991c541279d00d51a48730665c550f4b7ac64a99
David Schlenk <dschlenk@convergeone.com>
2025-04-25 10:59:57 -0500

    Update and rename script.rb to scriptd_engine.rb
    implement scriptd_engine custom inspec resource

---
+++ test/integration/default/libraries/scriptd_engine.rb
@@ -0,0 +1,32 @@
+require 'rexml/document'
+class ScriptdEngine < Inspec.resource(1)
+  name 'scriptd_engine'
+
+  desc '
+    Custom InSpec resource to validate OpenNMS scriptd engine in config file
+  '
+  example '
+      describe scriptd_engine(\'beanshell\') do
+        it { should_exist }
+        its(\'class_name\') { should eq \'bsh.util.BeanShellBSFEngine\' }
+        its(\'extensions\') { should eq \'bsh\' }
+      end
+  '
+  def initialize(language)
+    @exists = false
+    @class_name = nil
+    @extensions = nil
+    doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
+    puts doc # TODO: remove once working
+    lang = doc.root.elements["/scriptd-configuration/engine[@language = '#{language}']"]
+    unless lang.nil?
+      @exists = true
+      @class_name = lang.attributes['className']
+      @extensions = lang.attributes['extensions']
+    end
+  end
+
+  def exist?
+    @exists
+  end
+end
