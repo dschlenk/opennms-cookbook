e2bdfdd7cfc151cc2388000e4185b07c299085b9
ssgayathri <sgayatri@onec1.com>
2025-05-01 16:29:52 +0530

    Update scriptd_engine.rb

--- resources/scriptd_engine.rb
+++ resources/scriptd_engine.rb
@@ -14,20 +14,20 @@ load_current_value do |new_resource|
     ro_scriptd_resource_init
     config = ro_scriptd_resource.variables[:config]
   end
-  engines = config.engine.select { |e| e[:language].eql?(new_resource.language) }
+  engines = config.engine.select { |e| e.language == new_resource.language }
   current_value_does_not_exist! if engines.empty?
   raise DuplicateEngines unless engines.one?
-  engine = engines.pop
-  class_name engine[:class_name]
-  extensions engine[:extensions]
+  engine = engines.first
+  class_name engine.class_name
+  extensions engine.extensions
 end
 
 action :create do
   converge_if_changed do
     scriptd_resource_init
     config = scriptd_resource.variables[:config]
-    engines = config.engine.find { |e| e.language == new_resource.language }
-    raise "Engine for language '#{new_resource.language}' already exists" unless engines.nil? || engines.empty?
+    engine = config.engine.find { |e| e.language == new_resource.language }
+    raise "Engine for language '#{new_resource.language}' already exists" unless engine.nil?
 
     config.add_engine(
       language: new_resource.language,
@@ -40,36 +40,33 @@ end
 action :create_if_missing do
   scriptd_resource_init
   config = scriptd_resource.variables[:config]
-  engines = config.engine.find { |e| e.language == new_resource.language }
-  run_action(:create) if engines.nil? || engines.empty?
+  engine = config.engine.find { |e| e.language == new_resource.language }
+  run_action(:create) if engine.nil?
 end
 
 action :update do
   scriptd_resource_init
   config = scriptd_resource.variables[:config]
-  engines = config.engine.find { |e| e.language == new_resource.language }
+  engine = config.engine.find { |e| e.language == new_resource.language }
 
-  if engines.nil? || engines.empty?
+  if engine.nil?
     raise Chef::Exceptions::ResourceNotFound,
           "No engine named #{new_resource.language} found to update. Use the `:create` or `:create_if_missing` actions to create a new engine."
   else
-    # If engines is an array of one engine, update that engine directly.
-    engines.first.update(
-      language: new_resource.language,
-      className: new_resource.class_name,
-      extensions: new_resource.extensions
-    )
+    engine.language = new_resource.language
+    engine.class_name = new_resource.class_name
+    engine.extensions = new_resource.extensions
   end
 end
 
 action :delete do
   scriptd_resource_init
   config = scriptd_resource.variables[:config]
-  engines = config.engine.find { |e| e.language == new_resource.language }
+  engine = config.engine.find { |e| e.language == new_resource.language }
 
-  unless engines.nil? || engines.empty?
-    converge_by "Removing engine #{engines}." do
-      config.delete_engine(engines)
+  unless engine.nil?
+    converge_by "Removing engine #{engine.language}." do
+      config.delete_engine(engine)
     end
   end
 end
