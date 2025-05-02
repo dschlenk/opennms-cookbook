79c189312358416a000b446ef2a274b0a6640787
ssgayathri <sgayatri@onec1.com>
2025-05-02 20:28:23 +0530

    Update scriptd_engine.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_engine.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_engine.rb
@@ -1,9 +1,9 @@
 opennms_scriptd_engine 'groovy' do
-  class_name 'org.gradle.tasks.build.CompileTaskHandler'
+  className 'org.gradle.tasks.build.CompileTaskHandler'
   extensions 'groovy'
 end
 
 opennms_scriptd_engine 'java' do
-  class_name 'com.game.core.physics.CollisionManagerr'
+  className 'com.game.core.physics.CollisionManagerr'
   extensions 'java'
 end
