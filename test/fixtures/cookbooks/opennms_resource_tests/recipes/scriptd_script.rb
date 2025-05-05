78cdba79200b050fce7bb8ae8b2e5a93ed070077
ssgayathri <sgayatri@onec1.com>
2025-05-05 19:29:21 +0530

    Update scriptd_script.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
@@ -1,8 +1,10 @@
 opennms_scriptd_script 'beanshell' do
+  language 'beanshell'
   script ''
 end
 
 opennms_scriptd_script 'groovy' do
+  language 'groovy'
   uei 'uei.opennms.org/cheftest/thresholdExceeded'
   script ''
 end
