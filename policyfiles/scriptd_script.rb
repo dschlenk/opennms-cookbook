39d17d1fd0a2d20923eb8db8e595821c96f47f81
David Schlenk <dschlenk@convergeone.com>
2025-05-08 10:52:24 -0500

    Delete policyfiles/scriptd_script.rb

--- policyfiles/scriptd_script.rb
+++
@@ -1,6 +0,0 @@
-name 'opennms_scriptd_script'
-default_source :supermarket
-run_list 'opennms::postgres', 'openjdk17', 'opennms', 'opennms_resource_tests::scriptd_script'
-cookbook 'opennms', path: '../'
-cookbook 'openjdk17', path: '../test/fixtures/cookbooks/openjdk17'
-cookbook 'opennms_resource_tests', path: '../test/fixtures/cookbooks/opennms_resource_tests'
