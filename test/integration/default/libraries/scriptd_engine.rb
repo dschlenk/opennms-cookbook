da6d5c23186747aaf506d59c9b88acfc8f11efe9
ssgayathri <sgayatri@onec1.com>
2025-05-09 16:04:06 +0530

    Update scriptd_engine.rb

--- test/integration/default/libraries/scriptd_engine.rb
+++ test/integration/default/libraries/scriptd_engine.rb
@@ -1,23 +1,25 @@
 require 'rexml/document'
+
 class ScriptdEngine < Inspec.resource(1)
   name 'scriptd_engine'
 
   desc '
     Custom InSpec resource to validate OpenNMS scriptd engine in config file
   '
+
   example '
       describe scriptd_engine(\'beanshell\') do
-        it { should_exist }
+        it { should exist }
         its(\'class_name\') { should eq \'bsh.util.BeanShellBSFEngine\' }
         its(\'extensions\') { should eq \'bsh\' }
       end
   '
+
   def initialize(language)
     @exists = false
     @class_name = nil
     @extensions = nil
     doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
-    puts doc # TODO: remove once working
     lang = doc.root.elements["/scriptd-configuration/engine[@language = '#{language}']"]
     unless lang.nil?
       @exists = true
@@ -28,5 +30,13 @@ class ScriptdEngine < Inspec.resource(1)
 
   def exist?
     @exists
+  end
+
+  def class_name
+    @class_name
+  end
+
+  def extensions
+    @extensions
   end
 end
