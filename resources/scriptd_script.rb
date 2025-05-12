48902704c017e4a7a361295bcd246019a70b114b
David Schlenk <dschlenk@convergeone.com>
2025-05-12 11:27:17 -0500

    Update scriptd_script.rb

--- resources/scriptd_script.rb
+++ resources/scriptd_script.rb
@@ -12,56 +12,69 @@ action_class do
 end
 
 load_current_value do |new_resource|
-  config = ::Opennms::Cookbook::Scriptd::ScriptdConfigurationFile.read("#{onms_etc}/scriptd-configuration.xml")
-
-  unless [config.config.start_script, config.config.stop_script, config.config.reload_script, config.config.event_script].any? do |script_list|
-    script_list.any? { |script| script.language == new_resource.language && script.script.include?(new_resource.script_name) }
+  template = scriptd_resource
+  if template.nil?
+    ro_scriptd_resource_init
+    template = ro_scriptd_resource
   end
-    current_value_does_not_exist!
+  config = template.variables[:config]
+  case new_resource.type
+    when 'start'
+      current_value_does_not_exist? if config.start_script.select { |ss| ss.language.eql?(new_resource.language) && ss.script.eql?(new_resource.script) }.one?
+    when 'stop'
+      # TODO: implement similar to 'start'
+    when 'reload'
+      # TODO: implement similar to 'start'
+    when 'event'
+      # TODO: implement similar to 'start' but also taking into account the `uei` field, which can be an array or a string (easiest to coerce it to a single avlue array if it is a string)
   end
-
-  script config.config.event_script.find { |script| script.language == new_resource.language }&.script
+  # since changing isn't supported, we either didn't find it above and already exited, or we did, and now we make the current value match the new value (since it does)
   language new_resource.language
+  script new_resource.script
+  uei new_resource.uei
 end
 
 action :add do
-  config = ::Opennms::Cookbook::Scriptd::ScriptdConfigurationFile.read("#{onms_etc}/scriptd-configuration.xml")
-  return if config.nil?
-
-  if [config.config.start_script, config.config.stop_script, config.config.reload_script, config.config.event_script].any? do |script_list|
-    script_list.any? { |script| script.language == new_resource.language && script.script.include?(new_resource.script_name) }
-  end
-    Chef::Log.info("Script '#{new_resource.script_name}' already exists.")
-  else
-    converge_by("Adding script '#{new_resource.script_name}'") do
-      config.config.add_script(
+  converge_if_changed do
+    scriptd_resource_init
+    template = scriptd_resource
+    config = template.variables[:config]
+    config.add_script(
         language: new_resource.language,
         script: new_resource.script,
         type: new_resource.type,
         uei: new_resource.uei
-      )
-      config.config.write("#{onms_etc}/scriptd-configuration.xml")
-      Chef::Log.info("Script '#{new_resource.script_name}' added.")
-    end
+    )
   end
 end
 
 action :delete do
-  config = ::Opennms::Cookbook::Scriptd::ScriptdConfigurationFile.read("#{onms_etc}/scriptd-configuration.xml")
-  return if config.nil?
-
-  if [config.config.start_script, config.config.stop_script, config.config.reload_script, config.config.event_script].any? do |script_list|
-    script_list.any? { |script| script.language == new_resource.language && script.script.include?(new_resource.script_name) }
+  template = scriptd_resource
+  if template.nil?
+    ro_scriptd_resource_init
+    template = ro_scriptd_resource
   end
-    converge_by("Deleting script '#{new_resource.script_name}'") do
-      config.config.delete_script(
-        name: new_resource.script_name,
-        language: new_resource.language
-      )
-      config.config.write("#{onms_etc}/scriptd-configuration.xml")
-      Chef::Log.info("Script '#{new_resource.script_name}' deleted.")
-    end
-  else
-    Chef::Log.info("Script '#{new_resource.script_name}' not found.")
+  config = template.variables[:config]
+  case new_resource.type
+    when 'start'
+      if config.start_script.select { |ss| ss.language.eql?(new_resource.language) && ss.script.eql?(new_resource.script) }.one?
+        converge_by("Deleting script '#{new_resource.script_name}'") do
+          scriptd_resource_init
+          template = scriptd_resource
+          config = template.variables[:config]
+          config.delete_script( # TODO: implement this method in the library file
+            language: new_resource.language,
+            script: new_resource.script,
+            type: new_resource.type,
+            uei: new_resource.type
+          )
+        end
+      end
+    when 'stop'
+      # TODO: implement similar to 'start'
+    when 'reload'
+      # TODO: implement similar to 'start'
+    when 'event'
+      # TODO: implement similar to 'start' but also taking into account the `uei` field, which can be an array or a string (easiest to coerce it to a single avlue array if it is a string)
   end
 end
