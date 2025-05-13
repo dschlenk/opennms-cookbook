bffa70a8c6cbc44168775799c8f5a1737e2abdc9
ssgayathri <sgayatri@onec1.com>
2025-05-13 17:17:00 +0530

    Update scriptd_script.rb

--- resources/scriptd_script.rb
+++ resources/scriptd_script.rb
@@ -23,13 +23,10 @@ load_current_value do |new_resource|
     current_value_does_not_exist? if config.start_script.select { |ss| ss.language.eql?(new_resource.language) && ss.script.eql?(new_resource.script) }.one?
   when 'stop'
     current_value_does_not_exist? if config.stop_script.select { |s| s.language.eql?(new_resource.language) && s.script.eql?(new_resource.script) }.one?
-    # TODO: implement similar to 'start'
   when 'reload'
     current_value_does_not_exist? if config.reload_script.select { |rs| rs.language.eql?(new_resource.language) && rs.script.eql?(new_resource.script) }.one?
-    # TODO: implement similar to 'start'
   when 'event'
     current_value_does_not_exist? if config.event_script.select { |es| es.language.eql?(new_resource.language) && es.script.eql?(new_resource.script) && Array(es.uei) == Array(new_resource.uei) }.one?
-    # TODO: implement similar to 'start' but also taking into account the `uei` field, which can be an array or a string (easiest to coerce it to a single value array if it is a string)
   end
   # since changing isn't supported, we either didn't find it above and already exited, or we did, and now we make the current value match the new value (since it does)
   language new_resource.language
@@ -65,7 +62,7 @@ action :delete do
         scriptd_resource_init
         template = scriptd_resource
         config = template.variables[:config]
-        config.delete_script( # TODO: implement this method in the library file
+        config.delete_script(
           language: new_resource.language,
           script: new_resource.script,
           type: new_resource.type,
@@ -79,7 +76,7 @@ action :delete do
         scriptd_resource_init
         template = scriptd_resource
         config = template.variables[:config]
-        config.delete_script( # TODO: implement this method in the library file
+        config.delete_script(
           language: new_resource.language,
           script: new_resource.script,
           type: new_resource.type,
@@ -87,14 +84,13 @@ action :delete do
         )
       end
     end
-    # TODO: implement similar to 'start'
   when 'reload'
     if config.reload_script.select { |rs| rs.language.eql?(new_resource.language) && rs.script.eql?(new_resource.script) }.one?
       converge_by("Deleting script '#{new_resource.script_name}'") do
         scriptd_resource_init
         template = scriptd_resource
         config = template.variables[:config]
-        config.delete_script( # TODO: implement this method in the library file
+        config.delete_script(
           language: new_resource.language,
           script: new_resource.script,
           type: new_resource.type,
@@ -102,14 +98,13 @@ action :delete do
         )
       end
     end
-    # TODO: implement similar to 'start'
   when 'event'
     if config.event_script.select { |es| es.language.eql?(new_resource.language) && es.script.eql?(new_resource.script) && es.uei.eql?(new_resource.uei) }.one?
       converge_by("Deleting script '#{new_resource.script_name}'") do
         scriptd_resource_init
         template = scriptd_resource
         config = template.variables[:config]
-        config.delete_script( # TODO: implement this method in the library file
+        config.delete_script(
           language: new_resource.language,
           script: new_resource.script,
           type: new_resource.type,
@@ -117,6 +112,5 @@ action :delete do
         )
       end
     end
-    # TODO: implement similar to 'start' but also taking into account the `uei` field, which can be an array or a string (easiest to coerce it to a single value array if it is a string)
   end
 end
