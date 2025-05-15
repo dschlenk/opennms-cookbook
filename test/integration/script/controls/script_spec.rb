91a6adb73c16642d947a5dd1538b8e0a4b3fcf59
ssgayathri <sgayatri@onec1.com>
2025-05-15 16:46:58 +0530

    Update script_spec.rb

--- test/integration/script/controls/script_spec.rb
+++ test/integration/script/controls/script_spec.rb
@@ -17,23 +17,28 @@ control 'script' do
     its('extensions') { should eq 'java' }
   end
 
-  describe scriptd_script('beanshell', 'start', "log = bsf.lookupBean(\"log\");\nlog.debug(\"start-script\");\nlog.debug(\"start-script too\");") do
+  describe scriptd_engine('beanshell') do
     it { should exist }
+    its('type') { should eq 'start' }
+    its('script') { should eq 'bsf.lookupBean("log")' }
   end
 
-  describe scriptd_script('beanshell', 'stop', "log = bsf.lookupBean(\"log\");\nlog.debug(\"stop-script\");\nlog.debug(\"stop-script also\");\nlog.debug(\"another line but it is indented\");") do
+  describe scriptd_engine('groovy') do
     it { should exist }
+    its('type') { should eq 'stop' }
+    its('uei') { should eq 'uei.opennms.org/cheftest/thresholdExceeded' }
+    its('script') { should eq 'bsf.lookupBean("log")' }
   end
 
-  describe scriptd_script('jython', 'stop', '1 + 1') do
+  describe scriptd_engine('jython') do
     it { should exist }
+    its('type') { should eq 'reload' }
+    its('uei') { should eq 'uei.opennms.org/cheftest/thresholdExceeded' }
+    its('script') { should eq 'bsf.lookupBean("log")' }
   end
 
-  describe scriptd_script('beanshell', 'reload', "log = bsf.lookupBean(\"log\");\nlog.debug(\"reload-script also\");\nlog.debug(\"another reload line but it is indented and there is a blank line between this one and the last one\");") do
+  describe scriptd_engine('eventbeanshell') do
     it { should exist }
+    its('type') { should eq 'event' }
+    its('script') { should eq 'bsf.lookupBean("log")' }
   end
-
-  describe scriptd_script('beanshell', 'event', "log = bsf.lookupBean(\"log\");\nlog.debug(\"cpqHoTrapFlags: {}\", cpqHoTrapFlags);\n\nif (\"CRITICAL\".equalsIgnoreCase(trapType)) {\n  log.warn(\"HP device sent a CRITICAL trap!\");\n} else {\n  log.warn(\"Received HP trap that should have had varbind cpqHoTrapFlags in it but it did not.\");\n}\nlog.debug(\"cpqHoMibHealthStatusArray: {}\", cpqHoMibHealthStatusArray);\nif (cpqHoMibHealthStatusArray == null || cpqHoMibHealthStatusArray.length() < 38) {\n  log.warn(\"Received HP trap that should have had varbind cpqHoMibHealthStatusArray in it but it did not.\");\n}\nlog.debug(\"Sending new event: \\n{}\", org.opennms.core.xml.JaxbUtils.marshal(bldr.getEvent()));") do
-    it { should exist }
-  end
-end
