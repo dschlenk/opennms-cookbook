3a27784e5341266323405a884ef0c455bafadd3d
ssgayathri <sgayatri@onec1.com>
2025-05-15 17:22:46 +0530

    Update scriptd_script.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
@@ -1,21 +1,25 @@
 opennms_scriptd_script 'beanshell' do
+  language 'beanshell'
   type 'start'
   script 'bsf.lookupBean("log")'
 end
 
 opennms_scriptd_script 'groovy' do
+  language 'groovy'
   type 'stop'
   uei 'uei.opennms.org/cheftest/thresholdExceeded'
   script 'bsf.lookupBean("log")'
 end
 
 opennms_scriptd_script 'jython' do
+  language 'jython'
   type 'reload'
   uei 'uei.opennms.org/cheftest/thresholdExceeded'
   script 'bsf.lookupBean("log")'
 end
 
 opennms_scriptd_script 'beanshell' do
+  language 'beanshell'
   type 'event'
-  script 'log = bsf.lookupBean(\"log\")'
+  script 'log = bsf.lookupBean("log")'
 end
