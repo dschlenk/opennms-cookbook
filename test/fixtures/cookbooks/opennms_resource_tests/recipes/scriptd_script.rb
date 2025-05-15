58651c2282a339e0f77514f23008a17701850278
ssgayathri <sgayatri@onec1.com>
2025-05-15 16:46:52 +0530

    Update scriptd_script.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
@@ -1,24 +1,21 @@
 opennms_scriptd_script 'beanshell' do
   type 'start'
-  script "log = bsf.lookupBean(\"log\");\nlog.debug(\"start-script\");\nlog.debug(\"start-script too\");"
+  script 'bsf.lookupBean("log")'
 end
 
-opennms_scriptd_script 'beanshell' do
+opennms_scriptd_script 'groovy' do
   type 'stop'
-  script "log = bsf.lookupBean(\"log\");\nlog.debug(\"stop-script\");\nlog.debug(\"stop-script also\");\nlog.debug(\"another line but it is indented\");"
+  uei 'uei.opennms.org/cheftest/thresholdExceeded'
+  script 'bsf.lookupBean("log")'
 end
 
 opennms_scriptd_script 'jython' do
-  type 'stop'
-  script '1 + 1'
-end
-
-opennms_scriptd_script 'beanshell' do
   type 'reload'
-  script "log = bsf.lookupBean(\"log\");\nlog.debug(\"reload-script also\");\nlog.debug(\"another reload line but it is indented and there is a blank line between this one and the last one\");"
+  uei 'uei.opennms.org/cheftest/thresholdExceeded'
+  script 'bsf.lookupBean("log")'
 end
 
 opennms_scriptd_script 'beanshell' do
   type 'event'
-  script "log = bsf.lookupBean(\"log\");\nlog.debug(\"cpqHoTrapFlags: {}\", cpqHoTrapFlags);\n\nif (\"CRITICAL\".equalsIgnoreCase(trapType)) {\n  log.warn(\"HP device sent a CRITICAL trap!\");\n} else {\n  log.warn(\"Received HP trap that should have had varbind cpqHoTrapFlags in it but it did not.\");\n}\nlog.debug(\"cpqHoMibHealthStatusArray: {}\", cpqHoMibHealthStatusArray);\nif (cpqHoMibHealthStatusArray == null || cpqHoMibHealthStatusArray.length() < 38) {\n  log.warn(\"Received HP trap that should have had varbind cpqHoMibHealthStatusArray in it but it did not.\");\n}\nlog.debug(\"Sending new event: \\n{}\", org.opennms.core.xml.JaxbUtils.marshal(bldr.getEvent()));"
+  script 'log = bsf.lookupBean(\"log\")'
 end
