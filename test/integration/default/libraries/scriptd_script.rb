03933bc14b65e61d2bf9add477114c99e3f66a26
David Schlenk <dschlenk@convergeone.com>
2025-05-19 10:38:45 -0500

    Update scriptd_script.rb

--- test/integration/default/libraries/scriptd_script.rb
+++ test/integration/default/libraries/scriptd_script.rb
@@ -22,17 +22,24 @@ class ScriptdScript < Inspec.resource(1)
     @script = nil
 
     doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
-
-    script_el = doc.root.elements["/scriptd-configuration/#{type}-script[@language = '#{language}']"]
-    unless script_el.nil?
-      @script = script_el.texts.join("\n").strip
-      if @script == script.strip
-        if type == 'event' && ueis
-          ul = []
-          script_el.each_element('uei') { |u| ul << u.attributes['name'] }
-          @exists = ul.sort == ueis.sort
-        else
-          @exists = true
+    xpath = "/scriptd-configuration/#{type}-script[@language = '#{language}']"
+    puts "script xpath: #{xpath}"
+    se = doc.root.elements[xpath]
+    unless se.nil?
+      doc.each_element(xpath) do |script_el|
+        @script = script_el.texts.join("\n").strip
+        puts "checking @script for a match"
+        if @script == script.strip
+          puts "@script matches"
+          if type == 'event' && ueis
+            puts "is event, checking if ueis match"
+            ul = []
+            script_el.each_element('uei') { |u| ul << u.attributes['name'] }
+            puts "ul.sort == ueis.sort ? #{ul.sort} == #{ueis.sort}"
+            @exists = ul.sort == ueis.sort
+          else
+            @exists = true
+          end
         end
       end
     end
