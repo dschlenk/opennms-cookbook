339bc03d996aab35f22845fba36a57cb7ea26656
ssgayathri <sgayatri@onec1.com>
2025-05-19 22:20:46 +0530

    Update scriptd_script.rb

--- test/integration/default/libraries/scriptd_script.rb
+++ test/integration/default/libraries/scriptd_script.rb
@@ -26,17 +26,21 @@ class ScriptdScript < Inspec.resource(1)
     doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
     xpath = "/scriptd-configuration/#{type}-script[@language = '#{language}']"
     puts "script xpath: #{xpath}"
-    se = doc.root.elements[xpath]
-    unless se.nil?
-      doc.each_element(xpath) do |script_el|
+
+    unless doc.root.elements[xpath].nil?
+      doc.root.elements.each(xpath) do |script_el|
         @script = script_el.texts.join("\n").strip
         puts "checking #{@script} for a match"
         next unless @script == script.strip
         puts "#{@script} matches"
+
         if type == 'event' && ueis
           puts 'type is event, checking if ueis match'
           ul = []
-          script_el.each_element('uei') { |u| ul << u.attributes['name'] }
+          script_el.each_element('uei') do |u|
+            puts "Found uei: #{u.attributes['name']}"
+            ul << u.attributes['name']
+          end
           puts "ul.sort == ueis.sort ? #{ul.sort} == #{ueis.sort}"
           @exists = ul.sort == ueis.sort
         else
