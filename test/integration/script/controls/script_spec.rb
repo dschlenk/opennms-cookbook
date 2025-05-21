4a43053ae0876bee8fafd3169b1200db36bb18b1
ssgayathri <sgayatri@onec1.com>
2025-05-21 16:57:38 +0530

    Update script_spec.rb

--- test/integration/script/controls/script_spec.rb
+++ test/integration/script/controls/script_spec.rb
@@ -13,11 +13,11 @@ control 'script' do
 
   describe scriptd_engine('java') do
     it { should exist }
-    its('class_name') { should eq 'com.game.core.physics.CollisionManagerr' }
+    its('class_name') { should eq 'com.game.core.physics.CollisionManager' }
     its('extensions') { should eq 'java' }
   end
 
-  describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log")') do
+  describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log");') do
     it { should exist }
   end
 
@@ -45,11 +45,11 @@ control 'script' do
     it { should exist }
   end
 
-  describe scriptd_script('beanshell', 'event', 'log = bsf.lookupBean("log"); String uei = event.getUei(); String node = event.getParm("nodeLabel").getValue(); log.info("Received UEI: " + uei + " for node: " + node); if (uei.contains("nodeDown")) { log.warn("ALERT: Node " + node + " is DOWN!"); } else if (uei.contains("nodeUp")) { log.info("INFO: Node " + node + " is UP."); }', 'uei.opennms.org/cheftest/nodeDown,uei.opennms.org/cheftest/nodeUp') do
+  describe scriptd_script('beanshell', 'event', 'log = bsf.lookupBean("log"); String uei = event.getUei(); String node = event.getParm("nodeLabel").getValue(); log.info("Received UEI: " + uei + " for node: " + node); if (uei.contains("nodeDown")) { log.warn("ALERT: Node " + node + " is DOWN"); } else if (uei.contains("nodeUp")) { log.info("INFO: Node " + node + " is UP"); }', 'uei.opennms.org/cheftest/nodeDown,uei.opennms.org/cheftest/nodeUp') do
     it { should exist }
   end
 
-  describe scriptd_script('groovy', 'event', 'def log = bsf.lookupBean("log"); def uei = event.getUei(); def iface = event.getParm("interface")?.value ?: "unknown"; log.info("Event received: ${uei} for interface: ${iface}"); if (uei.contains("interfaceDown")) { log.error("Interface ${iface} is DOWN! ⚠️"); } else if (uei.contains("interfaceUp")) { log.info("Interface ${iface} is UP ✅"); }', 'uei.opennms.org/cheftest/interfaceDown,uei.opennms.org/cheftest/interfaceUp') do
+  describe scriptd_script('groovy', 'event', 'def log = bsf.lookupBean("log"); def uei = event.getUei(); def iface = event.getParm("interface")?.value ?: "unknown"; log.info("Event received: ${uei} for interface: ${iface}"); if (uei.contains("interfaceDown")) { log.error("Interface ${iface} is DOWN"); } else if (uei.contains("interfaceUp")) { log.info("Interface ${iface} is UP"); }', 'uei.opennms.org/cheftest/interfaceDown,uei.opennms.org/cheftest/interfaceUp') do
     it { should exist }
   end
 end
