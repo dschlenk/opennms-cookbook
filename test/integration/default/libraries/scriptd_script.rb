81d6e4ed2d0ac3aeda828ebf67bb2b2a75a3fc44
ssgayathri <sgayatri@onec1.com>
2025-05-27 17:04:34 +0530

    Update scriptd_script.rb

--- test/integration/default/libraries/scriptd_script.rb
+++ test/integration/default/libraries/scriptd_script.rb
@@ -1,34 +1,32 @@
 require 'rexml/document'
- 
+
 class ScriptdScript < Inspec.resource(1)
   name 'scriptd_script'
- 
+
   desc '
     Custom InSpec resource to validate OpenNMS scriptd scripts in config file
   '
- 
+
   example '
     describe scriptd_script("beanshell", "event", "log = bsf.lookupBean(\"log\")", ["uei.opennms.org/anUei"]) do
       it { should exist }
     end
   '
- 
+
   attr_reader :type, :uei, :script
- 
+
   def initialize(language, type, script, ueis = nil)
     @exists = false
     @type = type
     @script = nil
     @uei = ueis.is_a?(String) ? [ueis] : ueis
- 
+
     doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
     xpath = "/scriptd-configuration/#{type}-script[@language = '#{language}']"
     puts "script xpath: #{xpath}"
- 
+
     unless doc.root.elements[xpath].nil?
       doc.root.elements.each(xpath) do |script_el|
-        # this gives us valid XML text, but we want the value instead
-        # @script = script_el.texts.join("\n").strip
         @script = ''
         script_el.texts.each do |t|
           @script += t.value
@@ -37,7 +35,7 @@ class ScriptdScript < Inspec.resource(1)
         puts "checking #{@script} for a match"
         next unless @script == script.strip
         puts "#{@script} matches"
- 
+
         if type == 'event' && @uei
           puts 'type is event, checking if ueis match'
           ul = []
@@ -53,7 +51,7 @@ class ScriptdScript < Inspec.resource(1)
       end
     end
   end
- 
+
   def exist?
     @exists
   end
