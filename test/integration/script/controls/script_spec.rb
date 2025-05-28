56c6ac021c4406d9784987993ee34512e08e3fa0
ssgayathri <sgayatri@onec1.com>
2025-05-28 17:22:05 +0530

    Update script_spec.rb

--- test/integration/script/controls/script_spec.rb
+++ test/integration/script/controls/script_spec.rb
@@ -1,20 +1,20 @@
 control 'script' do
   describe scriptd_engine('beanshell') do
     it { should exist }
-    its('class_name') { should eq 'bsh.util.BeanShellBSFEngine' }
-    its('extensions') { should eq 'bsh' }
+    its('class_name') { should eq "bsh.util.BeanShellBSFEngine" }
+    its('extensions') { should eq "bsh" }
   end
 
   describe scriptd_engine('groovy') do
     it { should exist }
-    its('class_name') { should eq 'org.gradle.tasks.build.CompileTaskHandler' }
-    its('extensions') { should eq 'groovy' }
+    its('class_name') { should eq "org.gradle.tasks.build.CompileTaskHandler" }
+    its('extensions') { should eq "groovy" }
   end
 
   describe scriptd_engine('java') do
     it { should exist }
-    its('class_name') { should eq 'com.game.core.physics.CollisionManagerr' }
-    its('extensions') { should eq 'java' }
+    its('class_name') { should eq "com.game.core.physics.CollisionManagerr" }
+    its('extensions') { should eq "java" }
   end
 
   describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log");') do
@@ -37,7 +37,7 @@ control 'script' do
     it { should exist }
   end
 
-  describe scriptd_script('groovy', 'stop', 'def log = bsf.lookupBean("log"); def now = new Date(); log.info("Groovy stop script executed at: ${now}"); if (now.hours > 18) { log.warn("Script stopped after hours."); }') do
+  describe scriptd_script('groovy', 'stop', "def log = bsf.lookupBean(\"log\"); def now = new Date(); log.info(\"Groovy stop script executed at: \${now}\"); if (now.hours > 18) { log.warn(\"Script stopped after hours.\"); }") do
     it { should exist }
   end
 
