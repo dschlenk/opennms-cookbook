c300de49c1493797dd090f1990714f325d387102
ssgayathri <sgayatri@onec1.com>
2025-05-29 00:40:12 +0530

    Update scriptd_script.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/scriptd_script.rb
@@ -1,47 +1,56 @@
-control 'script' do
-  describe scriptd_engine('beanshell') do
-    it { should exist }
-    its('class_name') { should eq 'bsh.util.BeanShellBSFEngine' }
-    its('extensions') { should eq 'bsh' }
-  end
+opennms_scriptd_script 'beanshell-start' do
+  language 'beanshell'
+  type 'start'
+  script 'bsf.lookupBean("log");'
+end
 
-  describe scriptd_engine('groovy') do
-    it { should exist }
-    its('class_name') { should eq 'org.gradle.tasks.build.CompileTaskHandler' }
-    its('extensions') { should eq 'groovy' }
-  end
+opennms_scriptd_script 'groovy-stop' do
+  language 'groovy'
+  type 'stop'
+  script 'bsf.lookupBean("log")'
+end
 
-  describe scriptd_engine('java') do
-    it { should exist }
-    its('class_name') { should eq 'com.game.core.physics.CollisionManagerr' }
-    its('extensions') { should eq 'java' }
-  end
+opennms_scriptd_script 'java-reload' do
+  language 'java'
+  type 'reload'
+  script 'bsf.lookupBean("log")'
+end
 
-  describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log");') do
-    it { should exist }
-  end
+opennms_scriptd_script 'beanshell-event' do
+  language 'beanshell'
+  type 'event'
+  uei 'uei.opennms.org/cheftest/thresholdExceeded'
+  script 'bsf.lookupBean("log");'
+end
 
-  describe scriptd_script('groovy', 'stop', 'bsf.lookupBean("log")') do
-    it { should exist }
-  end
+opennms_scriptd_script 'beanshell-start-extended' do
+  language 'beanshell'
+  type 'start'
+  script "log = bsf.lookupBean(\"log\");\nlog.info(\"Beanshell start script initialized.\");\nString user = System.getProperty(\"user.name\");\nlog.info(\"Running as user: \" + user);"
+end
 
-  describe scriptd_script('java', 'reload', 'bsf.lookupBean("log")') do
-    it { should exist }
-  end
+opennms_scriptd_script 'groovy-stop-extended' do
+  language 'groovy'
+  type 'stop'
+  script 'def log = bsf.lookupBean("log"); def now = new Date(); log.info("Groovy stop script executed at: ${now}"); if (now.hours > 18) { log.warn("Script stopped after hours."); }'
+end
 
-  describe scriptd_script('beanshell', 'event', 'bsf.lookupBean("log");', 'uei.opennms.org/cheftest/thresholdExceeded') do
-    it { should exist }
-  end
+opennms_scriptd_script 'java-reload-extended' do
+  language 'java'
+  type 'reload'
+  script "log = bsf.lookupBean(\"log\");\ntry { log.info(\"Reloading Java script.\");\nString version = System.getProperty(\"java.version\");\nlog.info(\"Java version: \" + version);\n} catch (Exception e) {\nlog.error(\"Error during reload: \" + e.getMessage());\n}"
+end
 
-  describe scriptd_script('beanshell', 'start', 'log = bsf.lookupBean("log");\nlog.info("Beanshell start script initialized.");\nString user = System.getProperty("user.name");\nlog.info("Running as user: " + user);') do
-    it { should exist }
-  end
+opennms_scriptd_script 'beanshell-event-multiuei' do
+  language 'beanshell'
+  type 'event'
+  uei 'uei.opennms.org/cheftest/nodeDown,uei.opennms.org/cheftest/nodeUp'
+  script 'log = bsf.lookupBean("log"); String uei = event.getUei(); String node = event.getParm("nodeLabel").getValue(); log.info("Received UEI: " + uei + " for node: " + node); if (uei.contains("nodeDown")) { log.warn("ALERT: Node " + node + " is DOWN"); } else if (uei.contains("nodeUp")) { log.info("INFO: Node " + node + " is UP"); }'
+end
 
-  describe scriptd_script('groovy', 'stop', 'def log = bsf.lookupBean("log"); def now = new Date(); log.info("Groovy stop script executed at: ${now}"); if (now.hours > 18) { log.warn("Script stopped after hours."); }') do
-    it { should exist }
-  end
-
-  describe scriptd_script('java', 'reload', 'log = bsf.lookupBean("log");\ntry { log.info("Reloading Java script.");\nString version = System.getProperty("java.version");\nlog.info("Java version: " + version);\n}\ncatch (Exception e) {\nlog.error("Error during reload: " + e.getMessage());\n}') do
-    it { should exist }
-  end
+opennms_scriptd_script 'groovy-event-interface' do
+  language 'groovy'
+  type 'event'
+  uei 'uei.opennms.org/cheftest/interfaceDown,uei.opennms.org/cheftest/interfaceUp'
+  script 'def log = bsf.lookupBean("log"); def uei = event.getUei(); def iface = event.getParm("interface")?.value ?: "unknown"; log.info("Event received: ${uei} for interface: ${iface}"); if (uei.contains("interfaceDown")) { log.error("Interface ${iface} is DOWN"); } else if (uei.contains("interfaceUp")) { log.info("Interface ${iface} is UP"); }'
 end
