21778b3600fc00cd039e08df0be615a1ae8245eb
ssgayathri <sgayatri@onec1.com>
2025-05-28 17:24:48 +0530

    Update script_spec.rb

--- test/integration/script/controls/script_spec.rb
+++ test/integration/script/controls/script_spec.rb
@@ -1,20 +1,20 @@
 control 'script' do
   describe scriptd_engine('beanshell') do
     it { should exist }
-    its('class_name') { should eq "bsh.util.BeanShellBSFEngine" }
-    its('extensions') { should eq "bsh" }
+    its('class_name') { should eq 'bsh.util.BeanShellBSFEngine' }
+    its('extensions') { should eq 'bsh' }
   end
 
   describe scriptd_engine('groovy') do
     it { should exist }
-    its('class_name') { should eq "org.gradle.tasks.build.CompileTaskHandler" }
-    its('extensions') { should eq "groovy" }
+    its('class_name') { should eq 'org.gradle.tasks.build.CompileTaskHandler' }
+    its('extensions') { should eq 'groovy' }
   end
 
   describe scriptd_engine('java') do
     it { should exist }
-    its('class_name') { should eq "com.game.core.physics.CollisionManagerr" }
-    its('extensions') { should eq "java" }
+    its('class_name') { should eq 'com.game.core.physics.CollisionManagerr' }
+    its('extensions') { should eq 'java' }
   end
 
   describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log");') do
