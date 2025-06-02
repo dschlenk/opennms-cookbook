7c2cdbe6efdd45e53c989eba739c2109f06b1689
David Schlenk <dschlenk@convergeone.com>
2025-06-02 14:47:36 -0500

    custom resources for managing scriptd configuration (#87)
    * new scriptd resources: opennms_scriptd_engine, opennms_scriptd_script
    
    ---------
    
    Co-authored-by: ssgayathri <sgayatri@onec1.com>

---
+++ test/integration/default/libraries/scriptd_script.rb
@@ -0,0 +1,58 @@
+require 'rexml/document'
+
+class ScriptdScript < Inspec.resource(1)
+  name 'scriptd_script'
+
+  desc '
+    Custom InSpec resource to validate OpenNMS scriptd scripts in config file
+  '
+
+  example '
+    describe scriptd_script("beanshell", "event", "log = bsf.lookupBean(\"log\")", ["uei.opennms.org/anUei"]) do
+      it { should exist }
+    end
+  '
+
+  attr_reader :type, :uei, :script
+
+  def initialize(language, type, script, ueis = nil)
+    @exists = false
+    @type = type
+    @script = nil
+    @uei = ueis.is_a?(String) ? [ueis] : ueis
+
+    doc = REXML::Document.new(inspec.file('/opt/opennms/etc/scriptd-configuration.xml').content)
+    xpath = "/scriptd-configuration/#{type}-script[@language = '#{language}']"
+    puts "script xpath: #{xpath}"
+
+    unless doc.root.elements[xpath].nil?
+      doc.root.elements.each(xpath) do |script_el|
+        @script = ''
+        script_el.texts.each do |t|
+          @script += t.value
+        end
+        @script = @script.strip
+        puts "checking #{@script} for a match"
+        next unless @script == script.strip
+        puts "#{@script} matches"
+
+        if type == 'event' && @uei
+          puts 'type is event, checking if ueis match'
+          ul = []
+          script_el.each_element('uei') do |u|
+            puts "Found uei: #{u.attributes['name']}"
+            ul << u.attributes['name']
+          end
+          puts "ul.sort == @uei.sort ? #{ul.sort} == #{@uei.sort}"
+          @exists = ul.sort == @uei.sort
+        else
+          @exists = true
+        end
+      end
+    end
+  end
+
+  def exist?
+    @exists
+  end
+end
---
+++ test/integration/script/controls/script_spec.rb
@@ -0,0 +1,55 @@
+control 'script' do
+  describe scriptd_engine('beanshell') do
+    it { should exist }
+    its('class_name') { should eq 'bsh.util.BeanShellBSFEngine' }
+    its('extensions') { should eq 'bsh' }
+  end
+
+  describe scriptd_engine('groovy') do
+    it { should exist }
+    its('class_name') { should eq 'org.gradle.tasks.build.CompileTaskHandler' }
+    its('extensions') { should eq 'groovy' }
+  end
+
+  describe scriptd_engine('java') do
+    it { should exist }
+    its('class_name') { should eq 'com.game.core.physics.CollisionManagerr' }
+    its('extensions') { should eq 'java' }
+  end
+
+  describe scriptd_script('beanshell', 'start', 'bsf.lookupBean("log");') do
+    it { should exist }
+  end
+
+  describe scriptd_script('groovy', 'stop', 'bsf.lookupBean("log")') do
+    it { should exist }
+  end
+
+  describe scriptd_script('java', 'reload', 'bsf.lookupBean("log")') do
+    it { should exist }
+  end
+
+  describe scriptd_script('beanshell', 'event', 'bsf.lookupBean("log");', 'uei.opennms.org/cheftest/thresholdExceeded') do
+    it { should exist }
+  end
+
+  describe scriptd_script('beanshell', 'start', "log = bsf.lookupBean(\"log\");\nlog.info(\"Beanshell start script initialized.\");\nString user = System.getProperty(\"user.name\");\nlog.info(\"Running as user: \" + user);") do
+    it { should exist }
+  end
+
+  describe scriptd_script('groovy', 'stop', 'def log = bsf.lookupBean("log"); def now = new Date(); log.info("Groovy stop script executed at: ${now}"); if (now.hours > 18) { log.warn("Script stopped after hours."); }') do
+    it { should exist }
+  end
+
+  describe scriptd_script('java', 'reload', <<~JAVA.strip) do
+    log = bsf.lookupBean("log");
+    try { log.info("Reloading Java script.");
+    String version = System.getProperty("java.version");
+    log.info("Java version: " + version);
+    } catch (Exception e) {
+    log.error("Error during reload: " + e.getMessage());
+    }
+  JAVA
+    it { should exist }
+  end
+end
