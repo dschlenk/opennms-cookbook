8e3a8b0792a42f11148ed703d58254c3c3fae7c6
ssgayathri <sgayatri@onec1.com>
2025-06-12 19:15:05 +0530

    Update correlation.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/correlation.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/correlation.rb
@@ -18,15 +18,15 @@ opennms_drools_correlation_rule 'cookbook-drl-rule' do
   drl_source ['cookbook-rule.drl']
   drl_source_type 'cookbook_file'
   drl_source_properties(
-    mode: '0755',
-    owner: 'opennms',
-    group: 'opennms'
+    mode: '0700',
+    owner: 'root',
+    group: 'nobody'
   )
 end
 
 opennms_drools_correlation_rule 'remote-engine-rule' do
   engine_source_type 'remote_file'
-  engine_source 'https://example.com/drools-engine.xml'
+  engine_source 'https://raw.githubusercontent.com/UberPinguin/opennms-drools-sample-race-tests/refs/heads/master/src/test/opennms-home/etc/drools-engine.d/OpenNMS_Tests/drools-engine.xml'
   engine_source_properties(
     mode: '0644',
     show_progress: true
