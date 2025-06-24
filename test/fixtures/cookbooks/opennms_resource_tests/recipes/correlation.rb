6717c21a06792bb244c67b00e3ad4674bd8b291f
ssgayathri <sgayatri@onec1.com>
2025-06-24 16:54:54 +0530

    Update correlation.rb

--- test/fixtures/cookbooks/opennms_resource_tests/recipes/correlation.rb
+++ test/fixtures/cookbooks/opennms_resource_tests/recipes/correlation.rb
@@ -1,6 +1,12 @@
 if respond_to?(:opennms_drools_correlation_rule)
+
   opennms_drools_correlation_rule 'basic-rule' do
     engine_source 'drools-engine.xml.erb'
+    engine_source_variables(
+      rule_name: 'basic-rule',
+      engine_name: 'BasicEngine',
+      drl_files: ['sample.drl']
+    )
     drl_source ['sample.drl']
   end
 
@@ -9,19 +15,25 @@ if respond_to?(:opennms_drools_correlation_rule)
     engine_source 'drools-engine.xml.erb'
     engine_source_variables(
       engine_name: 'TemplateEngine',
-      rule_name: 'TemplateRule'
+      rule_name: 'TemplateRule',
+      drl_files: ['template-rule.drl']
     )
     drl_source ['template-rule.drl']
   end
 
   opennms_drools_correlation_rule 'cookbook-drl-rule' do
     engine_source 'drools-engine.xml.erb'
-    drl_source ['cookbook-rule.drl']
+    engine_source_variables(
+      engine_name: 'CookbookEngine',
+      rule_name: 'CookbookRule',
+      drl_files: ['sample.drl']
+    )
+    drl_source ['sample.drl']
     drl_source_type 'cookbook_file'
     drl_source_properties(
-      mode: '0700',
-      owner: 'root',
-      group: 'nobody'
+      mode: '0755',
+      owner: 'opennms',
+      group: 'opennms'
     )
   end
 
@@ -39,11 +51,10 @@ if respond_to?(:opennms_drools_correlation_rule)
     engine_source_type 'template'
     engine_source 'custom-engine.xml.erb'
     engine_source_variables(
-      engine_name: 'OverrideEngine',
-      rule_name: 'OverrideRule'
+      engine_name: 'Overridden',
+      rule_name: 'OverriddenRule'
     )
     engine_source_properties(
-      variables: { engine_name: 'Overridden', rule_name: 'OverriddenRule' },
       mode: '0600'
     )
     drl_source ['override-rule.drl']
@@ -51,6 +62,11 @@ if respond_to?(:opennms_drools_correlation_rule)
 
   opennms_drools_correlation_rule 'create-if-missing-rule' do
     engine_source 'drools-engine.xml.erb'
+    engine_source_variables(
+      engine_name: 'MissingEngine',
+      rule_name: 'MissingRule',
+      drl_files: ['create-if-missing.drl']
+    )
     drl_source ['create-if-missing.drl']
     action :create_if_missing
   end
@@ -61,9 +77,9 @@ if respond_to?(:opennms_drools_correlation_rule)
     engine_source_variables(
       engine_name: 'UserEngine',
       rule_name: 'UserDefinedRule',
-      drl_files: ['user-defined-rule.drl', 'template-rule.drl', 'cookbook-rule.drl']
+      drl_files: ['user-defined-rule.drl', 'template-rule.drl', 'sample.drl']
     )
-    drl_source ['user-defined-rule.drl', 'template-rule.drl', 'cookbook-rule.drl']
+    drl_source ['user-defined-rule.drl', 'template-rule.drl', 'sample.drl']
     drl_source_type 'template'
     drl_source_variables(
       rule_name: 'UserDefinedRule',
@@ -72,6 +88,7 @@ if respond_to?(:opennms_drools_correlation_rule)
       action: 'System.out.println("Critical alert: " + $alert.getMessage());'
     )
   end
+
 else
   Chef::Log.warn("The resource 'opennms_drools_correlation_rule' is not available. Please ensure the 'opennms' cookbook is included.")
 end
