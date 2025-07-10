1d83879ac2d42232a9791d029b22439760f12a87
ssgayathri <sgayatri@onec1.com>
2025-07-10 20:08:30 +0530

    Update jms_nb_destination_helper.rb

--- libraries/jms_nb_destination_helper.rb
+++ libraries/jms_nb_destination_helper.rb
@@ -118,3 +118,93 @@ module Opennms
     end
   end
 end
+
+module Opennms
+  module Cookbook
+    module Jms
+      module JmsNbTemplate
+        def jms_nb_resource_init
+          jms_nb_resource_create unless jms_nb_resource_exist?
+        end
+
+        def jms_nb_resource
+          return unless jms_nb_resource_exist?
+          find_resource!(:template, "#{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml")
+        end
+
+        def ro_jms_nb_resource_init
+          ro_jms_nb_resource_create unless ro_jms_nb_resource_exist?
+        end
+
+        def ro_jms_nb_resource
+          return unless ro_jms_nb_resource_exist?
+          find_resource!(:template, "RO #{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml")
+        end
+
+        private
+
+        def jms_nb_resource_exist?
+          !find_resource(:template, "#{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml").nil?
+        rescue Chef::Exceptions::ResourceNotFound
+          false
+        end
+
+        def jms_nb_resource_create
+          config_path = "#{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml"
+          config = Opennms::Cookbook::ConfigHelpers::Jms::JmsNbConfig.new
+
+          if ::File.exist?(config_path)
+            config.read!(config_path)
+          else
+            Chef::Log.warn("JMS config file #{config_path} does not exist. Initializing empty config.")
+          end
+
+          with_run_context :root do
+            declare_resource(:template, config_path) do
+              source 'jms-northbounder-configuration.xml.erb'
+              cookbook 'opennms'
+              owner node['opennms']['username']
+              group node['opennms']['groupname']
+              mode '0664'
+              variables(config: config)
+              action :nothing
+              delayed_action :create
+              notifies :restart, 'service[opennms]', :delayed
+            end
+          end
+        end
+
+        def ro_jms_nb_resource_exist?
+          !find_resource(:template, "RO #{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml").nil?
+        rescue Chef::Exceptions::ResourceNotFound
+          false
+        end
+
+        def ro_jms_nb_resource_create
+          config_path = "#{node['opennms']['conf']['home']}/etc/jms-northbounder-configuration.xml"
+          config = Opennms::Cookbook::ConfigHelpers::Jms::JmsNbConfig.new
+
+          if ::File.exist?(config_path)
+            config.read!(config_path)
+          else
+            Chef::Log.warn("RO: JMS config file #{config_path} does not exist. Initializing empty config.")
+          end
+
+          with_run_context :root do
+            declare_resource(:template, "RO #{config_path}") do
+              path "#{Chef::Config[:file_cache_path]}/jms-northbounder-configuration.xml"
+              source 'jms-northbounder-configuration.xml.erb'
+              cookbook 'opennms'
+              owner node['opennms']['username']
+              group node['opennms']['groupname']
+              mode '0664'
+              variables(config: config)
+              action :nothing
+              delayed_action :nothing
+            end
+          end
+        end
+      end
+    end
+  end
+end
