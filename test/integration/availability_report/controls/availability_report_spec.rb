119880f41d24a6c9aec9d1d0b1875cae418702c0
ssgayathri <sgayatri@onec1.com>
2025-07-21 16:59:48 +0530

    Update availability_report_spec.rb

--- test/integration/availability_report/controls/availability_report_spec.rb
+++ test/integration/availability_report/controls/availability_report_spec.rb
@@ -1,63 +1,62 @@
 control 'availability_reports' do
   describe availability_report('foo') do
     it { should exist }
-    its('type') { should eq 'calendar' }
-    its('parameters["category"]["name"]') { should eq 'category' }
-    its('parameters"]') { should eq 'Category' }
-    its('parameters["category"]["input-type"]') { should eq 'freeText' }
-    its('parameters["category"]["default"]') { should eq 'Core' }
-    its('parameters["category"]["display-name"]') { should eq 'Category' }
-    its('parameters["category"]["input-type"]') { should eq 'reportCategorySelector' }
-    its('parameters["category"]["default"]') { should eq 'Network' }
-    its('parameters["endDate"]["name"]') { should eq 'endDate' }
-    its('parameters["endDate"]["display-name"]') { should eq 'End Date' }
-    its('parameters["endDate"]["use-absolute-date"]') { should eq 'false' }
-    its('parameters["endDate"]["default-interval"]') { should eq 'month' }
-    its('parameters["endDate"]["default-count"]') { should eq '1' }
-    its('parameters["endDate"]["default-time.hour"]') { should eq '0' }
-    its('parameters["endDate"]["default-time.minute"]') { should eq '0' }
-    its('parameters["retryCount"]["name"]') { should eq 'retryCount' }
-    its('parameters["retryCount"]["display-name"]') { should eq 'Retry Count' }
-    its('parameters["retryCount"]["input-type"]') { should eq 'freeText' }
-    its('parameters["retryCount"]["default"]') { should eq '3' }
+    it { expect(subject.type).to eq 'calendar' }
+    it { expect(subject.parameters['category']['name']).to eq 'category' }
+    it { expect(subject.parameters['category']['display-name']).to eq 'Category' }
+    it { expect(subject.parameters['category']['input-type']).to eq 'reportCategorySelector' }
+    it { expect(subject.parameters['category']['default']).to eq 'Network' }
+    it { expect(subject.parameters['endDate']['name']).to eq 'endDate' }
+    it { expect(subject.parameters['endDate']['display-name']).to eq 'End Date' }
+    it { expect(subject.parameters['endDate']['use-absolute-date']).to eq 'false' }
+    it { expect(subject.parameters['endDate']['default-interval']).to eq 'month' }
+    it { expect(subject.parameters['endDate']['default-count']).to eq '1' }
+    it { expect(subject.parameters['endDate']['default-time']['hour']).to eq '0' }
+    it { expect(subject.parameters['endDate']['default-time']['minute']).to eq '0' }
+    it { expect(subject.parameters['retryCount']['name']).to eq 'retryCount' }
+    it { expect(subject.parameters['retryCount']['display-name']).to eq 'Retry Count' }
+    it { expect(subject.parameters['retryCount']['input-type']).to eq 'freeText' }
+    it { expect(subject.parameters['retryCount']['default']).to eq '3' }
   end
 
   describe availability_report('bar') do
     it { should exist }
-    its('type') { should eq 'classic' }
-    its('parameters["category"]["name"]') { should eq 'category' }
-    its('parameters["category"]["display-name    # Date parameter: endDate
-    its('parameters["endDate"]["name"]') { should eq 'endDate' }
-    its('parameters["endDate"]["display-name"]') { should eq 'End Date' }
-    its('parameters["endDate"]["use-absolute-date"]') { should eq 'true' }
-    its('parameters["endDate"]["default-interval"]') { should eq 'day' }
-    its('parameters["endDate"]["default-count"]') { should eq '7' }
-    its('parameters["endDate"]["default-time.hour"]') { should eq '6' }
-    its('parameters["endDate"]["default-time.minute"]') { should eq '30' }
-    its('parameters["retryCount"]["name"]') { should eq 'retryCount' }
-    its('parameters["retryCount"]["display-name"]') { should eq 'Retry Count' }
-    its('parameters["retryCount"]["input-type"]') { should eq 'freeText' }
-    its('parameters["retryCount"]["default"]') { should eq '5' }
+    it { expect(subject.type).to eq 'classic' }
+    it { expect(subject.parameters['category']['name']).to eq 'category' }
+    it { expect(subject.parameters['category']['display-name']).to eq 'Category' }
+    it { expect(subject.parameters['category']['input-type']).to eq 'freeText' }
+    it { expect(subject.parameters['category']['default']).to eq 'Core' }
+    it { expect(subject.parameters['endDate']['name']).to eq 'endDate' }
+    it { expect(subject.parameters['endDate']['display-name']).to eq 'End Date' }
+    it { expect(subject.parameters['endDate']['use-absolute-date']).to eq 'true' }
+    it { expect(subject.parameters['endDate']['default-interval']).to eq 'day' }
+    it { expect(subject.parameters['endDate']['default-count']).to eq '7' }
+    it { expect(subject.parameters['endDate']['default-time']['hour']).to eq '6' }
+    it { expect(subject.parameters['endDate']['default-time']['minute']).to eq '30' }
+    it { expect(subject.parameters['retryCount']['name']).to eq 'retryCount' }
+    it { expect(subject.parameters['retryCount']['display-name']).to eq 'Retry Count' }
+    it { expect(subject.parameters['retryCount']['input-type']).to eq 'freeText' }
+    it { expect(subject.parameters['retryCount']['default']).to eq '5' }
   end
 
   describe availability_report('baz') do
     it { should exist }
-    its('type') { should eq 'calendar' }
-    its('parameters["category"]["name"]') { should eq 'category' }
-    its('parameters["category"]["display-name"]') { should eq 'Category' }
-    its('parameters["category"]["input-type"]') { should eq 'reportCategorySelector' }
-    its('parameters["category"]["default"]') { should eq 'WAN' }
-    its('parameters["endDate"]["name"]') { should eq 'endDate' }
-    its('parameters["endDate"]["display-name"]') { should eq 'End Date' }
-    its('parameters["endDate"]["use-absolute-date"]') { should eq 'false' }
-    its('parameters["endDate"]["default-interval"]') { should eq 'year' }
-    its('parameters["endDate"]["default-count"]') { should eq '2' }
-    its('parameters["endDate"]["default-time.hour"]') { should eq '12' }
-    its('parameters["endDate"]["default-time.minute"]') { should eq '15' }
-    its('parameters["retryCount"]["name"]') { should eq 'retryCount' }
-    its('parameters["retryCount"]["display-name"]') { should eq 'Retry Count' }
-    its('parameters["retryCount"]["input-type"]') { should eq 'freeText' }
-    its('parameters["retryCount"]["default"]') { should eq '10' }
+    it { expect(subject.type).to eq 'calendar' }
+    it { expect(subject.parameters['category']['name']).to eq 'category' }
+    it { expect(subject.parameters['category']['display-name']).to eq 'Category' }
+    it { expect(subject.parameters['category']['input-type']).to eq 'reportCategorySelector' }
+    it { expect(subject.parameters['category']['default']).to eq 'WAN' }
+    it { expect(subject.parameters['endDate']['name']).to eq 'endDate' }
+    it { expect(subject.parameters['endDate']['display-name']).to eq 'End Date' }
+    it { expect(subject.parameters['endDate']['use-absolute-date']).to eq 'false' }
+    it { expect(subject.parameters['endDate']['default-interval']).to eq 'year' }
+    it { expect(subject.parameters['endDate']['default-count']).to eq '2' }
+    it { expect(subject.parameters['endDate']['default-time']['hour']).to eq '12' }
+    it { expect(subject.parameters['endDate']['default-time']['minute']).to eq '15' }
+    it { expect(subject.parameters['retryCount']['name']).to eq 'retryCount' }
+    it { expect(subject.parameters['retryCount']['display-name']).to eq 'Retry Count' }
+    it { expect(subject.parameters['retryCount']['input-type']).to eq 'freeText' }
+    it { expect(subject.parameters['retryCount']['default']).to eq '10' }
   end
 
   describe availability_report('delete') do
