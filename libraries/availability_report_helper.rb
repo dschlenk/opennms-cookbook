97591820c9dcd76e45d6767a09f811b49a4b58a2
ssgayathri <sgayatri@onec1.com>
2025-07-23 19:40:59 +0530

    Update availability_report_helper.rb

--- libraries/availability_report_helper.rb
+++ libraries/availability_report_helper.rb
@@ -1,125 +1,122 @@
 module Opennms
   module Cookbook
-    module ConfigHelpers
-      module AvailabilityReportTemplate
-        class Report
-          attr_accessor :id, :type, :parameters
+    module AvailabilityReportHelper
+      class ReportConfig
+        attr_reader :data
 
-          def initialize(id:, type:, parameters: {})
-            @id = id
-            @type = type
-            @parameters = parameters
-          end
+        def initialize
+          @data = { reports: [] }
+        end
 
-          def update(type:, parameters:)
-            @type = type unless type.nil?
-            @parameters = parameters unless parameters.nil?
+        def read!(file_path)
+          edit_xml_file(file_path) do |doc|
+            doc.elements.each('opennms-reports/report') do |el|
+              report = {
+                id: el.attributes['id'],
+                type: el.attributes['type'],
+                pdf_template: el.elements['pdf-template']&.text,
+                svg_template: el.elements['svg-template']&.text,
+                html_template: el.elements['html-template']&.text,
+                logo: el.elements['logo']&.text,
+                parameters: {},
+              }
+              el.elements.each('parameter') do |param|
+                report[:parameters][param.attributes['key']] = param.text
+              end
+              @data[:reports] << report
+            end
           end
         end
 
-        class ReportConfig
-          include Opennms::XmlHelper
+        def report_exists?(report_id)
+          @data[:reports].any? { |r| r[:id] == report_id }
+        end
 
-          def initialize
-            @data = { reports: [] }
-            Chef::Log.debug('[ReportConfig] Initialized with empty report list.')
-          end
+        def create!(file_path, new_report)
+          edit_xml_file(file_path) do |doc|
+            root = doc.root
 
-          def read!(file)
-            Chef::Log.info("[ReportConfig] Reading availability reports from: #{file}")
-            raise ArgumentError, "File #{file} does not exist" unless ::File.exist?(file)
-
-            edit_xml_file(file) do |doc|
-              doc.xpath('//report').each do |report_el|
-                id = report_el['id']
-                type = report_el['type']
-                parameters = parse_parameters(report_el)
-
-                report = Report.new(id: id, type: type, parameters: parameters)
-                Chef::Log.debug("[ReportConfig] Loaded report: #{id}")
-                @data[:reports] << report
+            report_el = REXML::Element.new('report')
+            report_el.add_attributes(
+              'id' => new_report[:id],
+              'type' => new_report[:type],
+            )
+            %i[pdf_template svg_template html_template logo].each do |field|
+              if new_report[field]
+                child = REXML::Element.new(field.to_s.tr('_', '-'))
+                child.text = new_report[field]
+                report_el.add_element(child)
               end
             end
 
-            Chef::Log.info("[ReportConfig] Finished reading. Total reports: #{@data[:reports].size}")
+            if new_report[:parameters]
+              new_report[:parameters].each do |k, v|
+                param_el = REXML::Element.new('parameter')
+                param_el.add_attribute('key', k)
+                param_el.text = v
+                report_el.add_element(param_el)
+              end
+            end
+            root.add_element(report_el)
           end
+        end
 
-          def reports
-            @data[:reports]
-          end
-
-          def find_report_by_id(id)
-            Chef::Log.debug("[ReportConfig] Searching for report: #{id}")
-            found = reports.find { |r| r.id == id }
-            Chef::Log.debug("[ReportConfig] Found report: #{found.inspect}") if found
-            found
-          end
-
-          def delete_report(id)
-            Chef::Log.info("[ReportConfig] Deleting report: #{id}")
-            @data[:reports].reject! { |r| r.id == id }
-            Chef::Log.info('[ReportConfig] Deleted report.')
-          end
-
-          def add_or_update_report(new_report)
-            existing = find_report_by_id(new_report.id)
-            if existing
-              Chef::Log.info("[ReportConfig] Updating existing report: #{new_report.id}")
-              existing.update(type: new_report.type, parameters: new_report.parameters)
-            else
-              Chef::Log.info("[ReportConfig] Adding new report: #{new_report.id}")
-              @data[:reports] << new_report
+        def update!(file_path, updated_report)
+          edit_xml_file(file_path) do |doc|
+            doc.elements.each('opennms-reports/report') do |el|
+              next unless el.attributes['id'] == updated_report[:id]
+              el.attributes['type'] = updated_report[:type]
+              %w[pdf-template svg-template html-template logo].each do |tag|
+                child = el.elements[tag]
+                value = updated_report[tag.tr('-', '_').to_sym]
+                if value
+                  if child
+                    child.text = value
+                  else
+                    new_child = REXML::Element.new(tag)
+                    new_child.text = value
+                    el.add_element(new_child)
+                  end
+                elsif child
+                  el.delete_element(child)
+                end
+              end
+              el.elements.delete_all('parameter')
+              if updated_report[:parameters]
+                updated_report[:parameters].each do |k, v|
+                  param_el = REXML::Element.new('parameter')
+                  param_el.add_attribute('key', k)
+                  param_el.text = v
+                  el.add_element(param_el)
+                end
+              end
             end
           end
+        end
 
-          def to_hash
-            @data
+        def delete!(file_path, report_id)
+          edit_xml_file(file_path) do |doc|
+            doc.elements.each('opennms-reports/report') do |el|
+              if el.attributes['id'] == report_id
+                doc.root.delete_element(el)
+                break
+              end
+            end
           end
+        end
 
-          private
+        private
 
-          def parse_parameters(report_el)
-            param_el = report_el.at_xpath('parameters')
-            return {} unless param_el
-
-            {
-              string_parms: param_el.xpath('string-parameter').map do |el|
-                {
-                  name: el['name'],
-                  display_name: el['display-name'],
-                  input_type: el['input-type'],
-                  default: el['default'],
-                }
-              end,
-              date_parms: param_el.xpath('date-parameter').map do |el|
-                default_time = el.at_xpath('default-time')
-                default_time_hash =
-                  if default_time
-                    {
-                      hour: default_time['hour'].to_i,
-                      minute: default_time['minute'].to_i,
-                    }
-                  end
-
-                {
-                  name: el['name'],
-                  display_name: el['display-name'],
-                  use_absolute_date: el['use-absolute-date'] == 'true',
-                  default_interval: el['default-interval'],
-                  default_count: el['default-count'].to_i,
-                  default_time: default_time_hash,
-                }
-              end,
-              int_parms: param_el.xpath('int-parameter').map do |el|
-                {
-                  name: el['name'],
-                  display_name: el['display-name'],
-                  input_type: el['input-type'],
-                  default: el['default']&.to_i,
-                }
-              end,
-            }
-          end
+        def edit_xml_file(path)
+          content = ::File.read(path)
+          doc = REXML::Document.new(content)
+          yield(doc)
+          formatter = REXML::Formatters::Pretty.new(2)
+          formatter.compact = true
+          output = ''
+          formatter.write(doc, output)
+          ::File.write(path, output)
+          doc
         end
       end
     end
