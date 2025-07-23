99193ce718540f76208d4523814b77d378021f29
ssgayathri <sgayatri@onec1.com>
2025-07-23 19:14:32 +0530

    Update availability_report_helper.rb

--- libraries/availability_report_helper.rb
+++ libraries/availability_report_helper.rb
@@ -88,7 +88,7 @@ module Opennms
                   name: el['name'],
                   display_name: el['display-name'],
                   input_type: el['input-type'],
-                  default: el['default']
+                  default: el['default'],
                 }
               end,
               date_parms: param_el.xpath('date-parameter').map do |el|
@@ -97,7 +97,7 @@ module Opennms
                   if default_time
                     {
                       hour: default_time['hour'].to_i,
-                      minute: default_time['minute'].to_i
+                      minute: default_time['minute'].to_i,
                     }
                   end
 
@@ -107,7 +107,7 @@ module Opennms
                   use_absolute_date: el['use-absolute-date'] == 'true',
                   default_interval: el['default-interval'],
                   default_count: el['default-count'].to_i,
-                  default_time: default_time_hash
+                  default_time: default_time_hash,
                 }
               end,
               int_parms: param_el.xpath('int-parameter').map do |el|
@@ -115,9 +115,9 @@ module Opennms
                   name: el['name'],
                   display_name: el['display-name'],
                   input_type: el['input-type'],
-                  default: el['default']&.to_i
+                  default: el['default']&.to_i,
                 }
-              end
+              end,
             }
           end
         end
