ebfa099162c5378c14af2a8433a8bf6d469fe63b
ssgayathri <sgayatri@onec1.com>
2025-07-23 15:32:32 +0530

    Update availability_report.rb

--- resources/availability_report.rb
+++ resources/availability_report.rb
@@ -1,8 +1,6 @@
 include Opennms::XmlHelper
 include Opennms::Cookbook::ConfigHelpers::AvailabilityReportTemplate
 
-provides :opennms_availability_report
-
 property :report_id, String, name_property: true
 property :type, String, equal_to: %w(calendar classic), default: 'calendar'
 
@@ -37,13 +35,56 @@ default_action :create
 load_current_value do |desired|
   require 'rexml/document'
   file_path = ::File.join(node['opennms']['conf']['home'], 'etc', 'availability-reports.xml')
-  if ::File.exist?(file_path)
-    file = ::File.read(file_path)
-    doc = REXML::Document.new(file)
-    report = REXML::XPath.first(doc, "//report[@id='#{desired.report_id}']")
-    current_value_does_not_exist! if report.nil?
-    type report.attributes['type']
+  unless ::File.exist?(file_path)
+    current_value_does_not_exist!
+    next
   end
+
+  file = ::File.read(file_path)
+  doc = REXML::Document.new(file)
+  report = REXML::XPath.first(doc, "//report[@id='#{desired.report_id}']")
+  if report.nil?
+    current_value_does_not_exist!
+    next
+  end
+
+  type report.attributes['type']
+
+  parameters_hash = {
+    'string_parms' => [],
+    'date_parms' => [],
+    'int_parms' => []
+  }
+
+  param_elem = report.elements['parameters']
+  if param_elem
+    param_elem.elements.each('string-parm') do |el|
+      parameters_hash['string_parms'] << el.attributes.to_h.transform_keys { |k| k.tr('-', '_') }
+    end
+
+    param_elem.elements.each('date-parm') do |el|
+      dp = {
+        'name' => el.attributes['name'],
+        'display_name' => el.attributes['display-name'],
+        'use_absolute_date' => el.attributes['use-absolute-date'],
+        'default_interval' => el.attributes['default-interval'],
+        'default_count' => el.attributes['default-count']
+      }
+      if (dt = el.elements['default-time'])
+        dp['default_time'] = {
+          'hour' => dt.attributes['hour'],
+          'minute' => dt.attributes['minute']
+        }
+      end
+      parameters_hash['date_parms'] << dp
+    end
+
+    param_elem.elements.each('int-parm') do |el|
+      parameters_hash['int_parms'] << el.attributes.to_h.transform_keys { |k| k.tr('-', '_') }
+    end
+  end
+
+  parameters parameters_hash
 end
 
 action_class do
@@ -52,6 +93,38 @@ action_class do
 
   def config_file
     ::File.join(node['opennms']['conf']['home'], 'etc', 'availability-reports.xml')
+  end
+
+  def load_xml_document
+    require 'rexml/document'
+    file = ::File.read(config_file)
+    REXML::Document.new(file)
+  end
+
+  def save_xml_document(doc)
+    ::File.open(config_file, 'w') do |f|
+      formatter = REXML::Formatters::Pretty.new
+      formatter.compact = true
+      formatter.write(doc, f)
+    end
+  end
+
+  def find_or_create_report_element(doc)
+    doc.root ||= REXML::Element.new('reports-configuration')
+    reports_el = doc.root.elements['reports'] || doc.root.add_element('reports')
+    report_elem = REXML::XPath.first(doc, "//report[@id='#{new_resource.report_id}']")
+    unless report_elem
+      report_elem = REXML::Element.new('report')
+      report_elem.add_attribute('id', new_resource.report_id)
+      reports_el.add_element(report_elem)
+    end
+    report_elem
+  end
+
+  def update_report_element(report_elem)
+    report_elem.attributes['type'] = new_resource.type
+    report_elem.elements.delete_all('parameters')
+    build_parameters_xml(report_elem)
   end
 
   def validate_or_create_file(prefix)
@@ -84,13 +157,13 @@ action_class do
     param_elem = REXML::Element.new('parameters')
 
     Array(params['string_parms']).each do |sp|
-      string_elem = REXML::Element.new('string-parameter')
+      string_elem = REXML::Element.new('string-parm')
       sp.each { |k, v| string_elem.add_attribute(k.to_s.tr('_', '-'), v.to_s) }
       param_elem.add_element(string_elem)
     end
 
     Array(params['date_parms']).each do |dp|
-      date_elem = REXML::Element.new('date-parameter')
+      date_elem = REXML::Element.new('date-parm')
       date_elem.add_attribute('name', dp['name'])
       date_elem.add_attribute('display-name', dp['display_name'])
       date_elem.add_attribute('use-absolute-date', dp['use_absolute_date'].to_s)
@@ -108,7 +181,7 @@ action_class do
     end
 
     Array(params['int_parms']).each do |ip|
-      int_elem = REXML::Element.new('int-parameter')
+      int_elem = REXML::Element.new('int-parm')
       ip.each { |k, v| int_elem.add_attribute(k.to_s.tr('_', '-'), v.to_s) }
       param_elem.add_element(int_elem)
     end
@@ -119,31 +192,12 @@ end
 
 action :create do
   converge_if_changed do
-    require 'rexml/document'
+    raise "File not found: #{config_file}" unless ::File.exist?(config_file)
 
-    file_path = config_file
-    raise "File not found: #{file_path}" unless ::File.exist?(file_path)
-
-    file = ::File.read(file_path)
-    doc = REXML::Document.new(file)
-
-    doc.elements.each("//report[@id='#{new_resource.report_id}']") { |el| el.parent.delete(el) }
-
-    report_elem = REXML::Element.new('report')
-    report_elem.add_attribute('id', new_resource.report_id)
-    report_elem.add_attribute('type', new_resource.type)
-
-    build_parameters_xml(report_elem)
-
-    doc.root ||= REXML::Element.new('reports-configuration')
-    reports_el = doc.root.elements['reports'] || doc.root.add_element('reports')
-    reports_el.add_element(report_elem)
-
-    ::File.open(file_path, 'w') do |f|
-      formatter = REXML::Formatters::Pretty.new
-      formatter.compact = true
-      formatter.write(doc, f)
-    end
+    doc = load_xml_document
+    report_elem = find_or_create_report_element(doc)
+    update_report_element(report_elem)
+    save_xml_document(doc)
   end
 
   %w(pdf svg html).each do |prefix|
@@ -166,7 +220,6 @@ end
 
 action :create_if_missing do
   require 'rexml/document'
-
   file_path = config_file
   found = false
   if ::File.exist?(file_path)
@@ -179,7 +232,6 @@ end
 
 action :delete do
   require 'rexml/document'
-
   file_path = config_file
   raise "File not found: #{file_path}" unless ::File.exist?(file_path)
 
