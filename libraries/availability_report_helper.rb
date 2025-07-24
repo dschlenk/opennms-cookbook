ea213ea1eb97fc866aa537b8c3cbb54c8eab6f0e
ssgayathri <sgayatri@onec1.com>
2025-07-24 20:41:29 +0530

    Update availability_report_helper.rb

--- libraries/availability_report_helper.rb
+++ libraries/availability_report_helper.rb
@@ -21,10 +21,6 @@ module Opennms
                 parameters: {},
               }
 
-              el.elements.each('parameter') do |param|
-                report[:parameters][param.attributes['key']] = param.text
-              end
-
               @data[:reports] << report
             end
           end
@@ -43,12 +39,9 @@ module Opennms
             root = doc.root
 
             report_el = REXML::Element.new('report')
-            report_el.add_attributes(
-              'id' => new_report[:id],
-              'type' => new_report[:type]
-            )
+            report_el.add_attributes('id' => new_report[:id], 'type' => new_report[:type])
 
-            %i(pdf_template svg_template html_template logo).each do |field|
+            %i[pdf_template svg_template html_template logo].each do |field|
               next unless new_report[field]
 
               child = REXML::Element.new(field.to_s.tr('_', '-'))
@@ -56,15 +49,7 @@ module Opennms
               report_el.add_element(child)
             end
 
-            if new_report[:parameters]
-              new_report[:parameters].each do |k, v|
-                param_el = REXML::Element.new('parameter')
-                param_el.add_attribute('key', k)
-                param_el.text = v
-                report_el.add_element(param_el)
-              end
-            end
-
+            report_el.add_element(build_parameters(new_report[:parameters])) if new_report[:parameters]
             root.add_element(report_el)
           end
         end
@@ -76,7 +61,7 @@ module Opennms
 
               el.attributes['type'] = updated_report[:type]
 
-              %w(pdf-template svg-template html-template logo).each do |tag|
+              %w[pdf-template svg-template html-template logo].each do |tag|
                 child = el.elements[tag]
                 value = updated_report[tag.tr('-', '_').to_sym]
 
@@ -93,15 +78,8 @@ module Opennms
                 end
               end
 
-              el.elements.delete_all('parameter')
-              next unless updated_report[:parameters]
-
-              updated_report[:parameters].each do |k, v|
-                param_el = REXML::Element.new('parameter')
-                param_el.add_attribute('key', k)
-                param_el.text = v
-                el.add_element(param_el)
-              end
+              el.delete_element('parameters')
+              el.add_element(build_parameters(updated_report[:parameters])) if updated_report[:parameters]
             end
           end
         end
@@ -118,6 +96,64 @@ module Opennms
         end
 
         private
+
+        def build_parameters(params)
+          params_el = REXML::Element.new('parameters')
+
+          (params['string_parms'] || []).each do |sp|
+            el = REXML::Element.new('string-parm')
+            el.add_attributes(
+              'name' => sp['name'],
+              'display-name' => sp['display_name'],
+              'input-type' => sp['input_type']
+            )
+            el.add_attribute('default', sp['default']) if sp.key?('default')
+            params_el.add_element(el)
+          end
+
+          (params['date_parms'] || []).each do |dp|
+            el = REXML::Element.new('date-parm')
+            el.add_attributes(
+              'name' => dp['name'],
+              'display-name' => dp['display_name']
+            )
+            el.add_attribute('use-absolute-date', dp['use_absolute_date'].to_s) if dp.key?('use_absolute_date')
+
+            interval = REXML::Element.new('default-interval')
+            interval.text = dp['default_interval']
+            el.add_element(interval)
+
+            count = REXML::Element.new('default-count')
+            count.text = dp['default_count'].to_s
+            el.add_element(count)
+
+            if dp['default_time']
+              dt = REXML::Element.new('default-time')
+              hour = REXML::Element.new('hours')
+              hour.text = dp['default_time']['hour'].to_s
+              minute = REXML::Element.new('minutes')
+              minute.text = dp['default_time']['minute'].to_s
+              dt.add_element(hour)
+              dt.add_element(minute)
+              el.add_element(dt)
+            end
+
+            params_el.add_element(el)
+          end
+
+          (params['int_parms'] || []).each do |ip|
+            el = REXML::Element.new('int-parm')
+            el.add_attributes(
+              'name' => ip['name'],
+              'display-name' => ip['display_name'],
+              'input-type' => ip['input_type']
+            )
+            el.add_attribute('default', ip['default'].to_s) if ip.key?('default')
+            params_el.add_element(el)
+          end
+
+          params_el
+        end
 
         def edit_xml_file(path)
           content = ::File.read(path)
