6d1542b055e9c20e8c8f8f59d35d44eec8ca588f
ssgayathri <sgayatri@onec1.com>
2025-07-28 15:02:24 +0530

    Update availability_report.rb

--- resources/availability_report.rb
+++ resources/availability_report.rb
@@ -1,5 +1,5 @@
 include Opennms::XmlHelper
-include Opennms::Cookbook::AvailabilityReportHelper
+include Opennms::Cookbook::AvailabilityReportTemplate
 
 property :report_id, String, name_property: true
 property :type, String, equal_to: %w(calendar classic), default: 'calendar'
@@ -48,7 +48,7 @@ end
 
 action_class do
   include Chef::DSL::Recipe
-  include ::Opennms::Cookbook::AvailabilityReportHelper
+  include Opennms::XmlHelper
   include ::Opennms::Cookbook::AvailabilityReportTemplate
 
   def etc_dir
