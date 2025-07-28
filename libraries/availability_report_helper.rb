698d13daabbab7f81c1acc1b4b8b79e12f87f938
ssgayathri <sgayatri@onec1.com>
2025-07-28 16:11:45 +0530

    Update availability_report_helper.rb

--- libraries/availability_report_helper.rb
+++ libraries/availability_report_helper.rb
@@ -24,7 +24,7 @@ module Opennms
               svg_template: el.elements['svg-template']&.text,
               html_template: el.elements['html-template']&.text,
               logo: el.elements['logo']&.text,
-              parameters: parse_parameters(el.elements['parameters'])
+              parameters: parse_parameters(el.elements['parameters']),
             }
             @reports << report
           end
@@ -132,17 +132,18 @@ module Opennms
 
             default_time_el = el.elements['default-time']
             if default_time_el
-              h['default-time'] = if default_time_el.attributes['hour'] && default_time_el.attributes['minute']
-                {
-                  'hour' => default_time_el.attributes['hour'],
-                  'minute' => default_time_el.attributes['minute']
-                }
-              else
-                {
-                  'hour' => default_time_el.elements['hours']&.text,
-                  'minute' => default_time_el.elements['minutes']&.text
-                }
-              end
+              h['default-time'] =
+                if default_time_el.attributes['hour'] && default_time_el.attributes['minute']
+                  {
+                    'hour' => default_time_el.attributes['hour'],
+                    'minute' => default_time_el.attributes['minute'],
+                  }
+                else
+                  {
+                    'hour' => default_time_el.elements['hours']&.text,
+                    'minute' => default_time_el.elements['minutes']&.text,
+                  }
+                end
             end
 
             params_hash[el.attributes['name']] = h
@@ -163,7 +164,7 @@ module Opennms
             el.add_attributes(
               'name' => sp['name'],
               'display-name' => sp['display_name'],
-              'input-type' => sp['input_type']
+              'input-type' => sp['input_type'],
             )
             el.add_attribute('default', sp['default']) if sp.key?('default')
             params_el.add_element(el)
@@ -173,7 +174,7 @@ module Opennms
             el = REXML::Element.new('date-parm')
             el.add_attributes(
               'name' => dp['name'],
-              'display-name' => dp['display_name']
+              'display-name' => dp['display_name'],
             )
             el.add_attribute('use-absolute-date', dp['use_absolute_date'].to_s) if dp.key?('use_absolute_date')
 
@@ -204,7 +205,7 @@ module Opennms
             el.add_attributes(
               'name' => ip['name'],
               'display-name' => ip['display_name'],
-              'input-type' => ip['input_type']
+              'input-type' => ip['input_type'],
             )
             el.add_attribute('default', ip['default'].to_s) if ip.key?('default')
             params_el.add_element(el)
