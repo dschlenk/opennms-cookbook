17990b1f0d1aeac5adcf90e2bfef852a50eb1a94
ssgayathri <sgayatri@onec1.com>
2025-07-29 19:41:20 +0530

    Update availability_report_helper.rb

--- libraries/availability_report_helper.rb
+++ libraries/availability_report_helper.rb
@@ -33,90 +33,24 @@ module Opennms
           @reports.any? { |r| r[:id] == report_id }
         end
 
-        def find_report_by_id(report_id)
+        def find_by_id(report_id)
           @reports.find { |r| r[:id] == report_id }
         end
 
-        def add_or_update_report(file_path, report)
-          if report_exists?(report[:id])
-            update!(file_path, report)
+        def add_or_update_report_in_memory(report)
+          idx = @reports.index { |r| r[:id] == report[:id] }
+          if idx
+            @reports[idx] = report
           else
-            create!(file_path, report)
+            @reports << report
           end
-          true
         end
 
-        def delete!(file_path, report_id)
-          edit_xml_file(file_path) do |doc|
-            reports = doc.elements.to_a('opennms-reports/report')
-            reports.each do |el|
-              next unless el.attributes['id'] == report_id
-
-              doc.root.delete_element(el)
-              @reports.delete_if { |r| r[:id] == report_id }
-              break
-            end
-          end
+        def delete_report_in_memory(report_id)
+          @reports.reject! { |r| r[:id] == report_id }
         end
 
         private
-
-        def create!(file_path, new_report)
-          edit_xml_file(file_path) do |doc|
-            root = doc.root
-
-            report_el = REXML::Element.new('report')
-            report_el.add_attributes('id' => new_report[:id], 'type' => new_report[:type])
-
-            add_optional_children(report_el, new_report)
-
-            root.add_element(report_el)
-            @reports << new_report
-          end
-        end
-
-        def update!(file_path, updated_report)
-          edit_xml_file(file_path) do |doc|
-            doc.elements.each('opennms-reports/report') do |el|
-              next unless el.attributes['id'] == updated_report[:id]
-
-              el.attributes['type'] = updated_report[:type]
-
-              %w(pdf-template svg-template html-template logo).each do |tag|
-                child = el.elements[tag]
-                value = updated_report[tag.tr('-', '_').to_sym]
-                if value
-                  if child
-                    child.text = value
-                  else
-                    new_child = REXML::Element.new(tag)
-                    new_child.text = value
-                    el.add_element(new_child)
-                  end
-                elsif child
-                  el.delete_element(child)
-                end
-              end
-
-              el.delete_element('parameters') if el.elements['parameters']
-              el.add_element(build_parameters(updated_report[:parameters])) if updated_report[:parameters]
-
-              idx = @reports.find_index { |r| r[:id] == updated_report[:id] }
-              @reports[idx] = updated_report if idx
-            end
-          end
-        end
-
-        def add_optional_children(report_el, report_hash)
-          %i(pdf_template svg_template html_template logo).each do |key|
-            next unless report_hash[key]
-
-            child = REXML::Element.new(key.to_s.tr('_', '-'))
-            child.text = report_hash[key]
-            report_el.add_element(child)
-          end
-          report_el.add_element(build_parameters(report_hash[:parameters])) if report_hash[:parameters]
-        end
 
         def parse_parameters(params_elem)
           return {} unless params_elem
@@ -124,147 +58,68 @@ module Opennms
           params_hash = {}
 
           params_elem.elements.each('string-parm') do |el|
-            params_hash[el.attributes['name']] = el.attributes.transform_keys(&:to_s)
+            params_hash[el.attributes['name']] = el.attributes.to_h.transform_keys(&:to_s)
           end
 
           params_elem.elements.each('date-parm') do |el|
-            h = el.attributes.transform_keys(&:to_s)
+            h = el.attributes.to_h.transform_keys(&:to_s)
             h['default-interval'] = el.elements['default-interval']&.text
             h['default-count'] = el.elements['default-count']&.text
 
             default_time_el = el.elements['default-time']
             if default_time_el
               h['default-time'] = if default_time_el.attributes['hour'] && default_time_el.attributes['minute']
-                                    {
-                                      'hour' => default_time_el.attributes['hour'],
-                                      'minute' => default_time_el.attributes['minute'],
-                                    }
-                                  else
-                                    {
-                                      'hour' => default_time_el.elements['hours']&.text,
-                                      'minute' => default_time_el.elements['minutes']&.text,
-                                    }
-                                  end
+                {
+                  'hour' => default_time_el.attributes['hour'],
+                  'minute' => default_time_el.attributes['minute'],
+                }
+              else
+                {
+                  'hour' => default_time_el.elements['hours']&.text,
+                  'minute' => default_time_el.elements['minutes']&.text,
+                }
+              end
             end
 
             params_hash[el.attributes['name']] = h
           end
 
           params_elem.elements.each('int-parm') do |el|
-            params_hash[el.attributes['name']] = el.attributes.transform_keys(&:to_s)
+            params_hash[el.attributes['name']] = el.attributes.to_h.transform_keys(&:to_s)
           end
 
           params_hash
-        end
-
-        def build_parameters(params)
-          params_el = REXML::Element.new('parameters')
-
-          (params['string_parms'] || []).each do |sp|
-            el = REXML::Element.new('string-parm')
-            el.add_attributes(
-              'name' => sp['name'],
-              'display-name' => sp['display_name'],
-              'input-type' => sp['input_type']
-            )
-            el.add_attribute('default', sp['default']) if sp.key?('default')
-            params_el.add_element(el)
-          end
-
-          (params['date_parms'] || []).each do |dp|
-            el = REXML::Element.new('date-parm')
-            el.add_attributes(
-              'name' => dp['name'],
-              'display-name' => dp['display_name']
-            )
-            el.add_attribute('use-absolute-date', dp['use_absolute_date'].to_s) if dp.key?('use_absolute_date')
-
-            interval = REXML::Element.new('default-interval')
-            interval.text = dp['default_interval']
-            el.add_element(interval)
-
-            count = REXML::Element.new('default-count')
-            count.text = dp['default_count'].to_s
-            el.add_element(count)
-
-            if dp['default_time']
-              dt = REXML::Element.new('default-time')
-              hour = REXML::Element.new('hours')
-              hour.text = dp['default_time']['hour'].to_s
-              minute = REXML::Element.new('minutes')
-              minute.text = dp['default_time']['minute'].to_s
-              dt.add_element(hour)
-              dt.add_element(minute)
-              el.add_element(dt)
-            end
-
-            params_el.add_element(el)
-          end
-
-          (params['int_parms'] || []).each do |ip|
-            el = REXML::Element.new('int-parm')
-            el.add_attributes(
-              'name' => ip['name'],
-              'display-name' => ip['display_name'],
-              'input-type' => ip['input_type']
-            )
-            el.add_attribute('default', ip['default'].to_s) if ip.key?('default')
-            params_el.add_element(el)
-          end
-
-          params_el
-        end
-
-        def edit_xml_file(path)
-          raise 'edit_xml_file requires a block' unless block_given?
-
-          content = ::File.read(path)
-          doc = REXML::Document.new(content)
-          yield(doc)
-
-          formatter = REXML::Formatters::Pretty.new(2)
-          formatter.compact = true
-          output = ''
-          formatter.write(doc, output)
-          ::File.write(path, output)
-          doc
         end
       end
     end
 
     module AvailabilityReportTemplate
-      def availability_reports_resource
+      def availability_template_resource
         begin
-          find_resource(:template, availability_reports_config_path)
-        rescue
+          run_context.resource_collection.find(template: availability_report_path)
+        rescue Chef::Exceptions::ResourceNotFound
           nil
         end
       end
 
-      def availability_reports_resource_exist?
-        !availability_reports_resource.nil?
+      def availability_report_path
+        ::File.join(node['opennms']['home'], 'etc', 'availability-reports.xml')
       end
 
-      def availability_reports_config_path
-        ::File.join(node['opennms']['conf']['home'], 'etc', 'availability-reports.xml')
-      end
-
-      def availability_reports_resource_create
-        config_path = availability_reports_config_path
+      def availability_template_resource_create
         config = AvailabilityReportHelper::ReportConfig.new
-
-        if ::File.exist?(config_path)
-          config.read!(config_path)
+        if ::File.exist?(availability_report_path)
+          config.read!(availability_report_path)
         else
-          Chef::Log.info("Config file #{config_path} does not exist; starting with empty report config.")
+          Chef::Log.info "No availability reports config found at #{availability_report_path}, starting fresh."
         end
 
         with_run_context :root do
-          declare_resource(:template, config_path) do
+          declare_resource(:template, availability_report_path) do
             source 'availability-reports.xml.erb'
-            cookbook 'opennms'
-            owner node['opennms']['username']
-            group node['opennms']['groupname']
+            cookbook 'opennms' # adjust to your cookbook name
+            owner node['opennms']['user'] || 'root'
+            group node['opennms']['group'] || 'root'
             mode '0644'
             variables(reports: config.reports)
             action :nothing
