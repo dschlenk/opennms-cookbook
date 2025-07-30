3b7a4f19cbe129147a2011b0ea67136fbeb3bafa
ssgayathri <sgayatri@onec1.com>
2025-07-30 19:40:50 +0530

    Update availability_report.rb

--- resources/availability_report.rb
+++ resources/availability_report.rb
@@ -54,13 +54,17 @@ load_current_value do
   logo report[:logo]
 end
 
-def availability_path
-  ::File.join(new_resource.onms_home, 'etc', 'availability-reports.xml')
-end
+# def availability_path
+  # ::File.join(new_resource.onms_home, 'etc', 'availability-reports.xml')
+# end
 
 action_class do
   include ::Opennms::XmlHelper
   include ::Opennms::Cookbook::AvailabilityReportTemplate
+
+  def availability_path
+    ::File.join(new_resource.onms_home, 'etc', 'availability-reports.xml')
+  end
 
   def etc
     ::File.join(node['opennms']['home'], 'etc')
