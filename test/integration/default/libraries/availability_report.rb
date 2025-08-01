3b8dd5e6f3bd2f9e5852fb42073f085dc9650517
ssgayathri <sgayatri@onec1.com>
2025-08-01 15:45:31 +0530

    Update availability_report.rb

--- test/integration/default/libraries/availability_report.rb
+++ test/integration/default/libraries/availability_report.rb
@@ -55,8 +55,8 @@ module Inspec::Resources
     private
 
     def read_report
-      Inspec::Log.info("DEBUG: Listing /opt/opennms/etc/: #{`ls -la /opt/opennms/etc/`}")
-      Inspec::Log.info("DEBUG: Running user: #{`whoami`.strip}")
+      Inspec::Log.info("DEBUG: Listing /opt/opennms/etc/: #{inspec.command('ls -la /opt/opennms/etc').stdout}")
+      Inspec::Log.info("DEBUG: Running user: #{inspec.command('whoami').stdout.strip}")
       file = inspec.file(@file_path)
       unless file.exist?
         Inspec::Log.warn("Availability report file #{@file_path} does not exist")
