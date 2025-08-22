ff140183a266a41b6c431f57fe958d1a3f2168a0
Melissa Comfort <mcomfort@onec1.com>
2025-08-22 14:36:04 -0400

    Changing the vault version

--- test/fixtures/cookbooks/upgrade/recipes/default.rb
+++ test/fixtures/cookbooks/upgrade/recipes/default.rb
@@ -1,8 +1,8 @@
 canary = '/root/.upgrade-canary'
 if !::File.exist?('/opt/opennms') || !::File.exist?(canary)
   Chef::Log.warn('canary does not exist; overriding')
-  node.override['opennms']['repos']['vault'] = ['33.6.0']
-  node.override['opennms']['version'] = '33.6.0-1'
+  node.override['opennms']['repos']['vault'] = ['33.7.0']
+  node.override['opennms']['version'] = '33.7.0-1'
   node.override['opennms']['stable'] = false
   file canary do
     action :touch
