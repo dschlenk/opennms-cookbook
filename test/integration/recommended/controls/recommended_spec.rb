46f09711925c63a8872aacdd41981fc58c0dc27c
David Schlenk <dschlenk@onec1.com>
2025-09-25 15:24:06 +0000

    lint

--- test/integration/recommended/controls/recommended_spec.rb
+++ test/integration/recommended/controls/recommended_spec.rb
@@ -1,7 +1,7 @@
 %w(opennms-core opennms-webapp-jetty).each do |p|
   describe package(p) do
     it { should be_installed }
-    its('version') { should eq '34.0.0-1' }
+    its('version') { should eq '34.0.1-1' }
   end
 end
 %w(rrdtool jrrd2).each do
